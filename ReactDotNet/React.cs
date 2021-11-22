ebdf954e01e8c68d9418ed9c1122cb39c9112b06
beyaztas <beyaz1404@gmail.com>
2021-11-22 22:54:14 +0300

    .

--- ReactDotNet/React.cs
+++ ReactDotNet/React.cs
@@ -1,24 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Bridge;
-using Bridge.Html5;
 
 namespace ReactDotNet
 {
-    [Name("ReactDOM")]
-    [External]
-    public static class ReactDOM
-    {
-        [Name("render")]
-        public static extern void render(ReactElement element, HTMLElement container);
-
-        [Name("render")]
-        public static extern void render(ReactElement element, HTMLElement container, Action callback);
-
-        [Name("unmountComponentAtNode")]
-        public static extern bool unmountComponentAtNode(HTMLElement container);
-    }
-
     [Name("React")]
     [External]
     public static class React
