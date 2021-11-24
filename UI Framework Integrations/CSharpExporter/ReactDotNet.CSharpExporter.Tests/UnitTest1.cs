6d559ceddafdb95ab1250d1de74f5557814d8b2e
beyaztas <beyaz1404@gmail.com>
2021-11-24 17:51:12 +0300

    .

--- UI Framework Integrations/CSharpExporter/ReactDotNet.CSharpExporter.Tests/UnitTest1.cs
+++ UI Framework Integrations/CSharpExporter/ReactDotNet.CSharpExporter.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ReactDotNet.CSharpExporter.Tests
@@ -8,6 +9,22 @@ namespace ReactDotNet.CSharpExporter.Tests
         [TestMethod]
         public void TestMethod1()
         {
+
+            var input = "type BadgeSizeType = 'normal' | 'large' | 'xlarge';";
+
+            var expected = @"
+    [Enum(Emit.StringNameLowerCase)]
+    public enum BadgeSizeType
+    {
+        normal, large, xlarge
+    }
+
+";
+
+            var output = MyParser.GetCSharpDefinition(input);
+
+            output.Trim().Should().Be(expected);
         }
+
     }
 }
--- UI Framework Integrations/CSharpExporter/ReactDotNet.CSharpExporter/Class1.cs
+++ UI Framework Integrations/CSharpExporter/ReactDotNet.CSharpExporter/Class1.cs
@@ -1,8 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sdcb.TypeScript;
+using Sdcb.TypeScript.TsTypes;
 
 namespace ReactDotNet.CSharpExporter
 {
-    public class Class1
+    public static class MyParser
     {
+        public static string GetCSharpDefinition(string tsCode)
+        {
+            var typeScriptAst = new TypeScriptAST(tsCode);
+
+            
+            var decleration = (TypeAliasDeclaration)typeScriptAst.OfKind(SyntaxKind.TypeAliasDeclaration).First();
+
+            return string.Join(Environment.NewLine, Visit(decleration));
+
+        }
+
+        const string Padding = "    ";
+        static IReadOnlyList<string> Visit(TypeAliasDeclaration typeAliasDeclaration)
+        {
+            return CreateList(add =>
+            {
+                add("[Enum(Emit.StringNameLowerCase)]");
+                add($"public enum {typeAliasDeclaration.IdentifierStr}");
+                add("{");
+
+                var unionType = (UnionTypeNode)typeAliasDeclaration.OfKind(SyntaxKind.UnionType).First();
+
+                add(Padding+string.Join(", ", CreateList(emitEnumName =>
+                {
+                    foreach (var typeNode in unionType.Types)
+                    {
+                        var text = ((StringLiteral)typeNode.First).Text;
+
+                        emitEnumName(text);
+                    }
+
+                })));
+
+                add("}");
+
+            });
+        }
+
+        static IReadOnlyList<string> CreateList(Action<Action<string>> modify)
+        {
+            var list = new List<string>();
+
+            modify(list.Add);
+
+            return list;
+        }
     }
 }
