cfe4fb1183622dab39b9dd456ecd765d22a3dd16
beyaztas <beyaz1404@gmail.com>
2021-12-10 23:14:53 +0300

    .

--- ReactDotNet.ComponentPreviewVsExtension/Configuration.cs
+++ ReactDotNet.ComponentPreviewVsExtension/Configuration.cs
@@ -11,6 +11,8 @@ namespace ___Module___
     {
         #region Public Properties
         public string OutputJsFilePath { get; set; }
+        public string HtmlFilePath { get; set; }
+        public string HtmlElementId { get; set; }
         #endregion
     }
 
@@ -19,11 +21,14 @@ namespace ___Module___
         #region Public Methods
         public static Response<string> GetOutputJsFilePath()
         {
+            return GetConfiguration().Bind(config => config.OutputJsFilePath);
+        }
+
+        public static Response<ConfigurationContract> GetConfiguration()
+        {
             string readConfig() => File.ReadAllText(DirectoryHelper.GetFilePath("ReactDotNet.VsComponentPreview.Config.json"));
 
-            return Try(readConfig)
-                  .Bind( /*deserialize*/JsonConvert.DeserializeObject<ConfigurationContract>)
-                  .Bind(config => config.OutputJsFilePath);
+            return Try(readConfig).Bind( /*deserialize*/JsonConvert.DeserializeObject<ConfigurationContract>);
         }
         #endregion
     }
