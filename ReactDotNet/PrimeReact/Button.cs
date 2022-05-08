6aaed811afda3d01930a8e584bd9f7dfc3531199
beyaztas <beyaz1404@gmail.com>
2022-05-08 12:56:56 +0300

    .

--- ReactDotNet/PrimeReact/Button.cs
+++ ReactDotNet/PrimeReact/Button.cs
@@ -64,7 +64,8 @@ namespace ReactDotNet.PrimeReact
         [React]
         public string badgeClassName { get; set; }
 
-        
+        [React]
+        public bool? autoFocus { get; set; }
 
     }
 }
