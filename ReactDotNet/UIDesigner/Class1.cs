c8581eeae640099cb70281ce442c20de2bd4920c
beyaztas <beyaz1404@gmail.com>
2022-05-23 23:32:46 +0300

    .

--- ReactDotNet/UIDesigner/Class1.cs
+++ ReactDotNet/UIDesigner/Class1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using ReactDotNet.PrimeReact;
 using static ReactDotNet.Mixin;
 
 namespace ReactDotNet.UIDesigner
@@ -8,6 +10,19 @@ namespace ReactDotNet.UIDesigner
     [Serializable]
     public class UIDesignerModel
     {
+        public IReadOnlyList<AAA> Suggestions { get; set; } = new List<AAA>
+        {
+            new() {Name = "Abx", Value = "xyz"}, 
+            new() {Name = "Abx", Value = "xyzyy"},
+        };
+        public string SelectedComponentName { get; set; }
+
+    }
+
+    public class AAA
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
     }
 
     public class UIDesignerView:ReactComponent<UIDesignerModel>
@@ -16,16 +31,34 @@ namespace ReactDotNet.UIDesigner
         {
             state = new UIDesignerModel();
         }
-
+        
         public override Element render()
         {
-            
+            var componentSelector = new ListBox
+            {
+                options     = state.Suggestions,
+                optionLabel = "Name",
+                optionValue = "Value",
+                value       = state.SelectedComponentName,
+                onChange    = OnChange,
+                style =
+                {
+                    height = px(500),
+                    width = px(300)
+                },
+                filter = true
+            };
             return new div(width(px(500)) , height(px(500)), border("1px dashed #e0e0e0"), padding(5))
             {
-                new div("select component"),
+                componentSelector,
                 new div("data"),
-                new div("output")
+                new div("output" + state.SelectedComponentName)
             };
+        }
+
+        void OnChange(ListBoxChangeParams e)
+        {
+            state.SelectedComponentName = e.value;
         }
     }
 }
