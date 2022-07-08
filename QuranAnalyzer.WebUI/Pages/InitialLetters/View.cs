0ac046e2efd5a2676f9a2666b8f8d2f354197382
beyaztas <beyaz1404@gmail.com>
2022-07-08 18:02:58 +0300

    .

--- QuranAnalyzer.WebUI/Pages/InitialLetters/View.cs
+++ QuranAnalyzer.WebUI/Pages/InitialLetters/View.cs
@@ -2,6 +2,7 @@
 using QuranAnalyzer.WebUI.Components;
 using QuranAnalyzer.WebUI.Pages.FactPage;
 using ReactDotNet.Html5;
+using ReactDotNet.react_xarrows;
 using static ReactDotNet.Mixin;
 
 namespace QuranAnalyzer.WebUI.Pages.InitialLetters;
@@ -15,7 +16,55 @@ public class View : PageBase
     public override Element render()
     {
 
-        return new div("başlangıç Harfleri");
+        return new div
+        {
+            children =
+            {
+                new div
+                {
+
+                    style =
+                    {
+                        display = "flex"
+                    },
+                    children =
+                    {
+                        new div("Elif")
+                        {
+                            style = { margin = "10px"},
+                            id    = "Elif"
+                        },
+
+                        new div("Lam")
+                        {
+                            style = { margin = "10px"},
+                            id    = "Lam"
+                        },
+
+                        new div("Mim")
+                        {
+                            style = { margin = "10px"},
+                            id    = "Mim"
+                        },
+
+                        
+
+                       
+                    }
+
+                },
+
+                new div("19 x 344")
+                {
+                    style = { margin = "150px"},
+                    id    = "Result"
+                },
+
+                new Xarrow{start = "Elif", end = "Result", path = "smooth"},
+                new Xarrow{start = "Lam", end  = "Result",path = "smooth"},
+                new Xarrow{start = "Mim", end  = "Result",path = "smooth"},
+            }
+        };
         
     }
 
--- ReactDotNet.Libraries/react-xarrows/Xarrow.cs
+++ ReactDotNet.Libraries/react-xarrows/Xarrow.cs
@@ -1,10 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactDotNet.Html5;
 
 namespace ReactDotNet.react_xarrows
 {
-    public class Xarrow
+    public class Xarrow : ThirdPartyComponent
     {
+        /// <summary>
+        ///     required
+        ///     can be a reference to a react ref to html element or string - an id of a DOM element.
+        /// </summary>
+        [React]
+        public string start { get; set; }
+
+        /// <summary>
+        ///     required
+        ///     can be a reference to a react ref to html element or string - an id of a DOM element.
+        /// </summary>
+        [React]
+        public string end { get; set; }
+
+        /// <summary>
+        ///     "auto" | "middle" | "left" | "right" | "top" | "bottom"
+        /// </summary>
+        [React]
+        public string startAnchor { get; set; }
+
+        /// <summary>
+        ///     "auto" | "middle" | "left" | "right" | "top" | "bottom"
+        /// </summary>
+        [React]
+        public string endAnchor { get; set; }
+
+        [React]
+        public string labels { get; set; }
+
+        /// <summary>
+        ///     defines color to the entire arrow. lineColor,headColor and tailColor will override color specifically for line,tail
+        ///     or head
+        /// </summary>
+        [React]
+        public string color { get; set; }
+
+        [React]
+        public string headColor { get; set; }
+
+        [React]
+        public string tailColor { get; set; }
+
+        [React]
+        public string lineColor { get; set; }
+
+        /// <summary>
+        ///     defines the thickness of the entire arrow. headSize and tailSize defines how big will be the head or tail relative
+        ///     to the strokeWidth
+        /// </summary>
+        [React]
+        public int? strokeWidth { get; set; }
+
+        [React]
+        public int? headSize { get; set; }
+
+        [React]
+        public int? tailSize { get; set; }
+
+        /// <summary>
+        ///     can be one of: "smooth" | "grid" | "straight", and it controls the path arrow is drawn, exactly how their name
+        ///     suggest
+        /// </summary>
+        [React]
+        public string path { get; set; }
+
+        /// <summary>
+        ///     defines how much the lines curve. makes a difference only in path='smooth'
+        /// </summary>
+        [React]
+        public int? curveness { get; set; }
+
+        /// <summary>
+        ///     can animate the drawing of the arrow using svg animation. type: boolean|number. if true animation duration is 1s.
+        ///     if number is passed then animation duration is number's value in seconds
+        ///     <example>animateDrawing={0.1} will animate the drawing of the arrow in 100 milliseconds.</example>
+        /// </summary>
+        [React]
+        public double? animateDrawing { get; set; }
     }
 }
