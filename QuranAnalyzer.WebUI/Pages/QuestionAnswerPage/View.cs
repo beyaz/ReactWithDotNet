3e4ef16f8c2b16bde78cf2e4cfbaf80eeb9da173
beyaztas <beyaz1404@gmail.com>
2022-07-12 11:10:57 +0300

    .

--- QuranAnalyzer.WebUI/Pages/QuestionAnswerPage/View.cs
+++ QuranAnalyzer.WebUI/Pages/QuestionAnswerPage/View.cs
@@ -34,35 +34,39 @@ public class View : PageBase
 
         public override Element render()
         {
-            return new div(Display.flex, AlignItems.center)
-                {
-                    new div
-                    {
-                        style={width_height = "24px"},
-                        children=
-                        {
-                            new svg
-                            {
-                                new path
-                                {
-                                    d    = "M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z",
-                                    fill = "#0b57d0"
+            return new div
+                { 
+                    style={display = "flex", alignItems = "center"},
+                   children=
+                   {
+                       new div
+                       {
+                           style ={width_height = "24px"},
+                           children =
+                           {
+                               new svg
+                               {
+                                   new path
+                                   {
+                                       d    = "M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z",
+                                       fill = "#0b57d0"
 
-                                },
-                                new path
-                                {
-                                    d    = "M0 0h24v24H0z",
-                                    fill = "none"
-                                }
-                            }
-                        }
-                    }
-                    ,
-                    new div
-                    {
-                        innerText = Question,
-                        style={ paddingLeft = "14px", paddingTopBottom = "10px"}
-                    }
+                                   },
+                                   new path
+                                   {
+                                       d    = "M0 0h24v24H0z",
+                                       fill = "none"
+                                   }
+                               }
+                           }
+                       }
+                      ,
+                       new div
+                       {
+                           innerText = Question,
+                           style     ={ paddingLeft = "14px", paddingTopBottom = "10px"}
+                       }
+                   }
 
                 }
                 ;
