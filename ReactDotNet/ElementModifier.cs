d206d33f1858a25cf6bf96ec83725cd17b83f887
beyaztas <beyaz1404@gmail.com>
2022-07-12 11:09:33 +0300

    .

--- ReactDotNet/ElementModifier.cs
+++ ReactDotNet/ElementModifier.cs
@@ -25,47 +25,9 @@ public sealed class ElementModifier
         return new ElementModifier(element => element.style.Import(style));
 
     }
+    
 
-    public static implicit operator ElementModifier(Display display)
-    {
-        return new ElementModifier(element => element.style.display = display);
-
-    }
-
-    public static implicit operator ElementModifier(AlignItems alignItems)
-    {
-        return new ElementModifier(element => element.style.alignItems = alignItems);
-    }
-
-    public static implicit operator ElementModifier(FlexWrap flexWrap)
-    {
-        return new ElementModifier(element => element.style.flexWrap = flexWrap);
-    }
-
-    public static implicit operator ElementModifier(FlexDirection flexDirection)
-    {
-        return new ElementModifier(element => element.style.flexDirection = flexDirection);
-    }
-
-    public static implicit operator ElementModifier(JustifyContent justifyContent)
-    {
-        return new ElementModifier(element => element.style.justifyContent = justifyContent);
-    }
-
-    public static implicit operator ElementModifier(WhiteSpace whiteSpace)
-    {
-        return new ElementModifier(element => element.style.whiteSpace= whiteSpace);
-    }
-
-    public static implicit operator ElementModifier(AlignContent alignContent)
-    {
-        return new ElementModifier(element => element.style.alignContent = alignContent);
-    }
-
-    public static implicit operator ElementModifier(TextAlign textAlign)
-    {
-        return new ElementModifier(element => element.style.textAlign = textAlign);
-    }
+   
 
     public static ElementModifier operator +(ElementModifier a, ElementModifier b)
     {
