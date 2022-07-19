928e928ab1b91e3b9353f87802cf330074737354
beyaztas <beyaz1404@gmail.com>
2022-07-19 23:09:15 +0300

    .

--- ReactWithDotNet.Libraries/UIDesigner/MetaDataHelper.cs
+++ ReactWithDotNet.Libraries/UIDesigner/MetaDataHelper.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,7 +9,7 @@ namespace ReactWithDotNet.UIDesigner;
 
 class MetadataHelper
 {
-    public static List<Type> getAllTypes(Assembly assembly)
+    public static List<Type> GetAllTypes(Assembly assembly)
     {
         List<Type> types = new List<Type>();
 
@@ -30,26 +29,16 @@ class MetadataHelper
             }
         }
     }
-    
+
     public static MetadataNode[] GetMetadataNodes(string assemblyFilePath)
     {
-        var coreAssemblies = Directory.GetFiles(RuntimeEnvironment.GetRuntimeDirectory(), "*.dll");
-        var optionalLibraries = Directory.GetFiles(Path.GetDirectoryName(assemblyFilePath), "*.dll");
-
-        var libraries = new List<string>();
-        libraries.AddRange(coreAssemblies);
-        libraries.AddRange(optionalLibraries);
-
-
-        var resolver = new PathAssemblyResolver(libraries);
+        var (assembly, metadataLoadContext) = ReadAssembly(assemblyFilePath);
 
         var items = new List<MetadataNode>();
-        
-        using (var metadataContext = new MetadataLoadContext(resolver))
-        {
-            Assembly assembly = metadataContext.LoadFromAssemblyPath(assemblyFilePath);
 
-            var types = getAllTypes(assembly);
+        using (metadataLoadContext)
+        {
+            var types = GetAllTypes(assembly);
 
             foreach (var namespaceName in types.Select(t => t.Namespace).Distinct())
             {
@@ -58,18 +47,14 @@ class MetadataHelper
                     Name        = namespaceName,
                     IsNamespace = true
                 };
-                
+
                 nodeForNamespace.children.AddRange(types.Where(x => x.Namespace == namespaceName).Select(classToMetaData));
-                
+
                 items.Add(nodeForNamespace);
             }
         }
 
         return items.ToArray();
-
- 
-
-       
 
         static MetadataNode classToMetaData(Type x)
         {
@@ -79,15 +64,16 @@ class MetadataHelper
                 Name    = x.Name
             };
 
-            foreach (var methodInfo in x.GetMethods(BindingFlags.Instance| BindingFlags.Static| BindingFlags.Public| BindingFlags.NonPublic ))
+            foreach (var methodInfo in x.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
             {
                 if (methodInfo.Name.StartsWith("get_") || methodInfo.Name.StartsWith("set_"))
                 {
                     continue;
                 }
+
                 classNode.children.Add(createFromMethod(methodInfo));
             }
-            
+
             return classNode;
         }
 
@@ -97,11 +83,26 @@ class MetadataHelper
             {
                 IsMethod                  = true,
                 Name                      = methodInfo.Name,
-                FullNameWithoutReturnType =string.Join(" ", methodInfo.ToString()!.Split(new[] { ' ' }).Skip(1)),
+                FullNameWithoutReturnType = string.Join(" ", methodInfo.ToString()!.Split(new[] { ' ' }).Skip(1)),
                 MetadataToken             = methodInfo.MetadataToken,
             };
         }
     }
 
-    
+    public static (Assembly assembly, MetadataLoadContext metadataLoadContext) ReadAssembly(string assemblyFilePath)
+    {
+        var coreAssemblies = Directory.GetFiles(RuntimeEnvironment.GetRuntimeDirectory(), "*.dll");
+
+        var optionalLibraries = Directory.GetFiles(Path.GetDirectoryName(assemblyFilePath), "*.dll");
+
+        var libraries = new List<string>();
+        libraries.AddRange(coreAssemblies);
+        libraries.AddRange(optionalLibraries);
+
+        var resolver = new PathAssemblyResolver(libraries);
+
+        var metadataContext = new MetadataLoadContext(resolver);
+
+        return (metadataContext.LoadFromAssemblyPath(assemblyFilePath), metadataContext);
+    }
 }
