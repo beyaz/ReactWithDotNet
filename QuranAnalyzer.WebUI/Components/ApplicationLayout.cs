62fafd8277ce3470a6f56171d8489f715f2116c7
beyaztas <beyaz1404@gmail.com>
2022-07-27 19:29:37 +0300

    .

--- QuranAnalyzer.WebUI/Components/ApplicationLayout.cs
+++ QuranAnalyzer.WebUI/Components/ApplicationLayout.cs
@@ -13,27 +13,11 @@ class ApplicationLayout : ReactComponent
    
     public override Element render()
     {
-        var top = new div
+        var top = new FixedTopPanelContainer
         {
-            style =
-            {
-                position = "fixed",
-                top      = "0px",
-                left     = "0px",
-
-                width        = "100%",
-                height       = "50px",
-                zIndex       = "1",
-                borderBottom = "1px solid #dadce0"
-            },
+            HasShadow = mainDivScrollY > 0 ,
             children = { topContent }
-        } ;
-
-        if (mainDivScrollY > 0)
-        {
-            top.style.borderBottom = "";
-            top.style.boxShadow    = "0 1px 2px hsla(0,0%,0%,0.05),0 1px 4px hsla(0,0%,0%,0.05),0 2px 8px hsla(0,0%,0%,0.05)";
-        }
+        };
 
         var backDrop = new div { className = "p-blockui p-component-overlay p-component-overlay-enter",style = { zIndex = "3"}};
         var main = new div
---
+++ QuranAnalyzer.WebUI/Components/FixedTopPanelContainer.cs
@@ -0,0 +1,35 @@
+﻿using ReactWithDotNet;
+
+namespace QuranAnalyzer.WebUI.Components;
+
+class FixedTopPanelContainer : ReactComponent
+{
+    public bool HasShadow { get; set; }
+    
+    public override Element render()
+    {
+        var top = new div
+        {
+            style =
+            {
+                position = "fixed",
+                top      = "0px",
+                left     = "0px",
+
+                width        = "100%",
+                height       = "50px",
+                zIndex       = "1",
+                borderBottom = "1px solid #dadce0"
+            },
+            Children = children
+        };
+
+        if (HasShadow)
+        {
+            top.style.borderBottom = "";
+            top.style.boxShadow    = "0 1px 2px hsla(0,0%,0%,0.05),0 1px 4px hsla(0,0%,0%,0.05),0 2px 8px hsla(0,0%,0%,0.05)";
+        }
+        
+        return top;
+    }
+}
