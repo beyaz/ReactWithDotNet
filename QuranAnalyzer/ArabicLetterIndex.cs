85854f6b16426ecd770c1c41e3e4a20c81ef7d30
beyaztas <beyaz1404@gmail.com>
2022-07-30 19:13:19 +0300

    .

--- QuranAnalyzer/ArabicLetterIndex.cs
+++ QuranAnalyzer/ArabicLetterIndex.cs
@@ -2,7 +2,34 @@
 
 public static class ArabicLetterIndex
 {
-    #region Public Properties
+    // @formatter:off
     public static int Alif => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Alif).Value;
-    #endregion
+    public static int Baa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Baa).Value;
+    public static int Taa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Taa).Value;
+    public static int Thaa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Thaa).Value;
+    public static int Jiim => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Jiim).Value;
+    public static int Haa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Haa).Value;
+    public static int Khaa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Khaa).Value;
+    public static int Daal => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Daal).Value;
+    public static int Dhaal => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Dhaal).Value;
+    public static int Raa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Raa).Value;
+    public static int Zay => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Zay).Value;
+    public static int Siin => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Siin).Value;
+    public static int Shiin => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Shiin).Value;
+    public static int Saad => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Saad).Value;
+    public static int Daad => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Daad).Value;
+    public static int Taa_ => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Taa_).Value;
+    public static int Zaa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Zaa).Value;
+    public static int Ayn => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Ayn).Value;
+    public static int Ghayn => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Ghayn).Value;
+    public static int Faa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Faa).Value;
+    public static int Qaaf => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Qaaf).Value;
+    public static int Kaaf => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Kaaf).Value;
+    public static int Laam => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Laam).Value;
+    public static int Miim => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Miim).Value;
+    public static int Nun => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Nun).Value;
+    public static int Haa_ => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Haa_).Value;
+    public static int Waaw => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Waaw).Value;
+    public static int Yaa => ArabicLetter.AllArabicLetters.GetIndex(ArabicLetter.Yaa).Value;
+    // @formatter:on
 }
