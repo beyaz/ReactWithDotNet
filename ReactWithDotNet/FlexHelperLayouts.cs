810d8a12b7adc9db94ed7a8b1a185c8019237db6
beyaztas <beyaz1404@gmail.com>
2022-07-31 20:53:01 +0300

    .

---
+++ ReactWithDotNet/FlexHelperLayouts.cs
@@ -0,0 +1,43 @@
+﻿namespace ReactWithDotNet;
+
+public sealed class HStack : div
+{
+    public HStack()
+    {
+        style.display       = "flex";
+        style.flexDirection = "row";
+        style.alignItems    = "stretch";
+        style.width         = "100%";
+    }
+}
+
+public sealed class HPanel : div
+{
+    public HPanel()
+    {
+        style.display       = "flex";
+        style.flexDirection = "row";
+        style.alignItems    = "center";
+    }
+}
+
+public sealed class VPanel : div
+{
+    public VPanel()
+    {
+        style.display       = "flex";
+        style.flexDirection = "column";
+        style.alignItems    = "center";
+    }
+}
+
+public sealed class VStack : div
+{
+    public VStack()
+    {
+        style.display       = "flex";
+        style.flexDirection = "column";
+        style.alignItems    = "stretch";
+        style.height        = "100%";
+    }
+}
--- ReactWithDotNet/HtmlElements.cs
+++ ReactWithDotNet/HtmlElements.cs
@@ -138,48 +138,6 @@ public class img : HtmlElement
     public int height { get; set; }
 }
 
-public class HStack : div
-{
-    public HStack()
-    {
-        style.display       = "flex";
-        style.flexDirection = "row";
-        style.alignItems    = "stretch";
-        style.width         = "100%";
-    }
-}
-
-public class HPanel : div
-{
-    public HPanel()
-    {
-        style.display       = "flex";
-        style.flexDirection = "row";
-        style.alignItems    = "center";
-    }
-}
-
-public class VPanel : div
-{
-    public VPanel()
-    {
-        style.display       = "flex";
-        style.flexDirection = "column";
-        style.alignItems    = "center";
-    }
-}
-
-
-public sealed class VStack : div
-{
-    public VStack()
-    {
-        style.display = "flex";
-        style.flexDirection = "column";
-        style.alignItems = "stretch";
-        style.height = "100%";
-    }
-}
 
 
 
