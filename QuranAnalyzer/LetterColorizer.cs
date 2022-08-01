299531fae80200529dc1bc1d61c923ca951e20a5
beyaztas <beyaz1404@gmail.com>
2022-08-01 19:01:48 +0300

    .

--- QuranAnalyzer/LetterColorizer.cs
+++ QuranAnalyzer/LetterColorizer.cs
@@ -13,19 +13,24 @@ class LetterColorizer : ReactComponent
     public string LettersForColorize { get; set; }
     public IReadOnlyList<LetterMatchInfo> LettersForColorizeNodes { get; set; }
     public string VerseText { get; set; }
-
     public IReadOnlyList<LetterMatchInfo> VerseTextNodes { get; set; }
+
+    public string ChapterNumber { get; set; }
+    public string VerseNumber { get; set; }
     #endregion
 
     #region Public Methods
     public override Element render()
     {
+        
         var verseText = VerseTextNodes ??= Analyzer.AnalyzeText(VerseText).Where(Analyzer.IsArabicLetter).ToList();
         
         var lettersForColorize = LettersForColorizeNodes ??= Analyzer.AnalyzeText(LettersForColorize).Where(Analyzer.IsArabicLetter).ToList();
 
         var cursor = 0;
 
+        var counts = new int[lettersForColorize.Count];
+        
         var html = new StringBuilder();
 
         for (var i = 0; i < verseText.Count; i++)
@@ -54,6 +59,9 @@ class LetterColorizer : ReactComponent
 
                     cursor = verseText[i].StartIndex + len;
 
+                    counts[j]++;
+
+
                     break;
                 }
             }
@@ -64,13 +72,49 @@ class LetterColorizer : ReactComponent
             html.Append(VerseText.Substring(cursor));
         }
 
-        return new div
+        var countsView = new HPanel
+        {
+            style =
+            {
+                border = "1px dashed rgb(218, 220, 224)",
+                padding = "5px"
+            },
+            children = { new div($"Ayet: {ChapterNumber} - {VerseNumber}"){style = { marginLeftRight = "10px"}} }
+        };
+        
+        for (var j = 0; j < lettersForColorize.Count; j++)
+        {
+            var letter = new div(lettersForColorize[j].MatchedLetter) { style = { color = getColor(j) } };
+
+            var countView = new HPanel
+            {
+                children =
+                {
+                    letter,
+                    new div(":") { style = { marginLeftRight = "4px" } },
+                    new div(counts[j].ToString())
+                },
+                style = { marginLeftRight = "10px" }
+            };
+
+
+            countsView.appendChild(countView);
+        }
+
+        var textView = new div
         {
             innerHTML = html.ToString(),
             style =
             {
-                fontSize = "1.4rem"
+                fontSize = "1.4rem",
+                border   = "1px dashed rgb(218, 220, 224)",
+                padding = "5px"
             }
+        };
+
+        return new VStack
+        {
+            textView, countsView
         };
     }
     #endregion
