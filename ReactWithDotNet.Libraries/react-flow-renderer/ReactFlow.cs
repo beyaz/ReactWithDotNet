573e114bac1f8bcc47586515852cf2204d9e7fc2
beyaztas <beyaz1404@gmail.com>
2022-08-03 16:40:29 +0300

    .

---
+++ ReactWithDotNet.Libraries/react-flow-renderer/ReactFlow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReactWithDotNet.react_flow_renderer
+{
+    public class ReactFlow: ThirdPartyReactComponent
+    {
+        public dynamic nodes { get; set; }
+        public dynamic edges { get; set; }
+        
+    }
+}
