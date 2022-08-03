709a09f0b21415cb96a87dc79e2cb0f73a69804d
beyaztas <beyaz1404@gmail.com>
2022-08-04 00:18:18 +0300

    .

--- QuranAnalyzer/QuranAnalyzerMixin.cs
+++ QuranAnalyzer/QuranAnalyzerMixin.cs
@@ -42,41 +42,7 @@ public static class QuranAnalyzerMixin
         return verseList.Sum(calculateCount);
     }
 
-    public static Response<IReadOnlyList<MatchInfo>> SearchCharachters(string searchScript, string searchCharachters, MushafOptions mushafOptions = null)
-    {
-        var indexList = Analyzer.AnalyzeText(searchCharachters).Where(x => x.ArabicLetterIndex >= 0).Select(x => x.ArabicLetterIndex).ToList();
-        if (indexList.Count == 0)
-        {
-            return "En az bir harf girilmelidir.";
-        }
-
-        var items = new List<MatchInfo>();
-
-        var verseListResponse = GetVerseList(searchScript);
-        if (verseListResponse.IsFail)
-        {
-            return verseListResponse.FailMessage;
-        }
-
-        var verseList = verseListResponse.Value;
-
-        bool canSelect(MatchInfo matchInfo)
-        {
-            return indexList.Contains(matchInfo.ArabicLetterIndex);
-        }
-
-        foreach (var verse in verseList)
-        {
-            items.AddRange(Analyzer.AnalyzeVerse(verse, mushafOptions?.CountHamzaAsAlif ?? true).Where(canSelect));
-        }
-
-        foreach (var arabicCharacterIndex in indexList)
-        {
-            items = RecalculateWithOptions(items, mushafOptions, arabicCharacterIndex);
-        }
-
-        return items;
-    }
+ 
     #endregion
 
     #region Methods
@@ -85,37 +51,6 @@ public static class QuranAnalyzerMixin
         return character.AsArabicCharacterIndex().Then(arabicCharacterIndex => matchList.Count(x => x.ArabicLetterIndex == arabicCharacterIndex));
     }
 
-    static List<MatchInfo> RecalculateWithOptions(List<MatchInfo> mathInfoList, MushafOptions mushafOptions, int arabicCharacterIndex)
-    {
-        if (mushafOptions != null)
-        {
-            if (mushafOptions.Use_Sad_in_Surah_7_Verse_69_in_word_bestaten)
-            {
-                var sadIndex = ArabicLetter.Saad.AsArabicCharacterIndex().Value;
-
-                if (arabicCharacterIndex == sadIndex)
-                {
-                    mathInfoList.RemoveAll(x => x.Verse.Id == "7:69" && x.ArabicLetterIndex == sadIndex);
-                }
-            }
-
-            if (mushafOptions.UseElifCountsSpecifiedByRK)
-            {
-                if (ArabicLetterIndex.Alif == arabicCharacterIndex)
-                {
-                    foreach (var key in SpecifiedByRK.RealElifCounts.Keys)
-                    {
-                        var match = mathInfoList.FirstOrDefault(x=>x.Verse.Id == key && x.ArabicLetterIndex == arabicCharacterIndex);
-                        if (match is not null)
-                        {
-                            
-                        }
-                    }
-                }
-            }
-        }
-
-        return mathInfoList;
-    }
+  
     #endregion
 }
