e392594f7aa479c74f326ad3eb81ddc0e8b18650
beyaztas <beyaz1404@gmail.com>
2022-08-13 19:42:56 +0300

    .

--- QuranAnalyzer/QuranAnalyzerMixin.cs
+++ QuranAnalyzer/QuranAnalyzerMixin.cs
@@ -1,7 +1,42 @@
 ﻿namespace QuranAnalyzer;
 
+public enum MushafId
+{
+    Tanzil = 1, RK = 2
+}
+
+public sealed class MushafTotalCountPerVerseDifference
+{
+    public string VerseId { get; init; }
+    public int TotalCount { get; init; }
+    public MushafId MushafId { get; init; }
+
+    public MushafTotalCountPerVerseDifference(string verseId, int count, MushafId mushafId = MushafId.RK)
+    {
+        VerseId    = verseId;
+        TotalCount = count;
+        MushafId   = mushafId;
+    }
+}
+
 public static class QuranAnalyzerMixin
 {
+    public static readonly IReadOnlyDictionary<int, IReadOnlyList<MushafTotalCountPerVerseDifference>> MushafDifferences = new Dictionary<int, IReadOnlyList<MushafTotalCountPerVerseDifference>>
+    {
+        [ArabicLetterIndex.Saad] = new[]
+        {
+            new MushafTotalCountPerVerseDifference("7:69",1,MushafId.Tanzil), // bestaten: *بَسْطَةً*
+            new MushafTotalCountPerVerseDifference("7:69",0) // bestaten: *بَصْۜطَةً*
+        },
+        [ArabicLetterIndex.Laam] = new[]
+        {
+            new MushafTotalCountPerVerseDifference("11:70",8,MushafId.Tanzil),
+            new MushafTotalCountPerVerseDifference("11:70",9)
+        }
+
+    };
+
+
     #region Public Methods
     public static int GetCountOfLetter(IReadOnlyList<Verse> verseList, int arabicLetterIndex, MushafOption option = null)
     {
@@ -31,7 +66,7 @@ public static class QuranAnalyzerMixin
 
         if (arabicLetterIndex == ArabicLetterIndex.Saad && option.Use_Sad_in_Surah_7_Verse_69_in_word_bestaten && verse.Id == "7:69")
         {
-            return SpecifiedByRK.SS[verse.Id];
+            return MushafDifferences[ArabicLetterIndex.Saad].First(x => x.MushafId == MushafId.RK && x.VerseId == "7:69").TotalCount;
         }
 
         if (arabicLetterIndex == ArabicLetterIndex.Laam && option.Use_Lam_SpecifiedByRK && SpecifiedByRK.Lam.ContainsKey(verse.Id))
