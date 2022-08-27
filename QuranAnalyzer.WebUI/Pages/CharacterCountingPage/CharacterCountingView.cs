2c82b758d97163d817e10cee69b4d81641aa54bc
beyaztas <beyaz1404@gmail.com>
2022-08-27 13:34:03 +0300

    .

--- QuranAnalyzer.WebUI/Pages/CharacterCountingPage/CharacterCountingView.cs
+++ QuranAnalyzer.WebUI/Pages/CharacterCountingPage/CharacterCountingView.cs
@@ -70,55 +70,6 @@ class SearchScript
     }
 }
 
-class ErrorTextModel
-{
-    public string Text { get; set; }
-}
-
-class ErrorText: ReactComponent<ErrorTextModel>
-{
-    public string Text { get; set; }
-    
-    public ErrorText()
-    {
-        state =  new ErrorTextModel();
-        
-        StateInitialized += () => state.Text = Text;
-    }
-    
-
-    public void ClearMessage()
-    {
-        state.Text = null;
-        Text       = null;
-    }
-    
-    public override Element render()
-    {
-        if (state.Text.HasValue())
-        {
-            ClientTask.GotoMethod(3000, nameof(ClearMessage));
-        }
-        
-        var element = new small
-        {
-            text = state.Text,
-            style =
-            {
-                color     = "#e24c4c",
-                marginTop = "5px"
-            }
-        };
-
-        if (state.Text.HasNoValue())
-        {
-            element.style.display = "none";
-        }
-        
-        return element;
-        
-    }
-}
 
 
 class CharacterCountingView : ReactComponent<CharacterCountingViewModel>
---
+++ QuranAnalyzer.WebUI/Pages/CharacterCountingPage/ErrorText.cs
@@ -0,0 +1,52 @@
+﻿namespace QuranAnalyzer.WebUI.Pages.CharacterCountingPage;
+
+
+class ErrorTextModel
+{
+    public string Text { get; set; }
+}
+
+class ErrorText: ReactComponent<ErrorTextModel>
+{
+    public string Text { get; set; }
+    
+    public ErrorText()
+    {
+        state = new ErrorTextModel();
+        
+        StateInitialized += () => state.Text = Text;
+    }
+    
+
+    public void ClearMessage()
+    {
+        state.Text = null;
+        Text       = null;
+    }
+    
+    public override Element render()
+    {
+        if (state.Text.HasValue())
+        {
+            ClientTask.GotoMethod(3000, nameof(ClearMessage));
+        }
+        
+        var element = new small
+        {
+            text = state.Text,
+            style =
+            {
+                color     = "#e24c4c",
+                marginTop = "5px"
+            }
+        };
+
+        if (state.Text.HasNoValue())
+        {
+            element.style.display = "none";
+        }
+        
+        return element;
+        
+    }
+}
