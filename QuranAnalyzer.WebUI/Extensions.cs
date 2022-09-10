c26051682b0b142bcc96950c8179627d300fa68f
beyaztas <beyaz1404@gmail.com>
2022-09-10 21:04:58 +0300

    .

--- QuranAnalyzer.WebUI/Extensions.cs
+++ QuranAnalyzer.WebUI/Extensions.cs
@@ -1,39 +1,17 @@
-﻿using System;
-using QuranAnalyzer.WebUI.Pages.MainPage;
-
+﻿
 namespace QuranAnalyzer.WebUI;
 
 static class Extensions
 {
-    public static string GetPageLink(string pageId) => $"/?{QueryKey.Page}=" + pageId;
+    public static string GetPageLink(string pageId) => App.Settings.RootLocation + $"?{QueryKey.Page}=" + pageId;
     
     public static bool HasNoValue(this string value) => string.IsNullOrWhiteSpace(value);
 
     public static bool HasValue(this string value) => !string.IsNullOrWhiteSpace(value);
 
-    public static T TryGet<T>(this T[] array, int index)
-    {
-        try
-        {
-            return array[index];
-        }
-        catch (Exception)
-        {
-            return default;
-        }
-    }
+   
 
-    public static (string ChapterFilter, string searchLetters) ParseLetterCountingScript(string value)
-    {
-        var arr = value.Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-
-        return (arr.TryGet(0), arr.TryGet(1));
-    }
-
-    public static IReadOnlyList<T> ToReadOnlyList<T>(this IEnumerable<T> items)
-    {
-        return items.ToList().AsReadOnly();
-    }
+   
 
     public static string GetLetterCountingScript(string chapterFilter, params string[] arabicLetters)
     {
--- QuranAnalyzer.WebUI/PageId.cs
+++ QuranAnalyzer.WebUI/PageId.cs
@@ -1,4 +1,7 @@
-﻿namespace QuranAnalyzer.WebUI;
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace QuranAnalyzer.WebUI;
 
 static class PageId
 {
@@ -40,4 +43,28 @@ static class ContextKey
 {
     public static ReactContextKey<MushafOption> MushafOptionKey = new(nameof(MushafOptionKey));
     public static ReactContextKey<bool> HamburgerMenuIsOpen = new(nameof(HamburgerMenuIsOpen));
+}
+
+class SettingsFile
+{
+    public string RootLocation { get; set; }
+    public string LogFilePath { get; set; }
+    public bool IsLogEnabled { get; set; }
+}
+
+static class App
+{
+    public static SettingsFile Settings = JsonConvert.DeserializeObject<SettingsFile>(File.ReadAllText("Settings.json"));
+
+    public static void Log(string message)
+    {
+        if (Settings.IsLogEnabled)
+        {
+            var path = Settings.LogFilePath;
+
+            using var file   = File.Exists(path) ? File.Open(path, FileMode.Append) : File.Open(path, FileMode.CreateNew);
+            using var stream = new StreamWriter(file);
+            stream.WriteLine(message);
+        }
+    }
 }
