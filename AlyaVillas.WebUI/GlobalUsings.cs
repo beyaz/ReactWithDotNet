0e107c900bd7ac1d9a2b336edd9f8b3705ef9e8a
beyaztas <beyaz1404@gmail.com>
2022-09-19 09:13:30 +0300

    .

--- AlyaVillas.WebUI/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-﻿global using System;
-global using System.Linq.Expressions;
-global using ReactWithDotNet;
-global using System.Collections.Generic;
-global using System.Linq;
-global using static ReactWithDotNet.Mixin;
-global using static AlyaVillas.WebUI.Extension;
-global using static AlyaVillas.WebUI.Mixin;
-
