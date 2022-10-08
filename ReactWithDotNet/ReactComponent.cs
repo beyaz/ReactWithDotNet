4ba7bf19a59d9cefcb3332747d9db448598509e3
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-10-08 14:54:22 +0300

    .

--- ReactWithDotNet/ReactComponent.cs
+++ ReactWithDotNet/ReactComponent.cs
@@ -4,36 +4,23 @@ using System.Web;
 
 namespace ReactWithDotNet;
 
-
-
-
-
-
-
-
-
 public sealed class ReactContext
 {
     readonly Dictionary<string, object> map = new();
+    public double ClientHeight { get; internal set; }
 
-    public TValue TryGetValue<TValue>(ReactContextKey<TValue> key)
+    public double ClientWidth { get; internal set; }
+    public NameValueCollection Query { get; internal set; }
+    public string QueryAsString => string.Join("&", Query.AllKeys.Select(key => $"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(Query[key])}"));
+
+    public bool Contains<TValue>(ReactContextKey<TValue> key)
     {
-        if (map.TryGetValue(key.Key, out var value))
-        {
-            return (TValue)value;
-        }
-
-        return default;
+        return map.ContainsKey(key.Key);
     }
 
     public void Set<TValue>(ReactContextKey<TValue> key, TValue value)
     {
         Set(key.Key, value);
-    }
-
-    public bool Contains<TValue>(ReactContextKey<TValue> key)
-    {
-        return map.ContainsKey(key.Key);
     }
 
     public void Set<TValue>(string key, TValue value)
@@ -47,15 +34,16 @@ public sealed class ReactContext
         map.Add(key, value);
     }
 
-    public double ClientWidth { get; internal set; }
-    public double ClientHeight { get; internal set; }
-    public NameValueCollection Query { get; internal set; }
-    public string QueryAsString=> string.Join("&", Query.AllKeys.Select(key => $"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(Query[key])}"));
+    public TValue TryGetValue<TValue>(ReactContextKey<TValue> key)
+    {
+        if (map.TryGetValue(key.Key, out var value))
+        {
+            return (TValue)value;
+        }
 
+        return default;
+    }
 }
-
-
-
 
 // ReSharper disable once UnusedTypeParameter
 public sealed class ReactContextKey<TValue>
@@ -68,60 +56,52 @@ public sealed class ReactContextKey<TValue>
     }
 }
 
-
-
 public abstract class ReactStatefulComponent : Element
 {
-    [JsonIgnore]
-    [Newtonsoft.Json.JsonIgnore]
-    protected internal ReactContext Context { get; set; }
+    internal List<IModifier> modifiers;
 
     [JsonIgnore]
     [Newtonsoft.Json.JsonIgnore]
     public ClientTaskCollection ClientTask { get; internal set; } = new();
 
-    protected abstract Element render();
-
-    protected  abstract void constructor();
-
-    internal void InvokeConstructor() => constructor();
-    
-    internal Element InvokeRender() => render();
-
-    protected virtual void componentDidMount()
-    {
-        
-    }
+    [JsonIgnore]
+    [Newtonsoft.Json.JsonIgnore]
+    protected internal ReactContext Context { get; set; }
 
     internal object Clone()
     {
         return MemberwiseClone();
     }
 
-    internal List<IModifier> modifiers;
-    
+    internal void InvokeConstructor() => constructor();
+
+    internal Element InvokeRender() => render();
+
     protected internal sealed override void ProcessModifier(IModifier modifier)
     {
         if (modifier is null)
         {
             return;
         }
-        
+
         modifiers ??= new List<IModifier>();
 
         modifiers.Add(modifier);
     }
 
+    protected virtual void componentDidMount()
+    {
+    }
 
-
+    protected abstract void constructor();
 
     /// <summary>
-    ///     Sample event decleration <br/>
+    ///     Sample event decleration <br />
     ///     [React] public Action OnUserChanged { get; set; }
-    /// <br/>
-    /// <br/>
-    /// Sample event dispatching <br/>
-    /// DispatchEvent(()=> OnUserChanged);
+    ///     <br />
+    ///     <br />
+    ///     Sample event dispatching <br />
+    ///     DispatchEvent(()=> OnUserChanged);
     /// </summary>
     protected void DispatchEvent(Expression<Func<Action>> expressionForAccessingCustomReactEventProperty)
     {
@@ -133,46 +113,46 @@ public abstract class ReactStatefulComponent : Element
     }
 
     /// <summary>
-    ///     Sample event decleration <br/>
+    ///     Sample event decleration <br />
     ///     [React] public Action&lt;UserInfo&gt; OnUserChanged { get; set; }
-    /// <br/>
-    /// <br/>
-    /// Sample event dispatching <br/>
-    /// DispatchEvent(()=> OnUserChanged, state.SelectedUserInfo);
+    ///     <br />
+    ///     <br />
+    ///     Sample event dispatching <br />
+    ///     DispatchEvent(()=> OnUserChanged, state.SelectedUserInfo);
     /// </summary>
     protected void DispatchEvent<A>(Expression<Func<Action<A>>> expressionForAccessingCustomReactEventProperty, A a)
     {
         var expression = (MemberExpression)expressionForAccessingCustomReactEventProperty.Body;
-        
+
         var propertyNameOfCustomReactEvent = expression.Member.Name;
 
         ClientTask.DispatchEvent(Mixin.GetEventKey(this, propertyNameOfCustomReactEvent), a);
     }
 
     /// <summary>
-    ///     Sample event decleration <br/>
+    ///     Sample event decleration <br />
     ///     [React] public Action&lt;UserInfo,OrderInfo&gt; OnUserChanged { get; set; }
-    /// <br/>
-    /// <br/>
-    /// Sample event dispatching <br/>
-    /// DispatchEvent(()=> OnUserChanged, state.SelectedUserInfo, state.SelectedOrderInfo);
+    ///     <br />
+    ///     <br />
+    ///     Sample event dispatching <br />
+    ///     DispatchEvent(()=> OnUserChanged, state.SelectedUserInfo, state.SelectedOrderInfo);
     /// </summary>
-    protected void DispatchEvent<A,B>(Expression<Func<Action<A,B>>> customReactEvent, A a, B b)
+    protected void DispatchEvent<A, B>(Expression<Func<Action<A, B>>> customReactEvent, A a, B b)
     {
         var expression = (MemberExpression)customReactEvent.Body;
 
         var propertyNameOfCustomReactEvent = expression.Member.Name;
 
-        ClientTask.DispatchEvent(Mixin.GetEventKey(this, propertyNameOfCustomReactEvent), a,b);
+        ClientTask.DispatchEvent(Mixin.GetEventKey(this, propertyNameOfCustomReactEvent), a, b);
     }
 
     /// <summary>
-    ///     Sample event decleration <br/>
+    ///     Sample event decleration <br />
     ///     [React] public Action&lt;UserInfo,OrderInfo,CommissionInfo&gt; OnUserChanged { get; set; }
-    /// <br/>
-    /// <br/>
-    /// Sample event dispatching <br/>
-    /// DispatchEvent(()=> OnUserChanged, state.SelectedUserInfo, state.SelectedOrderInfo, state.SelectedCommissionInfo);
+    ///     <br />
+    ///     <br />
+    ///     Sample event dispatching <br />
+    ///     DispatchEvent(()=> OnUserChanged, state.SelectedUserInfo, state.SelectedOrderInfo, state.SelectedCommissionInfo);
     /// </summary>
     protected void DispatchEvent<A, B, C>(Expression<Func<Action<A, B>>> customReactEvent, A a, B b, C c)
     {
@@ -183,6 +163,7 @@ public abstract class ReactStatefulComponent : Element
         ClientTask.DispatchEvent(Mixin.GetEventKey(this, propertyNameOfCustomReactEvent), a, b, c);
     }
 
+    protected abstract Element render();
 }
 
 public abstract class ReactComponent : ReactComponent<EmptyState>
