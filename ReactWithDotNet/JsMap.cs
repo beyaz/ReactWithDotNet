af0bac108f02c990f7944661cebf2bf2cbc415f9
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-10-18 00:44:24 +0300

    .

---
+++ ReactWithDotNet/JsMap.cs
@@ -0,0 +1,52 @@
+namespace ReactWithDotNet;
+
+sealed class JsMap
+{
+    int count;
+
+    Node head;
+    Node tail;
+
+    public int Count => count;
+
+    public void Add(string key, object value)
+    {
+        count++;
+
+        var node = new Node { key = key, value = value };
+
+        if (head == null)
+        {
+            tail = head = node;
+            return;
+        }
+
+        tail.next = node;
+
+        tail = node;
+    }
+
+    public void Foreach(Action<string, object> action)
+    {
+        if (head == null)
+        {
+            return;
+        }
+
+        var node = head;
+
+        while (node is not null)
+        {
+            action(node.key, node.value);
+
+            node = node.next;
+        }
+    }
+
+    class Node
+    {
+        public string key;
+        public Node next;
+        public object value;
+    }
+}
