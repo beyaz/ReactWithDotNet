dca1d730a301615de75afbd4f16ccc7eabcf007b
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-10-25 00:05:56 +0300

    .

--- ReactWithDotNet.Libraries/UIDesigner/Extensions.cs
+++ ReactWithDotNet.Libraries/UIDesigner/Extensions.cs
@@ -1,9 +1,43 @@
 ﻿using System.IO;
+using System.Reflection;
 
 namespace ReactWithDotNet.UIDesigner;
 
 static class Extensions
 {
+    public static string GetSvgUrl(string svgFileName)
+    {
+        var resourceFilePathInAssembly = $"ReactWithDotNet.Libraries.UIDesigner.Resources.{svgFileName}.svg";
+
+        return getDataUriFromSvgBytes(getEmbeddedFile(resourceFilePathInAssembly));
+
+        static string getDataUriFromSvgBytes(byte[] bytesOfSvgFile)
+        {
+            var imageBase64 = Convert.ToBase64String(bytesOfSvgFile);
+
+            return "data:image/svg+xml;base64," + imageBase64;
+        }
+
+        static byte[] getEmbeddedFile(string resourceFilePathInAssembly)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var resourceStream = assembly.GetManifestResourceStream(resourceFilePathInAssembly);
+            if (resourceStream == null)
+            {
+                return null;
+            }
+
+            using var memoryStream = new MemoryStream();
+
+            resourceStream.CopyTo(memoryStream);
+
+            return memoryStream.ToArray();
+        }
+    }
+
+    public static bool HasValue(this string value) => !string.IsNullOrWhiteSpace(value);
+
     public static (T value, Exception exception) Try<T>(Func<T> func)
     {
         try
@@ -14,33 +48,5 @@ static class Extensions
         {
             return (default, exception);
         }
-    }
-
-    public static bool HasValue(this string value) => !string.IsNullOrWhiteSpace(value);
-
-    public static string GetSvgUrl(string svgFileName) => GetDataUriFromSvgBytes(GetEmbeddedFile($"ReactWithDotNet.Libraries.UIDesigner.Resources.{svgFileName}.svg"));
-    
-    static string GetDataUriFromSvgBytes(byte[] bytesOfSvgFile)
-    {
-        var imageBase64 = Convert.ToBase64String(bytesOfSvgFile);
-
-        return "data:image/svg+xml;base64," + imageBase64;
-    }
-
-    static byte[] GetEmbeddedFile(string resourceFilePathInAssembly)
-    {
-        var assembly = typeof(Extensions).Assembly;
-        
-        var resourceStream = assembly.GetManifestResourceStream(resourceFilePathInAssembly);
-        if (resourceStream == null)
-        {
-            return null;
-        }
-
-        using var memoryStream = new MemoryStream();
-        
-        resourceStream.CopyTo(memoryStream);
-        
-        return memoryStream.ToArray();
     }
 }
