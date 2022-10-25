1fda25cc436ceac29bcb58ed70de5066371c62f0
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-10-25 21:05:37 +0300

    .

--- QuranAnalyzer.WebUI/Infrastructure/ReactWithDotNetIntegration.cs
+++ QuranAnalyzer.WebUI/Infrastructure/ReactWithDotNetIntegration.cs
@@ -7,29 +7,58 @@ namespace QuranAnalyzer.WebUI;
 
 static class ReactWithDotNetIntegration
 {
-    const string RootFolderName = "wwwroot";
+    public static async Task HandleReactWithDotNetRequest(HttpContext context)
+    {
+        ComponentRequest componentRequest = null;
 
+        using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8, true, 1024, true))
+        {
+            componentRequest = (ComponentRequest)Json.DeserializeJsonByNewtonsoft(await reader.ReadToEndAsync(), typeof(ComponentRequest));
+        }
+
+        var response = ComponentRequestHandler.HandleRequest(componentRequest, Type.GetType);
+
+        context.Response.ContentType = "application/json";
+
+        await context.Response.WriteAsync(response.ToJson());
+    }
 
     public static async Task HomePage(HttpContext context)
     {
-        var filePath = Path.Combine(RootFolderName, "index.html");
-
-        var htmlContent = await File.ReadAllTextAsync(filePath);
-
-        htmlContent = htmlContent.Replace("~/", RootFolderName + "/");
+        var htmlContent = await GetIndexHtmlFileContent();
 
         context.Response.ContentType = "text/html; charset=UTF-8";
-        
+
         await context.Response.WriteAsync(htmlContent);
+    }
+
+    public static async Task UIDesignerComponentPreview(HttpContext context)
+    {
+        var htmlContent = await GetIndexHtmlFileContent();
+
+        htmlContent = changeComponent();
+
+        context.Response.ContentType = "text/html; charset=UTF-8";
+
+        await context.Response.WriteAsync(htmlContent);
+
+        string changeComponent()
+        {
+            return string.Join(Environment.NewLine, htmlContent.Split(Environment.NewLine).Select(line =>
+            {
+                if (line.Trim().Contains("fullTypeNameOfReactComponent"))
+                {
+                    return "fullTypeNameOfReactComponent: 'ReactWithDotNet.UIDesigner.ComponentPreivew,ReactWithDotNet.Libraries',";
+                }
+
+                return line;
+            }));
+        }
     }
 
     public static async Task UIDesignerPage(HttpContext context)
     {
-        var filePath = Path.Combine(RootFolderName, "index.html");
-
-        var htmlContent = await File.ReadAllTextAsync(filePath);
-
-        htmlContent = htmlContent.Replace("~/", RootFolderName + "/");
+        var htmlContent = await GetIndexHtmlFileContent();
 
         htmlContent = changeComponent(htmlContent);
 
@@ -59,47 +88,16 @@ static class ReactWithDotNetIntegration
         }
     }
 
-    public static async Task UIDesignerComponentPreview(HttpContext context)
+    static async Task<string> GetIndexHtmlFileContent()
     {
+        const string RootFolderName = "wwwroot";
+
         var filePath = Path.Combine(RootFolderName, "index.html");
 
         var htmlContent = await File.ReadAllTextAsync(filePath);
 
         htmlContent = htmlContent.Replace("~/", RootFolderName + "/");
 
-        htmlContent = changeComponent();
-
-        context.Response.ContentType = "text/html; charset=UTF-8";
-
-        await context.Response.WriteAsync(htmlContent);
-
-        string changeComponent()
-        {
-            return string.Join(Environment.NewLine, htmlContent.Split(Environment.NewLine).Select(line =>
-            {
-                if (line.Trim().Contains("fullTypeNameOfReactComponent"))
-                {
-                    return "fullTypeNameOfReactComponent: 'ReactWithDotNet.UIDesigner.ComponentPreivew,ReactWithDotNet.Libraries',";
-                }
-
-                return line;
-            }));
-        }
-    }
-
-    public static async Task HandleReactWithDotNetRequest(HttpContext context)
-    {
-        ComponentRequest componentRequest = null;
-
-        using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8, true, 1024, true))
-        {
-            componentRequest = (ComponentRequest)Json.DeserializeJsonByNewtonsoft(await reader.ReadToEndAsync(), typeof(ComponentRequest));
-        }
-
-        var response = ComponentRequestHandler.HandleRequest(componentRequest, Type.GetType);
-
-        context.Response.ContentType = "application/json";
-       
-        await context.Response.WriteAsync(response.ToJson());
+        return htmlContent;
     }
 }
