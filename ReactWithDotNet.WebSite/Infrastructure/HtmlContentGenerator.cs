092ff3b9265fe6ce1ad94f4cdbd727bfcd217d1f
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-10-30 00:53:46 +0300

    .

---
+++ ReactWithDotNet.WebSite/Infrastructure/HtmlContentGenerator.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+
+namespace QuranAnalyzer.WebUI;
+
+sealed class HtmlContentGenerator
+{
+    public string[] Stylesheets { get; set; }
+
+    public Type TargetReactComponent { get; set; }
+
+    public string GetHtmlContent()
+    {
+        const string root = "wwwroot";
+
+        var lines = new List<Line>
+        {
+            "<!DOCTYPE html>",
+
+            "<html lang='en' xmlns='http://www.w3.org/1999/xhtml' dir='ltr'>",
+
+            "<head>",
+            "    <meta charset='utf-8' />",
+            "    <meta name='viewport' content='width=device-width, initial-scale=1'>",
+
+            "    <meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate' />",
+            "    <meta http-equiv='Pragma' content='no-cache' />",
+            "    <meta http-equiv='Expires' content='0' />",
+
+            "    <title>React with DotNet</title>",
+
+            "    <!-- Font -->",
+            "    <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;500;700&display=swap\">",
+            "    <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Marcellus:wght@400;500;700&display=swap\">",
+
+            $"    <link rel='stylesheet' href='{root}/index.css'>",
+
+            Stylesheets,
+
+            "</head>",
+
+            "<body>",
+            "    <div id='app'>",
+            $"        <script src='{root}/index.js'></script>",
+            "    </div>",
+            "</body>",
+
+            "</html>",
+
+            "<script type='text/javascript'>",
+            "    ReactWithDotNet.RenderComponentIn({",
+            $"        fullTypeNameOfReactComponent: '{TargetReactComponent.GetFullName()}',",
+            "        containerHtmlElementId: 'app'",
+            "    });",
+            "</script>"
+        };
+
+        return lines.Aggregate(new StringBuilder(), (sb, line) => line.WriteTo(sb)).ToString();
+    }
+
+    class Line
+    {
+        string value;
+
+        string[] values;
+
+        public static implicit operator Line(string line)
+        {
+            return new Line { value = line };
+        }
+
+        public static implicit operator Line(string[] lines)
+        {
+            return new Line { values = lines };
+        }
+
+        public StringBuilder WriteTo(StringBuilder sb)
+        {
+            if (values?.Length > 0)
+            {
+                foreach (var item in values)
+                {
+                    sb.AppendLine(item);
+                }
+
+                return sb;
+            }
+
+            sb.AppendLine(value);
+
+            return sb;
+        }
+    }
+}
--- ReactWithDotNet.WebSite/Infrastructure/ReactWithDotNetIntegration.cs
+++ ReactWithDotNet.WebSite/Infrastructure/ReactWithDotNetIntegration.cs
@@ -1,97 +1,67 @@
-using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using ReactWithDotNet.UIDesigner;
 
 namespace QuranAnalyzer.WebUI;
 
 static class ReactWithDotNetIntegration
 {
-    public const string RootFolderName = "wwwroot";
-
-
-    public static async Task HomePage(HttpContext context)
+    public static void ConfigureReactWithDotNet(this IEndpointRouteBuilder endpoints)
     {
-        var filePath = Path.Combine(RootFolderName, "index.html");
-
-        var htmlContent = await File.ReadAllTextAsync(filePath);
-
-        htmlContent = htmlContent.Replace("~/", RootFolderName + "/");
-
-        context.Response.ContentType = "text/html; charset=UTF-8";
-        
-        await context.Response.WriteAsync(htmlContent);
-    }
-    
-    public static async Task UIDesignerPage(HttpContext context)
-    {
-        var filePath = Path.Combine(RootFolderName, "index.html");
-
-        var htmlContent = await File.ReadAllTextAsync(filePath);
-
-        htmlContent = htmlContent.Replace("~/", RootFolderName + "/");
-
-        htmlContent = changeComponent(htmlContent);
-
-        htmlContent = enablePrimeReactCssList(htmlContent);
-
-        context.Response.ContentType = "text/html; charset=UTF-8";
-
-        await context.Response.WriteAsync(htmlContent);
-
-        static string changeComponent(string htmlContent)
-        {
-            return string.Join(Environment.NewLine, htmlContent.Split(Environment.NewLine).Select(line =>
-            {
-                if (line.Trim().Contains("fullTypeNameOfReactComponent"))
-                {
-                    return "fullTypeNameOfReactComponent: 'ReactWithDotNet.UIDesigner.UIDesignerView,ReactWithDotNet.Libraries',";
-                }
-
-                return line;
-            }));
-        }
-
-        static string enablePrimeReactCssList(string htmlContent)
-        {
-            return htmlContent.Replace("<!--ReactWithDotNet-UIDesigner", string.Empty)
-                              .Replace("ReactWithDotNet-UIDesigner-->", string.Empty);
-        }
+        endpoints.MapGet("/", HomePage);
+        endpoints.MapPost($"/{nameof(HandleReactWithDotNetRequest)}", HandleReactWithDotNetRequest);
+        endpoints.MapGet($"/{nameof(ReactWithDotNetDesigner)}", ReactWithDotNetDesigner);
+        endpoints.MapGet($"/{nameof(ReactWithDotNetDesignerComponentPreview)}", ReactWithDotNetDesignerComponentPreview);
     }
 
-    public static async Task UIDesignerComponentPreview(HttpContext context)
-    {
-        var filePath = Path.Combine(RootFolderName, "index.html");
-
-        var htmlContent = await File.ReadAllTextAsync(filePath);
-
-        htmlContent = htmlContent.Replace("~/", RootFolderName + "/");
-
-        htmlContent = changeComponent();
-
-        context.Response.ContentType = "text/html; charset=UTF-8";
-        
-        await context.Response.WriteAsync(htmlContent);
-
-        string changeComponent()
-        {
-            return string.Join(Environment.NewLine, htmlContent.Split(Environment.NewLine).Select(line =>
-            {
-                if (line.Trim().Contains("fullTypeNameOfReactComponent"))
-                {
-                    return "fullTypeNameOfReactComponent: 'ReactWithDotNet.UIDesigner.ComponentPreivew,ReactWithDotNet.Libraries',";
-                }
-
-                return line;
-            }));
-        }
-    }
-
-    public static async Task HandleReactWithDotNetRequest(HttpContext context)
+    static async Task HandleReactWithDotNetRequest(HttpContext context)
     {
         var input = new ProcessReactWithDotNetRequestInput
         {
             HttpContext = context
         };
         await ReactWithDotNetRequestProcessor.ProcessReactWithDotNetRequest(input);
+    }
+
+    static async Task HomePage(HttpContext context)
+    {
+        await context.WriteHtmlResponse(new HtmlContentGenerator
+        {
+            TargetReactComponent = typeof(ReactWithDotNet.WebSite.MainWindow)
+        });
+    }
+
+    static async Task ReactWithDotNetDesigner(HttpContext context)
+    {
+        await context.WriteHtmlResponse(new HtmlContentGenerator
+        {
+            TargetReactComponent = typeof(ReactWithDotNetDesigner),
+
+            Stylesheets = new[]
+            {
+                "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/primereact@8.2.0/resources/themes/saga-blue/theme.css'>",
+                "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/primereact@8.2.0/resources/primereact.min.css'>",
+                "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/primeicons@5.0.0/primeicons.css'>"
+            }
+        });
+    }
+
+    static async Task ReactWithDotNetDesignerComponentPreview(HttpContext context)
+    {
+        await context.WriteHtmlResponse(new HtmlContentGenerator
+        {
+            TargetReactComponent = typeof(ReactWithDotNetDesignerComponentPreview)
+        });
+    }
+
+    static async Task WriteHtmlResponse(this HttpContext context, HtmlContentGenerator htmlContentGenerator)
+    {
+        context.Response.ContentType = "text/html; charset=UTF-8";
+
+        var htmlContent = htmlContentGenerator.GetHtmlContent();
+
+        await context.Response.WriteAsync(htmlContent);
     }
 }
