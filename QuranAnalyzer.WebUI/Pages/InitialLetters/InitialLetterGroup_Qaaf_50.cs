a1e88249acc209225e19df33fd9b66b35c751e57
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-04 00:01:46 +0300

    .

--- QuranAnalyzer.WebUI/Pages/InitialLetters/InitialLetterGroup_Qaaf_50.cs
+++ QuranAnalyzer.WebUI/Pages/InitialLetters/InitialLetterGroup_Qaaf_50.cs
@@ -42,7 +42,7 @@ class InitialLetterGroup_Qaaf_50 : InitialLetterGroup
                                 }
                             }
                         }
-                    },
+                    }
                 }
             },
 
