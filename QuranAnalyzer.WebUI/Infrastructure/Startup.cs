857774a0b5f6451f18d8c82d161a2aa7d73094e5
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-03 21:25:51 +0300

    .

--- QuranAnalyzer.WebUI/Infrastructure/Startup.cs
+++ QuranAnalyzer.WebUI/Infrastructure/Startup.cs
@@ -1,7 +1,9 @@
 using System.IO.Compression;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,31 +30,55 @@ public class Startup
 
         app.UseRouting();
 
-        app.UseStaticFiles("/wwwroot");
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            RequestPath         = new PathString("/wwwroot"),
+            ContentTypeProvider = new Utf8CharsetContentTypeProvider()
+        });
 
         app.UseResponseCompression();
 
         app.UseEndpoints(endpoints =>
         {
             endpoints.ConfigureReactWithDotNet();
-            
+
             endpoints.MapControllers();
         });
     }
 
     public void ConfigureServices(IServiceCollection services)
     {
-        services.Configure<GzipCompressionProviderOptions>(options =>
-        {
-            options.Level = CompressionLevel.Optimal;
-        });
+        services.Configure<GzipCompressionProviderOptions>(options => { options.Level = CompressionLevel.Optimal; });
         services.AddResponseCompression(options =>
         {
             options.EnableForHttps = true;
             options.Providers.Add<GzipCompressionProvider>();
         });
 
-        services.AddControllers()
-                .AddJsonOptions(jsonOptions => { jsonOptions.JsonSerializerOptions.ModifyForReactWithDotNet(); });
+        services.AddControllers().AddJsonOptions(jsonOptions => { jsonOptions.JsonSerializerOptions.ModifyForReactWithDotNet(); });
+    }
+
+    class Utf8CharsetContentTypeProvider : IContentTypeProvider
+    {
+        readonly IContentTypeProvider _defaultProvider = new FileExtensionContentTypeProvider();
+
+        public bool TryGetContentType(string subpath, out string contentType)
+        {
+            subpath = subpath.ToLower();
+
+            if (subpath.EndsWith(".js"))
+            {
+                contentType = "application/javascript; charset=utf-8";
+                return true;
+            }
+
+            if (subpath.EndsWith(".css"))
+            {
+                contentType = "text/css; charset=utf-8";
+                return true;
+            }
+
+            return _defaultProvider.TryGetContentType(subpath, out contentType);
+        }
     }
 }
