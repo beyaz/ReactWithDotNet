e4690be72cea0f7a0a1e61a08c2c26c1c006dd5e
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-03 22:04:18 +0300

    .

--- QuranAnalyzer.WebUI/Pages/MainPage/MainPageContent.cs
+++ QuranAnalyzer.WebUI/Pages/MainPage/MainPageContent.cs
@@ -6,18 +6,57 @@ class MainPageContent : ReactComponent
 {
     protected override Element render()
     {
-        return new div
+        return new Article
         {
             new LargeTitle("Bu sitede ne var?"),
-            @"
-Bir kaç yıl önce Kuran hakkında 19 Sistemi - 19 Mucizesi benzeri isimlerle duyduğum bir konu üzerine vakit buldukça araştırma yapma fırsatım oldu.
-Elimden geldiğince aklımın yettiği ölçüde nedir ne değildir inceledim.
-Bu konu etrafında doğru yanlış bir çok şey duydum.
-Konuyu kendi bizzat incelemek ve konu etrafında dönen doğru yanlış şeylere kendimce verdiğim cevapları paylaşmak istedim.
+
+            new p
+            {
+                @"Bir kaç yıl önce Kuran hakkında 19 Sistemi - 19 Mucizesi benzeri isimlerle duyduğum bir konu üzerine vakit buldukça araştırma yapma fırsatım oldu.",
+                new br(),
+
+                @"Elimden geldiğince aklımın yettiği ölçüde nedir ne değildir inceledim.
+Bu konu etrafında doğru yanlış bir çok şey duydum ve okudum.
+Konuyu kendim bizzat incelemek ve konu etrafında dönen doğru yanlış şeylere kendimce verdiğim cevapları paylaşmak istedim.
 Böylelikle konuyu araştıran kişiler için tarafsız bir gözlem sunmak niyetindeyim.
-Site şu 3 ana konuyu ele alır.
-Lütfen konunun anlaşılması için soldaki menüyü sırası ile takip ediniz.
 "
+            },
+
+            (p)"Bu siteyi hazırlarken aşağıdaki konulara dikkat ettim;",
+
+            new ul
+            {
+                new li
+                {
+                    new p
+                    {
+                        "Özellikle Kuran'dan bir ayetin Türkçe mealini paylaşmadım. ",
+                        "Çünkü insanlar genelde önce kendi fikirlerini peşinen doğru kabul ediyorlar ardından Kuran ayetlerine de o bakış açısı ile mana veriyorlar. ",
+                        "Farkında olmadan Kuranı kendi fikirlerine uyduruyorlar."
+                    },
+                },
+
+                new li
+                {
+                    new p
+                    {
+                        "Her ne kadar düşünen insan sayısı az olsa da düşünebilen her insanın bir beyni olduğuna inanıyorum. ",
+                        "Bu sebeple doğru şudur veya şu yanlıştır gibi ifadelerden uzak durmaya çalıştım. ",
+                        "Konu etrafında dönen zıt fikirleri elimden geldiğince tarafsız bir biçimde aktarıp karar verme yorum yapma işini ise elimden geldiğince okuyucuya bırakmaya çalıştım.",
+                    }
+                },
+
+                new li
+                {
+                    new p
+                    {
+                        "Bu sitede bazı matematiksel veriler bulunmaktadır. ",
+                        "Bu veriler gerçek zamanlı olarak çalışmaktadır. Giriş değerlerini değiştirdiğinizde sonuçlar da otomatik olarak değişecektir."
+                    }
+                }
+            },
+
+            (p)"Lütfen konunun anlaşılması için soldaki menüyü sırası ile takip ediniz."
         };
     }
 }
