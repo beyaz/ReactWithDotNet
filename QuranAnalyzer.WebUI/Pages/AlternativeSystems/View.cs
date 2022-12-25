aefe0477ef2965052d2def9694b994d71ee0ff42
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-25 19:41:44 +0300

    .

--- QuranAnalyzer.WebUI/Pages/AlternativeSystems/View.cs
+++ QuranAnalyzer.WebUI/Pages/AlternativeSystems/View.cs
@@ -11,7 +11,7 @@ public class AlternativeSystemsView : ReactComponent
             new LargeTitle("Alternatif 19 Sistemleri"),
             new VSpace(15),
 
-            new p{@"
+            new p{ @"
 Kuran üzerinde 19 ile ilgili bilgiler duyulmaya başladıkça başka insanlar da 19 üzerine çalışma yapmaya başladılar.
 Bu kişiler de günümüz bilgisayar yazılımlarını kullanarak bazı sayımlar yapıyorlar ve 19'un katı olan bazı sayılar elde ediyorlar.
 Duruma göre bu kişilerin çalışmalarını da inceleyeilirsiniz.
@@ -20,21 +20,25 @@ Bu isimlerin ortaya attıkları iddaları incelerken muhtemelen ilk duyacağın�
 'Uyduruk 19 sistemi', 'Sahte 19 sistemi' vb kelimler söyledikten sonra 19 ile ilgili kendi buldukları verilerin gerçek 19 sistemi olduğunu ileri sürüyorlar.
 Bu kişilerin ortaya attıkları iddalara karşı cevaplar da var onları da dinlemenizi öneririm.
 Mesela ikiz kod(7-19) sistemi diye bir sistem daha var olduğunu idda edildi.
-Youtube ' da bu konu ile ilgili yapılan eleştiri videosunu incelemenizi tavsiye ederim.
+Youtube ' da bu konu ile ilgili yapılan eleştiri videosunu incelemenizi tavsiye ederim." },
 
-https://www.youtube.com/watch?v=ZBweugCUuyk
+new a{ href = "https://www.youtube.com/watch?v=ZBweugCUuyk", text = "Gürkan Engin İkiz Kod 7 & 19"},
 
-Hatta sinema filmlerinin bir sahnesinde geçen bir arabaların plaka nolarını toplayıp çarparak Kurandan 19 ve 238 sayıları ile ilgili veriler bulan bile var.
+  new p{ @"Hatta sinema filmlerinin bir sahnesinde geçen bir arabaların plaka nolarını toplayıp çarparak Kurandan 19 ve 238 sayıları ile ilgili veriler bulan bile var.
+Şaka değil konuyu araştırırken gerçekten böyle videolara denk geldim."},
 
-Bu saydığım isimlerin ortak takıldıkları şey Reşad Halife'nin elçilik meselesi ve Tevbe 128-129 ile ilgili tartışılan konulardır.
-
-Elbetteki madalyonun diğer yüzünde 19 olayını daha tarafsız inceleyen Gürkan Engin, Berker Yörgüç gibi isimler de var.
+  new p{@"Bu saydığım isimlerin ortak takıldıkları şey Reşad Halife'nin elçilik meselesi ve Tevbe 128-129 ile ilgili tartışılan iki konudur." },
+  
+  new p{ @"Elbetteki madalyonun diğer yüzünde 19 olayını daha tarafsız inceleyen Gürkan Engin, KuranMucizeler.com gibi isimler de var.
 
 Bu saydığım isimler sadece Türkiye'de olan resim. Elbette başka ülkelerde de buna benzer ayrışmalar var.
-
-Not: Bu sitede sadece Reşad Halife tarafından keşfedilen başlangıç harfleri üzerindeki verileri içermektedir.
-
-"}
+" },
+  
+  new p
+  {
+      @"Not: Bu sitede sadece Reşad Halife tarafından keşfedilen başlangıç harfleri üzerindeki verileri incelemektedir. 
+      Dilerseniz bu sitede kullanılan sayım programlarını kullanabilir ve alternatif 19 iddalarını inceleyebilirsiniz."
+  }
         };
     }
 }
