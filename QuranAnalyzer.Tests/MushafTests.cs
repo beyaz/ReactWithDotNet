f1a6a48166791296e71611b6d98853da985a729e
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-01-04 19:13:23 +0300

    .

--- QuranAnalyzer.Tests/MushafTests.cs
+++ QuranAnalyzer.Tests/MushafTests.cs
@@ -1,12 +1,34 @@
-﻿namespace QuranAnalyzer;
+﻿using System.Net;
+using System.Net.Http;
+
+namespace QuranAnalyzer;
 
 [TestClass]
-[Ignore]
+ [Ignore]
 public class MushafTests
 {
+    class Data
+    {
+        public string verse_text_arabic { get; set; }
+    }
+    class Response
+    {
+        public bool success { get; set; }
+        public Data[] data { get; set; }
+    }
+    
     [TestMethod]
     public void AnalyzeVerseTest()
     {
-        
+        var a = GetArabicText(2, 3);
+        var response = Newtonsoft.Json.JsonConvert.DeserializeObject<Response>(a);
+        var text = response.data[0].verse_text_arabic;
+    }
+
+    static string GetArabicText(int chapter, int verseNumber)
+    {
+        var webClient = new WebClient();
+
+        return webClient.DownloadString($"https://api.quraniclabs.com/quran/{chapter}/{verseNumber}-200");
     }
 }
