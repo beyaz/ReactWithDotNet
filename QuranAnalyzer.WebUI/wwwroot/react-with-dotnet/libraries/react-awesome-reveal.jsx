7486b45075186b5679f4f4e5d3ebc6830450e5ca
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-01-09 23:02:33 +0300

    .

--- QuranAnalyzer.WebUI/wwwroot/react-with-dotnet/libraries/react-awesome-reveal.jsx
+++ QuranAnalyzer.WebUI/wwwroot/react-with-dotnet/libraries/react-awesome-reveal.jsx
@@ -1,6 +1,15 @@
 
 import ReactWithDotNet from "../react-with-dotnet";
 
-import { Fade  } from 'react-awesome-reveal';
+import { AttentionSeeker, Bounce, Flip, Fade, Hinge, JackInTheBox, Rotate, Slide, Zoom } from 'react-awesome-reveal';
 
+ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.AttentionSeeker", AttentionSeeker);
+ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.Bounce", Bounce);
+ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.Flip", Flip);
 ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.Fade", Fade);
+ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.Hinge", Hinge);
+ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.JackInTheBox", JackInTheBox);
+ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.Rotate", Rotate);
+ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.Slide", Slide);
+ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_awesome_reveal.Zoom", Zoom);
+
--- ReactWithDotNet.Libraries/react_awesome_reveal/Fade.cs
+++ ReactWithDotNet.Libraries/react_awesome_reveal/Fade.cs
@@ -1,81 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ReactWithDotNet.Libraries.react_awesome_reveal;
-
+﻿namespace ReactWithDotNet.Libraries.react_awesome_reveal;
 
 public class RevealBase : ThirdPartyReactComponent
 {
     /// <summary>
-    /// <br/>Stagger its children animations.
-    /// @default false
-    ///    </summary>
+    ///     <br />Stagger its children animations.
+    ///     @default false
+    /// </summary>
     [React]
     public bool? cascade { get; set; }
 
-    
     /// <summary>
-    /// <br/> Factor that affects the delay that each animated element in a cascade animation will be assigned.
-    /// <br/>  @default 0.5
-    /// </summary>
-    [React]
-    public double? damping { get; set; }
-
-    /// <summary>
-    /// <br/> Initial delay, in milliseconds.
-    /// <br/> @default 0
-    /// </summary>
-    [React]
-    public double? delay { get; set; }
-
-    /// <summary>
-    /// <br/> Animation duration, in milliseconds.
-    /// <br/> @default 1000
-    /// </summary>
-    [React]
-    public double? duration { get; set; }
-
-    /// <summary>
-    /// <br/> Float number between 0 and 1 indicating how much the element should be in viewport before the animation is triggered.
-    /// <br/> @default 0
-    /// </summary>
-    [React]
-    public double? fraction { get; set; }
-
-    /// <summary>
-    /// <br/> Specifies if the animation should run only once or everytime the element enters/exits/re-enters the viewport.
-    /// <br/> @default false
-    /// </summary>
-    [React]
-    public bool? triggerOnce { get; set; }
-
-    /// <summary>
-    /// <br/> Class names to add to the container element.
-    /// </summary>
-    [React]
-    public string className { get; set; }
-
-    /// <summary>
-    /// <br/> Class names to add to the child element.
+    ///     <br /> Class names to add to the child element.
     /// </summary>
     [React]
     public string childClassName { get; set; }
 
     /// <summary>
-    /// <br/> Inline styles to add to the child element.
+    ///     <br /> Inline styles to add to the child element.
     /// </summary>
     [React]
     public Style childStyle { get; set; }
+
+    /// <summary>
+    ///     <br /> Class names to add to the container element.
+    /// </summary>
+    [React]
+    public string className { get; set; }
+
+    /// <summary>
+    ///     <br /> Factor that affects the delay that each animated element in a cascade animation will be assigned.
+    ///     <br />  @default 0.5
+    /// </summary>
+    [React]
+    public double? damping { get; set; }
+
+    /// <summary>
+    ///     <br /> Initial delay, in milliseconds.
+    ///     <br /> @default 0
+    /// </summary>
+    [React]
+    public double? delay { get; set; }
+
+    /// <summary>
+    ///     <br /> Animation duration, in milliseconds.
+    ///     <br /> @default 1000
+    /// </summary>
+    [React]
+    public double? duration { get; set; }
+
+    /// <summary>
+    ///     <br /> Float number between 0 and 1 indicating how much the element should be in viewport before the animation is
+    ///     triggered.
+    ///     <br /> @default 0
+    /// </summary>
+    [React]
+    public double? fraction { get; set; }
+
+    /// <summary>
+    ///     <br /> Specifies if the animation should run only once or everytime the element enters/exits/re-enters the
+    ///     viewport.
+    ///     <br /> @default false
+    /// </summary>
+    [React]
+    public bool? triggerOnce { get; set; }
 }
 
 public class AttentionSeeker : RevealBase
 {
     /// <summary>
-    /// The animation effect to use for this attention seeker.
-    /// <br/>
-    /// <br/>"bounce" | "flash" | "headShake" | "heartBeat" | "jello" | "pulse" | "rubberBand" | "shake" | "shakeX" | "shakeY" | "swing" | "tada" | "wobble"
-    /// <br/>  @default "bounce"
+    ///     The animation effect to use for this attention seeker.
+    ///     <br />
+    ///     <br />"bounce" | "flash" | "headShake" | "heartBeat" | "jello" | "pulse" | "rubberBand" | "shake" | "shakeX" |
+    ///     "shakeY" | "swing" | "tada" | "wobble"
+    ///     <br />  @default "bounce"
     /// </summary>
     [React]
     public string effect { get; set; }
@@ -84,16 +81,16 @@ public class AttentionSeeker : RevealBase
 public class Bounce : RevealBase
 {
     /// <summary>
-    /// <br/> Origin of the animation.
-    /// <br/>"down" | "left" | "right" | "up"
-    /// <br/>  @default undefined
+    ///     <br /> Origin of the animation.
+    ///     <br />"down" | "left" | "right" | "up"
+    ///     <br />  @default undefined
     /// </summary>
     [React]
     public string direction { get; set; }
 
     /// <summary>
-    /// <br/>Specifies if the animation should make element(s) disappear.
-    /// <br/>@default false
+    ///     <br />Specifies if the animation should make element(s) disappear.
+    ///     <br />@default false
     /// </summary>
     [React]
     public bool? reverse { get; set; }
@@ -102,17 +99,97 @@ public class Bounce : RevealBase
 public class Fade : RevealBase
 {
     /// <summary>
-    /// <br/> Causes the animation to start farther. Only works with "down", "left", "right" and "up" directions.
-    /// <br/> @default false
+    ///     <br /> Causes the animation to start farther. Only works with "down", "left", "right" and "up" directions.
+    ///     <br /> @default false
     /// </summary>
     [React]
     public bool? big { get; set; }
 
     /// <summary>
-    /// <br/> Origin of the animation.
-    /// <br/> @default undefined
-    /// <br/> "bottom-left" | "bottom-right" | "down" | "left" | "right" | "top-left" | "top-right" | "up"
+    ///     <br /> Origin of the animation.
+    ///     <br /> @default undefined
+    ///     <br /> "bottom-left" | "bottom-right" | "down" | "left" | "right" | "top-left" | "top-right" | "up"
     /// </summary>
     [React]
     public string direction { get; set; }
+}
+
+public class Flip : RevealBase
+{
+    /// <summary>
+    ///     <br /> Origin of the animation.
+    ///     <br /> @default undefined
+    ///     <br /> "horizontal" | "vertical"
+    /// </summary>
+    [React]
+    public string direction { get; set; }
+
+    /// <summary>
+    ///     Specifies if the animation should make element(s) disappear. It only has effect if a direction is specified.<br />
+    ///     @default false
+    /// </summary>
+    [React]
+    public bool? reverse { get; set; }
+}
+
+public class Hinge : RevealBase
+{
+}
+
+public class JackInTheBox : RevealBase
+{
+}
+
+public class Rotate : RevealBase
+{
+    /// <summary>
+    ///     <br /> Origin of the animation.
+    ///     <br /> @default undefined
+    ///     <br />  "bottom-left" | "bottom-right" | "top-left" | "top-right"
+    /// </summary>
+    [React]
+    public string direction { get; set; }
+
+    /// <summary>
+    ///     Specifies if the animation should make element(s) disappear.<br />
+    ///     @default false
+    /// </summary>
+    [React]
+    public bool? reverse { get; set; }
+}
+
+public class Slide : RevealBase
+{
+    /// <summary>
+    ///     <br /> Origin of the animation.
+    ///     <br /> @default undefined
+    ///     <br />  "down" | "left" | "right" | "up"
+    /// </summary>
+    [React]
+    public string direction { get; set; }
+
+    /// <summary>
+    ///     Specifies if the animation should make element(s) disappear.<br />
+    ///     @default false
+    /// </summary>
+    [React]
+    public bool? reverse { get; set; }
+}
+
+public class Zoom : RevealBase
+{
+    /// <summary>
+    ///     <br /> Origin of the animation.
+    ///     <br /> @default undefined
+    ///     <br />  "down" | "left" | "right" | "up"
+    /// </summary>
+    [React]
+    public string direction { get; set; }
+
+    /// <summary>
+    ///     Specifies if the animation should make element(s) disappear.<br />
+    ///     @default false
+    /// </summary>
+    [React]
+    public bool? reverse { get; set; }
 }
