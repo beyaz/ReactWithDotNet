628e5bdc63d38989531feefc26dd1fb9a872ddac
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-01-17 18:41:43 +0300

    .

--- QuranAnalyzer.WebUI/Pages/AllInitialLettersCombined/TotalCounts.cs
+++ QuranAnalyzer.WebUI/Pages/AllInitialLettersCombined/TotalCounts.cs
@@ -202,4 +202,12 @@ public class InitialLetterCountInfo
 {
     public string Count { get; set; }
     public string Text { get; set; }
+    
+    public List<CountInfo> Details { get; set; }
+}
+
+public class CountInfo
+{
+    public int ChapterNumber { get; set; }
+    public string Count { get; set; }
 }
