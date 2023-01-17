41fefc1f45d3ed0bf46627f8914d2961493a6c0f
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-01-18 00:31:03 +0300

    .

--- QuranAnalyzer.WebUI/Pages/AllInitialLettersCombined/View.cs
+++ QuranAnalyzer.WebUI/Pages/AllInitialLettersCombined/View.cs
@@ -1,5 +1,4 @@
 ﻿using QuranAnalyzer.WebUI.Components;
-using ReactWithDotNet.Libraries.react_awesome_reveal;
 
 namespace QuranAnalyzer.WebUI.Pages.AllInitialLettersCombined;
 
@@ -7,51 +6,6 @@ class View : ReactComponent
 {
     protected override Element render()
     {
-         Element CreateWithCount(string arabicLetter, int count)
-         {
-             Element countView = new FlexRowCentered { count.ToString() };
-             
-             if (EnterJoInMode)
-             {
-                 countView = new Fade
-                 {
-                     reverse=true,
-                     direction = "down",
-                     children =
-                     {
-                         countView
-                     }
-                 };
-             }
-             return new FlexColumn(ComponentBorder, Gap(4))
-             {
-                 new FlexRow { AsLetter(arabicLetter) } + When(EnterJoInMode,FontSize10),
-                 countView
-             };
-         }
-
-        Element CreateWithCount2(string arabicLetter, int count)
-        {
-            Element countView = new FlexRowCentered { count.ToString() };
-
-            if (EnterJoInMode)
-            {
-                countView = new Fade
-                {
-                    direction = "down",
-                    children =
-                    {
-                        countView
-                    }
-                };
-            }
-            return new FlexColumn
-            {
-                //new FlexRow { AsLetter(arabicLetter) },
-                countView
-            };
-        }
-
         return new Article
         {
             new LargeTitle("Başlangıç harflerinin yan yana yazılması ile oluşan büyük sayılar"),
@@ -70,14 +24,8 @@ class View : ReactComponent
             raisePanel(new TotalCounts()),
             new br(),
             new br(),
-            
-            raisePanel(new TotalCountsWithDetail()),
-            
-            
-            
-            
-                
 
+            raisePanel(new TotalCountsWithDetail()),
 
             new VSpace(10),
 
@@ -94,27 +42,5 @@ class View : ReactComponent
             BoxShadow("rgb(0 0 0 / 34%) 0px 2px 5px 0px"), Padding(15), BorderRadius(5), MarginTopBottom(10),
             element
         };
-
-        static Element InFadeAnimation(Element element,int delay)
-        {
-            return new Fade
-            {
-                triggerOnce = true,
-                delay = delay,
-                children =
-                {
-                    element
-                }
-            };
-        }
-
-    }
-
-    public bool EnterJoInMode { get; set; }
-    
-    void Join()
-    {
-        EnterJoInMode = true;
-
     }
 }
