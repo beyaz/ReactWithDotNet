f86a776aaaa41e91a9212f3234e4d8e354cb474d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-01-20 19:41:13 +0300

    .

--- QuranAnalyzer.WebUI/Pages/AllInitialLettersCombined/TotalCountsWithDetail.cs
+++ QuranAnalyzer.WebUI/Pages/AllInitialLettersCombined/TotalCountsWithDetail.cs
@@ -35,6 +35,7 @@ class TotalCountsWithDetail : ReactComponent
     
     protected override Element render()
     {
+        
         var nextDelay = CreateDelayAccessMethod();
 
         return new FlexColumn(Gap(10))
