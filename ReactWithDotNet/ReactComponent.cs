3d01c0d911a6d110551804b67af702e717e24a60
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-01-22 22:12:10 +0300

    .

--- ReactWithDotNet/ReactComponent.cs
+++ ReactWithDotNet/ReactComponent.cs
@@ -1,91 +1,18 @@
-﻿using System.Collections.Specialized;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Text.Json.Serialization;
-using System.Web;
 
 namespace ReactWithDotNet;
 
-public sealed class ReactContext
-{
-    public static ReactContext Create(string url, double clientWidth, double clientHeight)
-    {
-        url= (url??string.Empty).Split('?').Last();
-        var context = new ReactContext
-        {
-            Query        = string.IsNullOrWhiteSpace(url) ? new NameValueCollection() : HttpUtility.ParseQueryString(url),
-            ClientWidth  = clientWidth,
-            ClientHeight = clientHeight
-        };
-
-        return context;
-    }
-    readonly Dictionary<string, object> map = new();
-    public double ClientHeight { get; internal set; }
-
-    public double ClientWidth { get; internal set; }
-    public NameValueCollection Query { get; internal set; } = new ();
-    public string QueryAsString => string.Join("&", Query.AllKeys.Select(key => $"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(Query[key])}"));
-
-    public bool Contains<TValue>(ReactContextKey<TValue> key)
-    {
-        return map.ContainsKey(key.Key);
-    }
-
-    public void Set<TValue>(ReactContextKey<TValue> key, TValue value)
-    {
-        Set(key.Key, value);
-    }
-
-    public void Set<TValue>(string key, TValue value)
-    {
-        if (map.ContainsKey(key))
-        {
-            map[key] = value;
-            return;
-        }
-
-        map.Add(key, value);
-    }
-
-    public TValue TryGetValue<TValue>(ReactContextKey<TValue> key)
-    {
-        if (map.TryGetValue(key.Key, out var value))
-        {
-            return (TValue)value;
-        }
-
-        return default;
-    }
-
-    public TValue TryGetValue<TValue>(string key)
-    {
-        if (map.TryGetValue(key, out var value))
-        {
-            return (TValue)value;
-        }
-
-        return default;
-    }
-}
-
-// ReSharper disable once UnusedTypeParameter
-public sealed class ReactContextKey<TValue>
-{
-    public readonly string Key;
-
-    public ReactContextKey(string key)
-    {
-        Key = key;
-    }
-
-    public TValue this[ReactContext reactContext] => reactContext.TryGetValue(this);
-}
-
 public abstract class ReactStatefulComponent : Element
 {
-
     internal Style _styleForRootElement;
+
+    internal List<IModifier> modifiers;
+
+    [JsonIgnore]
+    [Newtonsoft.Json.JsonIgnore]
+    public Client Client { get; internal set; } = new();
 
     [JsonIgnore]
     [Newtonsoft.Json.JsonIgnore]
@@ -98,20 +25,27 @@ public abstract class ReactStatefulComponent : Element
             return _styleForRootElement;
         }
     }
-    
-    internal List<IModifier> modifiers;
 
     [JsonIgnore]
     [Newtonsoft.Json.JsonIgnore]
-    public Client Client { get; internal set; } = new();
+    protected internal int? ComponentUniqueIdentifier { get; set; }
 
     [JsonIgnore]
     [Newtonsoft.Json.JsonIgnore]
     protected internal ReactContext Context { get; set; }
 
-    [JsonIgnore]
-    [Newtonsoft.Json.JsonIgnore]
-    protected internal int? ComponentUniqueIdentifier { get; set; }
+    public static ReactStatefulComponent operator +(ReactStatefulComponent component, Style style)
+    {
+        component.style.Import(style);
+
+        return component;
+    }
+
+    [SuppressMessage("ReSharper", "ParameterHidesMember")]
+    public void Add(Style style)
+    {
+        this.style.Import(style);
+    }
 
     internal object Clone()
     {
@@ -138,7 +72,7 @@ public abstract class ReactStatefulComponent : Element
     /// </summary>
     protected void DispatchEvent(Expression<Func<Action>> expressionForAccessingCustomReactEventProperty)
     {
-        Client.DispatchDotNetCustomEvent(Mixin.GetEventSenderInfo(this, GetPropertyNameOfCustomReactEvent((MemberExpression)expressionForAccessingCustomReactEventProperty.Body)));
+        Client.DispatchDotNetCustomEvent(GetEventSenderInfo(this, GetPropertyNameOfCustomReactEvent((MemberExpression)expressionForAccessingCustomReactEventProperty.Body)));
     }
 
     /// <summary>
@@ -151,7 +85,7 @@ public abstract class ReactStatefulComponent : Element
     /// </summary>
     protected void DispatchEvent<A>(Expression<Func<Action<A>>> expressionForAccessingCustomReactEventProperty, A a)
     {
-        Client.DispatchDotNetCustomEvent(Mixin.GetEventSenderInfo(this, GetPropertyNameOfCustomReactEvent((MemberExpression)expressionForAccessingCustomReactEventProperty.Body)), a);
+        Client.DispatchDotNetCustomEvent(GetEventSenderInfo(this, GetPropertyNameOfCustomReactEvent((MemberExpression)expressionForAccessingCustomReactEventProperty.Body)), a);
     }
 
     /// <summary>
@@ -164,7 +98,7 @@ public abstract class ReactStatefulComponent : Element
     /// </summary>
     protected void DispatchEvent<A, B>(Expression<Func<Action<A, B>>> expressionForAccessingCustomReactEventProperty, A a, B b)
     {
-        Client.DispatchDotNetCustomEvent(Mixin.GetEventSenderInfo(this, GetPropertyNameOfCustomReactEvent((MemberExpression)expressionForAccessingCustomReactEventProperty.Body)), a, b);
+        Client.DispatchDotNetCustomEvent(GetEventSenderInfo(this, GetPropertyNameOfCustomReactEvent((MemberExpression)expressionForAccessingCustomReactEventProperty.Body)), a, b);
     }
 
     /// <summary>
@@ -177,7 +111,7 @@ public abstract class ReactStatefulComponent : Element
     /// </summary>
     protected void DispatchEvent<A, B, C>(Expression<Func<Action<A, B>>> expressionForAccessingCustomReactEventProperty, A a, B b, C c)
     {
-        Client.DispatchDotNetCustomEvent(Mixin.GetEventSenderInfo(this, GetPropertyNameOfCustomReactEvent((MemberExpression)expressionForAccessingCustomReactEventProperty.Body)), a, b, c);
+        Client.DispatchDotNetCustomEvent(GetEventSenderInfo(this, GetPropertyNameOfCustomReactEvent((MemberExpression)expressionForAccessingCustomReactEventProperty.Body)), a, b, c);
     }
 
     protected abstract Element render();
@@ -192,19 +126,6 @@ public abstract class ReactStatefulComponent : Element
         }
 
         return propertyNameOfCustomReactEvent;
-    }
-
-    [SuppressMessage("ReSharper", "ParameterHidesMember")]
-    public void Add(Style style)
-    {
-        this.style.Import(style);
-    }
-
-    public static ReactStatefulComponent operator +(ReactStatefulComponent component, Style style)
-    {
-        component.style.Import(style);
-
-        return component;
     }
 }
 
---
+++ ReactWithDotNet/ReactContext.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Specialized;
+using System.Web;
+
+namespace ReactWithDotNet;
+
+// ReSharper disable once UnusedTypeParameter
+public sealed class ReactContextKey<TValue>
+{
+    public readonly string Key;
+
+    public ReactContextKey(string key)
+    {
+        Key = key;
+    }
+
+    public TValue this[ReactContext reactContext] => reactContext.TryGetValue(this);
+}
+
+public sealed class ReactContext
+{
+    readonly Dictionary<string, object> map = new();
+    public double ClientHeight { get; internal set; }
+
+    public double ClientWidth { get; internal set; }
+    public NameValueCollection Query { get; internal set; } = new();
+    public string QueryAsString => string.Join("&", Query.AllKeys.Select(key => $"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(Query[key])}"));
+
+    public bool Contains<TValue>(ReactContextKey<TValue> key)
+    {
+        return map.ContainsKey(key.Key);
+    }
+
+    public void Set<TValue>(ReactContextKey<TValue> key, TValue value)
+    {
+        Set(key.Key, value);
+    }
+
+    public void Set<TValue>(string key, TValue value)
+    {
+        if (map.ContainsKey(key))
+        {
+            map[key] = value;
+            return;
+        }
+
+        map.Add(key, value);
+    }
+
+    public TValue TryGetValue<TValue>(ReactContextKey<TValue> key)
+    {
+        if (map.TryGetValue(key.Key, out var value))
+        {
+            return (TValue)value;
+        }
+
+        return default;
+    }
+
+    public TValue TryGetValue<TValue>(string key)
+    {
+        if (map.TryGetValue(key, out var value))
+        {
+            return (TValue)value;
+        }
+
+        return default;
+    }
+}
