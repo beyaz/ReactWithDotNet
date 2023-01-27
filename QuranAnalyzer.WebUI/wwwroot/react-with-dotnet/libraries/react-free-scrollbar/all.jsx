a7f699de5d559bac87efcb5b514783603fdcabc7
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-01-27 17:42:32 +0300

    .

--- QuranAnalyzer.WebUI/wwwroot/react-with-dotnet/libraries/react-free-scrollbar/all.jsx
+++ QuranAnalyzer.WebUI/wwwroot/react-with-dotnet/libraries/react-free-scrollbar/all.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 import ReactWithDotNet from "../../react-with-dotnet";
 
-const FreeScrollBar = React.lazy(() => import('react-free-scrollbar'));
+const FreeScrollBar = React.lazy(() => import('./FreeScrollBar'));
 
 ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.Libraries.react_free_scrollbar.FreeScrollBar", FreeScrollBar);
