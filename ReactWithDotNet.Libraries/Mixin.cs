39e032d14066e186ceca60afac52a7a687444341
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-02-08 20:32:35 +0300

    .

---
+++ ReactWithDotNet.Libraries/Mixin.cs
@@ -0,0 +1,6 @@
+﻿namespace ReactWithDotNet.Libraries;
+
+static class Mixin
+{
+    public const string Core__CalculateSyntheticChangeEventArguments = "ReactWithDotNet::Core::CalculateSyntheticChangeEventArguments";
+}
--- ReactWithDotNet.Libraries/mui/material/Switch.cs
+++ ReactWithDotNet.Libraries/mui/material/Switch.cs
@@ -7,4 +7,20 @@ public sealed class Switch: ElementBase
 
     [React]
     public bool? disabled { get; set; }
+
+    /// <summary>
+    /// 	If true, the component is checked.
+    /// </summary>
+    [React]
+    public bool? @checked { get; set; }
+
+
+    [React]
+    [ReactGrabEventArgumentsByUsingFunction(Core__CalculateSyntheticChangeEventArguments)]
+    public Action<ChangeEvent> onChange { get; set; }
+
+    [React]
+    [ReactBind(targetProp = nameof(@checked), jsValueAccess = "e.target.value", eventName = nameof(onChange))]
+    public Expression<Func<bool>> checkedBind { get; set; }
+
 }
