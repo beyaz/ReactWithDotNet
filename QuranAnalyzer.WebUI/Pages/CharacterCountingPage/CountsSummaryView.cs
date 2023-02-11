caf0fcc5a0156f26d1822346848dbe606fa875a2
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-02-11 22:51:56 +0300

    .

--- QuranAnalyzer.WebUI/Pages/CharacterCountingPage/CountsSummaryView.cs
+++ QuranAnalyzer.WebUI/Pages/CharacterCountingPage/CountsSummaryView.cs
@@ -11,7 +11,7 @@ public class SummaryInfo
 }
 
 [Serializable]
-class CountsSummaryView : ReactComponent
+class CountsSummaryView : ReactComponent // TODO: Check pure
 {
     #region Public Properties
     public IReadOnlyList<SummaryInfo> Counts { get; set; } = new List<SummaryInfo>();
