9084e7b25d5204146acec0ec4f78bbf03fc7db9d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-02-19 18:37:08 +0300

    .

--- ReactWithDotNet.WebSite/Components/Logo.cs
+++ ReactWithDotNet.WebSite/Components/Logo.cs
@@ -188,18 +188,3 @@ class MainPageContentSample: ReactPureComponent
         };
     }
 }
-
-
-
-class MainPageFooter: ReactPureComponent
-{
-    protected override Element render()
-    {
-        return new footer
-        {
-            BoxShadow($"{Theme[Context].grey_100} -1px -1px 1px"),
-            Height(50),
-            new div("Copyright")
-        };
-    }
-}
