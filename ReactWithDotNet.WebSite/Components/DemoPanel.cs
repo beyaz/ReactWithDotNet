92a07125e3a8bc3b5bae52c54f9a721edad53636
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-02-22 00:35:00 +0300

    .

---
+++ ReactWithDotNet.WebSite/Components/DemoPanel.cs
@@ -0,0 +1,26 @@
+﻿using ReactWithDotNet.Libraries.react_syntax_highlighter;
+
+namespace ReactWithDotNet.WebSite.Components;
+
+class DemoPanel : ReactPureComponent
+{
+    public string CSharpCode { get; set; }
+
+    public Element Element { get; set; }
+
+    protected override Element render()
+    {
+        return new div(BoxShadow("rgb(0 0 0 / 34%) 0px 2px 5px 0px"), Padding(15), BorderRadius(5), MarginTopBottom(10))
+        {
+            new SyntaxHighlighter
+            {
+                language = "csharp",
+                style    = SyntaxHighlighterStyle.vs,
+                children =
+                {
+                    Code
+                }
+            }
+        };
+    }
+}
