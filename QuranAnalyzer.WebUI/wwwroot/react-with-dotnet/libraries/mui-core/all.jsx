b4e9bd40ac5f8db8ab28e87623b903ced86a3877
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-02-25 22:30:39 +0300

    .

--- QuranAnalyzer.WebUI/wwwroot/react-with-dotnet/libraries/mui-core/all.jsx
+++ QuranAnalyzer.WebUI/wwwroot/react-with-dotnet/libraries/mui-core/all.jsx
@@ -12,31 +12,20 @@ function register(name, value)
 }
 
 // Connect components as Lazy
-register("Switch", React.lazy(() => import('./Switch')));
-register("Tooltip", React.lazy(() => import('./Tooltip')));
-register("Button", React.lazy(() => import('./Button')));
-register("Input", React.lazy(() => import('./Input')));
-register("InputBase", React.lazy(() => import('./InputBase')));
-register("Paper", React.lazy(() => import('./Paper')));
-register("Divider", React.lazy(() => import('./Divider')));
-register("IconButton", React.lazy(() => import('./IconButton')));
-register("TextField", React.lazy(() => import('./TextField')));
-register("CardMedia", React.lazy(() => import('./CardMedia')));
-register("Card", React.lazy(() => import('./Card')));
+register("Switch",      React.lazy(() => import('./Switch')));
+register("Tooltip",     React.lazy(() => import('./Tooltip')));
+register("Button",      React.lazy(() => import('./Button')));
+register("Input",       React.lazy(() => import('./Input')));
+register("InputBase",   React.lazy(() => import('./InputBase')));
+register("Paper",       React.lazy(() => import('./Paper')));
+register("Divider",     React.lazy(() => import('./Divider')));
+register("IconButton",  React.lazy(() => import('./IconButton')));
+register("TextField",   React.lazy(() => import('./TextField')));
+register("CardMedia",   React.lazy(() => import('./CardMedia')));
+register("Card",        React.lazy(() => import('./Card')));
 register("CardContent", React.lazy(() => import('./CardContent')));
 register("CardActions", React.lazy(() => import('./CardActions')));
-register("Typography", React.lazy(() => import('./Typography')));
+register("Typography",  React.lazy(() => import('./Typography')));
 
-
-
-
- 
- 
- 
- 
- 
-
-
-
-register("FormGroup", FormGroup);
+register("FormGroup",        FormGroup);
 register("FormControlLabel", FormControlLabel);
