e0ccfbc4365c9f7aca7438e748976412bd3ce532
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-02-25 20:34:01 +0300

    .

--- ReactWithDotNet/ElementSerializer.cs
+++ ReactWithDotNet/ElementSerializer.cs
@@ -475,7 +475,13 @@ static partial class ElementSerializer
 
     static ValueExportInfo<object> GetStylePropertyValueOfHtmlElementForSerialize(object instance, Style style, ElementSerializerContext context)
     {
-        var response = ConvertStyleToCssClass(style, false, context.componentStack.Peek()?.ComponentUniqueIdentifier, context.DynamicStyles.GetClassName);
+        var componentUniqueIdentifier = 0;
+        
+        if (context.componentStack.Count > 0)
+        {
+            componentUniqueIdentifier = context.componentStack.Peek()?.ComponentUniqueIdentifier ?? 0;
+        }
+        var response = ConvertStyleToCssClass(style, false, componentUniqueIdentifier, context.DynamicStyles.GetClassName);
         if (response.needToExport  is false)
         {
             if (style.IsEmpty == false)
