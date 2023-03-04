8d1d2d59cf7ff6f1e00a75f08102df23917337d5
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-03-04 17:12:13 +0300

    .

---
+++ QuranAnalyzer.Tests/AnalyzerTest.cs
@@ -0,0 +1,14 @@
+﻿namespace QuranAnalyzer;
+
+[TestClass]
+public class AnalyzerTest
+{
+    [TestMethod]
+    public void _1_()
+    {
+
+        Analyzer.AnalyzeText("يَٰصَىٰحِبَىِ")
+                .Count(x => x.ArabicLetterIndex == ArabicLetterIndex.Yaa)
+                .Should().Be(3);
+    }
+}
