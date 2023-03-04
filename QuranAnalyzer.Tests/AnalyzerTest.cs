3e4320d6e4c9593c7c4b6ad71cb76eff317b4e80
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-03-04 17:21:26 +0300

    .

--- QuranAnalyzer.Tests/AnalyzerTest.cs
+++ QuranAnalyzer.Tests/AnalyzerTest.cs
@@ -10,5 +10,10 @@ public class AnalyzerTest
         Analyzer.AnalyzeText("يَٰصَىٰحِبَىِ")
                 .Count(x => x.ArabicLetterIndex == ArabicLetterIndex.Yaa)
                 .Should().Be(3);
+
+
+        Analyzer.AnalyzeText("يَٰصَىٰحِبَىِ ٱلسِّجْنِ أَمَّآ أَحَدُكُمَا فَيَسْقِى رَبَّهُۥ خَمْرًا وَأَمَّا ٱلْءَاخَرُ فَيُصْلَبُ فَتَأْكُلُ ٱلطَّيْرُ مِن رَّأْسِهِۦ قُضِىَ ٱلْأَمْرُ ٱلَّذِى فِيهِ تَسْتَفْتِيَانِ")
+                .Count(x => x.ArabicLetterIndex == ArabicLetterIndex.Yaa)
+                .Should().Be(11);
     }
 }
