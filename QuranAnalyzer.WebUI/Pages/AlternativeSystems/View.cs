0c0386ed4a872dc4ff6828e7b1c1e6e43abe9431
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-03-05 11:20:32 +0300

    .

--- QuranAnalyzer.WebUI/Pages/AlternativeSystems/View.cs
+++ QuranAnalyzer.WebUI/Pages/AlternativeSystems/View.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuranAnalyzer.WebUI.Pages.AlternativeSystems;
 
-public class AlternativeSystemsView : ReactComponent
+public class AlternativeSystemsView : ReactPureComponent
 {
     protected override Element render()
     {
@@ -11,14 +11,12 @@ public class AlternativeSystemsView : ReactComponent
             new p
             {
                 @"
-Kuran üzerinde 19 ile ilgili bilgiler duyulmaya başladıkça başka insanlar da 19 üzerine çalışma yapmaya başladılar.
-Bu kişiler de günümüz bilgisayar yazılımlarını kullanarak bazı sayımlar yapıyorlar ve 19'un katı olan bazı sayılar elde ediyorlar.
-Duruma göre bu kişilerin çalışmalarını da inceleyeilirsiniz.
-İmran Akdemir, Gökmen Altay İki Sayının Sahibi gibi  isimler de çalışma yapıyorlar.
-Bu isimlerin ortaya attıkları iddaları incelerken muhtemelen ilk duyacağınız cümle şu olacaktır.
-'Uyduruk 19 sistemi', 'Sahte 19 sistemi' vb kelimler söyledikten sonra 19 ile ilgili kendi buldukları verilerin gerçek 19 sistemi olduğunu ileri sürüyorlar.
-Bu kişilerin ortaya attıkları iddalara karşı cevaplar da var onları da dinlemenizi öneririm.
-Mesela ikiz kod(7-19) sistemi diye bir sistem daha var olduğunu idda edildi.
+Kuran üzerinde 19 ile ilgili bilgiler duyulmaya başladıkça başka insanlar da 19 üzerine çalışma yapmaya başladılar. 
+Bu kişiler de günümüz bilgisayar yazılımlarını kullanarak bazı sayımlar yapıyorlar ve 19'un katı olan bazı sayılar elde ediyorlar. 
+Duruma göre bu kişilerin çalışmalarını da inceleyebilirsiniz. İmran Akdemir, Gökmen Altay, İki Sayının Sahibi gibi isimler de çalışma yapıyorlar. 
+Bu isimlerin ortaya attıkları iddiaları incelerken muhtemelen ilk duyacağınız cümle şu olacaktır. 'Uyduruk 19 sistemi', 'Sahte 19 sistemi' benzeri kelimler söyledikten sonra 19 ile ilgili kendi buldukları verilerin gerçek 19 sistemi olduğunu ileri sürüyorlar. 
+Bu kişilerin ortaya attıkları iddialara karşı cevaplar da var onları da dinlemenizi öneririm. 
+Mesela ikiz kod(7-19) sistemi diye bir sistem daha var olduğunu iddia edildi. 
 Youtube ' da bu konu ile ilgili yapılan eleştiri videosunu incelemenizi tavsiye ederim."
             },
 
@@ -30,14 +28,14 @@ Youtube ' da bu konu ile ilgili yapılan eleştiri videosunu incelemenizi tavsiy
 Şaka değil konuyu araştırırken gerçekten böyle videolara denk geldim. 
 Hatta Mustafa Kemal Atatürk'ün hayatındaki olan olayların tarihleri ile  19 rakamı arasında ilişkiler kurmaya çalışanlara bile denk gelebilirsiniz.
 Özetle bu konuyu araştırırken o da 19'un katı bu da 19 un katı şeklinde bir çok videoya - yazıya denk gelebilirsiniz.
-Tahminim o ki burada şeytanın bir oyunu olabilir. 19 meselesini sulandırarak, alakasız konularla ilişkilendirerek 19 gibi basit anlaşılır bir meseleyi anlaşılmaz işin içindne çıkılmaz ve şüpheli bir hale getirmeye çalışıyor olabilir."
+Tahminim o ki burada şeytanın bir oyunu olabilir. 19 meselesini sulandırarak, alakasız konularla ilişkilendirerek 19 gibi basit anlaşılır bir meseleyi anlaşılmaz işin içinden çıkılmaz ve şüpheli bir hale getirmeye çalışıyor olabilir."
             },
 
             new p { @"Bu saydığım isimlerin ortak takıldıkları şey Reşad Halife'nin elçilik meselesi ve Tevbe 128-129 ile ilgili tartışılan iki konudur." },
 
             new p
             {
-                @"Elbetteki madalyonun diğer yüzünde 19 olayını daha tarafsız inceleyen KuranMucizeler.com, Gürkan Engin gibi isimler de var.
+                @"Elbette madalyonun diğer yüzünde 19 olayını daha tarafsız inceleyen KuranMucizeler.com, Gürkan Engin gibi isimler de var.
 
 Bu saydığım isimler sadece Türkiye'de olan resim. Elbette başka ülkelerde de buna benzer ayrışmalar var.
 "
@@ -46,7 +44,7 @@ Bu saydığım isimler sadece Türkiye'de olan resim. Elbette başka ülkelerde 
             new p
             {
                 (b)"Not: ", "Bu sitede sadece Reşad Halife tarafından keşfedilen 19 sisteminin ", (b)"en majör noktası", @" olan başlangıç harfleri üzerindeki veriler incelenmiştir. 
-      Dilerseniz bu sitede kullanılan sayım programlarını kullanabilir ve alternatif iddaları inceleyebilirsiniz."
+      Dilerseniz bu sitede kullanılan sayım programlarını kullanabilir ve alternatif iddiaları inceleyebilirsiniz."
             }
         };
     }
