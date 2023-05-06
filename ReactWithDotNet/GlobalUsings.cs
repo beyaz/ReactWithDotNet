37926da2b9b27385378f33d0255acaf590418ee7
beyaz <beyaz1404@gmail.com>
2023-05-07 01:01:50 +0300

    .

--- ReactWithDotNet/GlobalUsings.cs
+++ ReactWithDotNet/GlobalUsings.cs
@@ -1,13 +1,4 @@
-﻿global using System;
-global using System.Linq.Expressions;
+﻿global using System.Linq.Expressions;
 global using static ReactWithDotNet.Extensions;
 global using static ReactWithDotNet.Mixin;
 
-// fix visual studio bug
-// https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit
-    {
-    }
-}
