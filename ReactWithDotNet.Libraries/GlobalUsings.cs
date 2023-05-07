c9bfce163720ee6e340aba0025e28f4567aabc8d
beyaz <beyaz1404@gmail.com>
2023-05-07 08:54:46 +0300

    .

--- ReactWithDotNet.Libraries/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using System;
-global using System.Linq.Expressions;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Dynamic;
-global using static ReactWithDotNet.Mixin;
-global using static ReactWithDotNet.Libraries.Mixin;
--- ReactWithDotNet.Libraries/Mixin.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ReactWithDotNet.Libraries;
-
-static class Mixin
-{
-    public const string Core__CalculateSyntheticChangeEventArguments = "ReactWithDotNet::Core::CalculateSyntheticChangeEventArguments";
-    public const string Core__ReplaceNullWhenEmpty = "ReactWithDotNet::Core::ReplaceNullWhenEmpty";
-}
