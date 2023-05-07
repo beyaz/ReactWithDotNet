331ec53dd0ec8052c968b76572dbf7994ca1fc2f
beyaz <beyaz1404@gmail.com>
2023-05-07 15:12:06 +0300

    .

--- ReactWithDotNet.WebSite/Components/YoutubeCard.cs
+++ ReactWithDotNet.WebSite/Components/YoutubeCard.cs
@@ -8,12 +8,17 @@ class YoutubeCard : ReactPureComponent
 
     protected override Element render()
     {
-        return new a(DisplayFlexRow, Background("white"), BorderRadius(5), BoxShadow("0 0 1px rgba(0,0,0,.1),0 2px 4px rgba(0,0,0,.02)"), Hover(BoxShadow(0, 0, 20,0, rgba(0, 0, 0, 0.12))))
+        return new a(DisplayFlexRow, Background("white"), BorderRadius(5))
         {
             Href($"https://www.youtube.com/watch?v={Model.YoutubeVideoId}"),
             TextDecorationNone,
             Color("black"),
+
+            BoxShadow(-3, -3, 5,0, rgba(0, 0, 0, 0.12)),
+            Hover(BoxShadow(0, 0, 20,0, rgba(0, 0, 0, 0.12))),
             
+
+
             new img { BorderTopLeftRadius(5), BorderBottomLeftRadius(5), Src($"https://i.ytimg.com/vi_webp/{Model.YoutubeVideoId}/maxresdefault.webp"), Alt(Model.Title), Width(200), Height(160) },
             new FlexColumn(PaddingLeftRight(30))
             {
