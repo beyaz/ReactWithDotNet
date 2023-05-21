8debfff00821d17f896bf19c8b32d2fd72cdcc06
beyaz <beyaz1404@gmail.com>
2023-05-22 01:01:53 +0300

    .

--- ReactWithDotNet.WebSite/wwwroot/react-with-dotnet/libraries/swiper/Swiper.jsx
+++ ReactWithDotNet.WebSite/wwwroot/react-with-dotnet/libraries/swiper/Swiper.jsx
@@ -63,7 +63,7 @@ const SwiperForwarded = React.forwardRef((props, ref) => (
         grabCursor={props.grabCursor}
         init={props.init}
         loop={props.loop}
-        loopAdditionalSlides={props.loopAdditionalSlides}
+        //loopAdditionalSlides={props.loopAdditionalSlides}
         onSlideChangeTransitionStart={props.onSlideChangeTransitionStart}
         onSlideChange={props.onSlideChange}
         slideChangeTransitionEnd={props.slideChangeTransitionEnd}
