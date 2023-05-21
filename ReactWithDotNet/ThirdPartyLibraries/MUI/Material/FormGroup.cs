770f2b5a27b431b7d604583c9b29b3690f84df48
beyaz <beyaz1404@gmail.com>
2023-05-21 22:23:20 +0300

    .

--- ReactWithDotNet/ThirdPartyLibraries/MUI/Material/FormGroup.cs
+++ ReactWithDotNet/ThirdPartyLibraries/MUI/Material/FormGroup.cs
@@ -10,7 +10,7 @@ public sealed class FormGroup : ElementBase
 public sealed class FormControlLabel : ElementBase
 {
     [ReactProp]
-    public string? label { get; set; }
+    public string label { get; set; }
 
     [ReactProp]
     public Element control { get; set; }
