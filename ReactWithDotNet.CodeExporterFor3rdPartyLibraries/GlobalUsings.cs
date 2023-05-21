93ceff5e8731fe42836abc5a965265d492654e4e
beyaz <beyaz1404@gmail.com>
2023-05-21 22:39:19 +0300

    .

--- ReactWithDotNet.CodeExporterFor3rdPartyLibraries/GlobalUsings.cs
+++ ReactWithDotNet.CodeExporterFor3rdPartyLibraries/GlobalUsings.cs
@@ -5,10 +5,3 @@ global using System.Collections.Generic;
 global using System.Linq;
 
 global using static ReactWithDotNet.Mixin;
-
-// fix visual studio bug
-// https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit { }
-}
--- ReactWithDotNet.Test/GlobalUsings.cs
+++ ReactWithDotNet.Test/GlobalUsings.cs
@@ -4,12 +4,3 @@ global using ReactWithDotNet;
 global using System.Collections.Generic;
 global using System.Linq;
 global using static ReactWithDotNet.Mixin;
-
-// fix visual studio bug
-// https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
-namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit
-    {
-    }
-}
