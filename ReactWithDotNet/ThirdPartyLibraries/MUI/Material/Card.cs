8bcfb59d1b440a724349f07cc09df58f2092a773
beyaz <beyaz1404@gmail.com>
2023-05-21 22:04:53 +0300

    .

--- ReactWithDotNet/ThirdPartyLibraries/MUI/Material/Card.cs
+++ ReactWithDotNet/ThirdPartyLibraries/MUI/Material/Card.cs
@@ -11,11 +11,4 @@ public sealed class Card : Paper
     /// </summary>
     [ReactProp]
     public bool? raised { get; set; }
-    
-    /// <summary>
-    ///     The system prop that allows defining system overrides as well as additional CSS styles.
-    /// </summary>
-    [ReactProp]
-    [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
-    public dynamic sx { get; } = new ExpandoObject();
 }
