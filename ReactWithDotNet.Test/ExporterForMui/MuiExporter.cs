44b5d7ccd01622c51f289123b10b5e9f6b73d780
beyaz <beyaz1404@gmail.com>
2023-05-27 13:50:18 +0300

    .

--- ReactWithDotNet.Test/ExporterForMui/MuiExporter.cs
+++ ReactWithDotNet.Test/ExporterForMui/MuiExporter.cs
@@ -3,27 +3,6 @@ using static ReactWithDotNet.TypeScriptCodeAnalyzer.Mixin;
 
 namespace ReactWithDotNet.ExporterForMui;
 
-public class MuiExportInput
-{
-    public string ClassName { get; set; }
-
-    public string DefinitionTsCode { get; set; }
-
-    public IReadOnlyList<string> SkipMembers { get; set; }
-
-    public string StartFrom { get; set; }
-
-    public IReadOnlyList<string> ExtraProps { get; set; }
-
-    public bool IsContainer { get; set; }
-
-    public string ClassModifier { get; set; } = "sealed";
-
-    public string BaseClassName { get; set; } = "ElementBase";
-
-    public string OutputFileLocation { get; set; } = @"\MUI\Material\";
-}
-
 static class MuiExporter
 {
     public static void ExportToCSharpFile(MuiExportInput input)
