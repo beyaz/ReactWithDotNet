8bcd4eaf6f80f5ff886f4c3257b67756f29879f9
beyaz <beyaz1404@gmail.com>
2023-05-27 13:51:46 +0300

    .

---
+++ ReactWithDotNet.Test/ExporterForPrimeReact/ExportInput.cs
@@ -0,0 +1,22 @@
+namespace ReactWithDotNet.ExporterForPrimeReact;
+
+public class ExportInput
+{
+    public string ClassName { get; set; }
+
+    public string DefinitionTsCode { get; set; }
+
+    public IReadOnlyList<string> SkipMembers { get; set; }
+
+    public string StartFrom { get; set; }
+
+    public IReadOnlyList<string> ExtraProps { get; set; }
+
+    public bool IsContainer { get; set; }
+
+    public string ClassModifier { get; set; } = "sealed";
+
+    public string BaseClassName { get; set; } = "ElementBase";
+
+    public string OutputFileLocation { get; set; } = @"\MUI\Material\";
+}
