6dd2389ef4ded6514a2cca7bb4f8bb085059e929
beyaz <beyaz1404@gmail.com>
2023-05-27 16:28:39 +0300

    .

--- ReactWithDotNet/ThirdPartyLibraries/PrimeReact/Splitter.cs
+++ ReactWithDotNet/ThirdPartyLibraries/PrimeReact/Splitter.cs
@@ -1,5 +1,7 @@
 // auto generated code (do not edit manually)
 
+using ReactWithDotNet.ThirdPartyLibraries.ReactFreeScrollbar;
+
 namespace ReactWithDotNet.ThirdPartyLibraries.PrimeReact;
 
 public  class Splitter : ElementBase
@@ -33,4 +35,6 @@ public  class Splitter : ElementBase
     /// </summary>
     [ReactProp]
     public string stateStorage { get; set; }
+
+    public static IModifier Modify(Action<Splitter> modifyAction) => CreateThirdPartyReactComponentModifier(modifyAction);
 }
