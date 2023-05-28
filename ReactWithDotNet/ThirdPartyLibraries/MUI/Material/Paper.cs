ff6fdcafa91bfac91b58935121b7a7014d3e3854
beyaz <beyaz1404@gmail.com>
2023-05-28 21:00:13 +0300

    .

--- ReactWithDotNet/ThirdPartyLibraries/MUI/Material/Paper.cs
+++ ReactWithDotNet/ThirdPartyLibraries/MUI/Material/Paper.cs
@@ -33,7 +33,7 @@ public  class Paper : ElementBase
     ///     The system prop that allows defining system overrides as well as additional CSS styles.
     /// </summary>
     [ReactProp]
-    [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
+    [ReactTransformValueInServerSide(typeof(DoNotSendToClientWhenEmpty))]
     public dynamic sx { get; } = new ExpandoObject();
     
     /// <summary>
