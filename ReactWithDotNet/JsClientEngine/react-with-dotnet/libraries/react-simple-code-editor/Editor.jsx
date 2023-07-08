2d6687f0ce9367bd32e18803869862102f5d3fb1
beyaz <beyaz1404@gmail.com>
2023-07-08 23:02:20 +0300

    .

--- ReactWithDotNet/JsClientEngine/react-with-dotnet/libraries/react-simple-code-editor/Editor.jsx
+++ ReactWithDotNet/JsClientEngine/react-with-dotnet/libraries/react-simple-code-editor/Editor.jsx
@@ -5,6 +5,7 @@ import Editor from 'react-simple-code-editor';
 
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-markup';
 //import 'prismjs/components/prism-javascript';
 //import 'prismjs/themes/prism.css'; //Example style, you can use another
 
@@ -13,6 +14,11 @@ function HighlightByLanguage(language, code)
     if (language === "clike")
     {
         return highlight(code, languages.clike);
+    }
+
+     if (language === "html")
+    {
+        return highlight(code, languages.html);
     }
 
     throw 'Not implemented yet.' + language;
