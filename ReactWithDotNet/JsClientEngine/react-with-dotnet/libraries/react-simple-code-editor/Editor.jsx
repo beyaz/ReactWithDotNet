3b545b44eafd3ca336ba6398efcd837ce72010ae
beyaz <beyaz1404@gmail.com>
2023-07-08 21:33:11 +0300

    .

---
+++ ReactWithDotNet/JsClientEngine/react-with-dotnet/libraries/react-simple-code-editor/Editor.jsx
@@ -0,0 +1,68 @@
+
+import React from 'react';
+
+import ReactWithDotNet from "../../react-with-dotnet";
+
+import Editor from 'react-simple-code-editor';
+
+
+//import { highlight, languages } from 'prismjs/components/prism-core';
+//import 'prismjs/components/prism-clike';
+//import 'prismjs/components/prism-javascript';
+//import 'prismjs/themes/prism.css'; //Example style, you can use another
+
+//function register(name, value)
+//{
+//    ReactWithDotNet.RegisterExternalJsObject("ReactWithDotNet.ThirdPartyLibraries.ReactSimpleCodeEditor." + name, value);
+//}
+
+//register("CodeMirror::OnChange", function (args)
+//{
+//    return [/*newText*/args[0]];
+//});
+
+//function calculateExtensions(/*string[]*/stringArray)
+//{
+//    return stringArray.map(name =>
+//    {
+//        if (name == "json")
+//        {
+//            return json();
+//        }
+
+//        if (name == "html")
+//        {
+//            return html();
+//        }
+
+//        if (name == "css")
+//        {
+//            return css();
+//        }
+
+//        if (name == "java")
+//        {
+//            return java();
+//        }
+
+//        if (name == "cpp")
+//        {
+//            return cpp();
+//        }
+
+//        if (name == "javascript")
+//        {
+//            return javascript();
+//        }
+
+//        if (name == "githubLight")
+//        {
+//            return githubLight;
+//        }
+
+//        throw 'Implement here: ' + name;
+//    });
+//}
+
+
+export default Editor
---
+++ ReactWithDotNet/ThirdPartyLibraries/ReactSimpleCodeEditor/Editor.cs
@@ -0,0 +1,43 @@
+﻿namespace ReactWithDotNet.ThirdPartyLibraries.ReactSimpleCodeEditor;
+
+public class Editor : ThirdPartyReactComponent
+{
+    protected const string Prefix = "ReactWithDotNet.ThirdPartyLibraries.ReactSimpleCodeEditor.";
+    
+    [ReactProp]
+    public string value { get; set; }
+    
+   
+
+    
+
+    [ReactProp]
+    [ReactBind(targetProp = nameof(value), jsValueAccess = "e", eventName ="onChange")]
+    public Expression<Func<string>> valueBind { get; set; }
+
+    /// <summary>
+    /// if you want to handle when user iteraction finished see example below<br/>
+    /// component.valueBind = ()=>state.UserInfo.Name<br/>
+    /// component.valueBindDebounceTimeout = 600 // milliseconds<br/>
+    /// component.valueBindDebounceHandler = OnUserIterationFinished<br/>
+    /// </summary>
+    public Action valueBindDebounceHandler { get; set; }
+
+
+    /// <summary>
+    /// if you want to handle when user iteraction finished see example below<br/>
+    /// component.valueBind = ()=>state.UserInfo.Name<br/>
+    /// component.valueBindDebounceTimeout = 600 // milliseconds<br/>
+    /// component.valueBindDebounceHandler = OnUserIterationFinished<br/>
+    /// </summary>
+    public int? valueBindDebounceTimeout { get; set; }
+
+   
+
+
+    protected  override Element GetSuspenseFallbackElement()
+    {
+        return base.GetSuspenseFallbackElement() + MinHeight(300) + MinWidth(400);
+    }
+}
+
