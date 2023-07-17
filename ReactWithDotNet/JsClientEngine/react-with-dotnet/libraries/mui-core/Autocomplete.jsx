2370d6a4f3d8e8684442627e7cce434bcbda65b6
beyaz <beyaz1404@gmail.com>
2023-07-17 23:56:24 +0300

    .

--- ReactWithDotNet/JsClientEngine/react-with-dotnet/libraries/mui-core/Autocomplete.jsx
+++ ReactWithDotNet/JsClientEngine/react-with-dotnet/libraries/mui-core/Autocomplete.jsx
@@ -1,3 +1,26 @@
+
+
+import React from 'react';
+
+import ReactWithDotNet from "../../react-with-dotnet";
+
 import Autocomplete from '@mui/material/Autocomplete';
+
+/**
+ * @param {string} dotNetFullClassNameOf3rdPartyComponent
+ */
+ReactWithDotNet.OnThirdPartyComponentPropsCalculated('ReactWithDotNet.ThirdPartyLibraries.MUI.Material.Autocomplete', props => {
+    if (props.renderInput)
+    {
+        var element = props.renderInput;
+
+        props.renderInput = (params) =>
+        {
+            return React.cloneElement(element, { ...params });
+        }
+    }
+
+    return props;
+});
 
 export default Autocomplete;
