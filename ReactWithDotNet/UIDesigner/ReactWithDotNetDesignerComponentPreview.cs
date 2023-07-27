0fc6346d8e7867ebba746557b0ea209d0bfb2756
beyaz <beyaz1404@gmail.com>
2023-07-28 01:44:45 +0300

    .

--- ReactWithDotNet/UIDesigner/ReactWithDotNetDesignerComponentPreview.cs
+++ ReactWithDotNet/UIDesigner/ReactWithDotNetDesignerComponentPreview.cs
@@ -58,7 +58,7 @@ public class ReactWithDotNetDesignerComponentPreview : ReactComponent<ReactWithD
             
         }
         
-        return createElement()+Border("0.5px dotted #f27070");
+        return createElement()+Border("0.5px dotted blue");
     }
 
     static Type FindType(string typeReference)
