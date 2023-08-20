fda3e6e3350429c3dcf032115ac89c203d2ba53d
beyaz <beyaz1404@gmail.com>
2023-08-20 22:25:35 +0300

    .

--- ReactWithDotNet.WebSite/wwwroot/react-with-dotnet/libraries/framer-motion/all.jsx
+++ ReactWithDotNet.WebSite/wwwroot/react-with-dotnet/libraries/framer-motion/all.jsx
@@ -14,3 +14,6 @@ function register(name, value)
 // Connect components as Lazy
 register("motion_div", React.lazy(() => import('./motion.div')));
 register("AnimatePresence", React.lazy(() => import('./AnimatePresence')));
+register("motion+button", React.lazy(() => import('./button')));
+
+
--- ReactWithDotNet/ThirdPartyLibraries/FramerMotion/motion_div.cs
+++ ReactWithDotNet/ThirdPartyLibraries/FramerMotion/motion_div.cs
@@ -18,4 +18,36 @@ public sealed class motion_div : ElementBase
     [ReactProp]
     [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
     public dynamic exit { get; } = new ExpandoObject();
+    
+    [ReactProp]
+    [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
+    public dynamic whileHover { get; } = new ExpandoObject();
+}
+
+
+public sealed class motion : ElementBase
+{
+    public sealed class button : ElementBase
+    {
+        [ReactProp]
+        [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
+        public dynamic animate { get; } = new ExpandoObject();
+
+        [ReactProp]
+        [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
+        public dynamic initial { get; } = new ExpandoObject();
+
+        [ReactProp]
+        [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
+        public  dynamic transition { get; } = new ExpandoObject();
+    
+    
+        [ReactProp]
+        [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
+        public dynamic exit { get; } = new ExpandoObject();
+    
+        [ReactProp]
+        [ReactTransformValueInClient(Core__ReplaceNullWhenEmpty)]
+        public dynamic whileHover { get; } = new ExpandoObject();
+    }
 }
