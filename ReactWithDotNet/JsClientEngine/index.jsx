b56007248c096dc112552e25430c39caeae9f70a
beyaz <beyaz1404@gmail.com>
2023-09-12 15:58:55 +0300

    .

--- ReactWithDotNet/JsClientEngine/index.jsx
+++ ReactWithDotNet/JsClientEngine/index.jsx
@@ -17,6 +17,6 @@ import "./react-with-dotnet/libraries/framer-motion/all";
 import "./react-with-dotnet/libraries/nextui-org/all";
 import "./react-with-dotnet/libraries/uiw-react-textarea-code-editor/all";
 import "./react-with-dotnet/libraries/react-simple-code-editor/all";
-
+import "./react-with-dotnet/libraries/react-player/all";
 
 export { ReactWithDotNet };
