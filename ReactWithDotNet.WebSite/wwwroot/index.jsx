8605fed1bf311e4089b4ff0067a1c2fb18679feb
beyaz <beyaz1404@gmail.com>
2023-09-18 09:30:50 +0300

    .

--- ReactWithDotNet.WebSite/wwwroot/index.jsx
+++ ReactWithDotNet.WebSite/wwwroot/index.jsx
@@ -18,7 +18,7 @@ import "./react-with-dotnet/libraries/nextui-org/all";
 import "./react-with-dotnet/libraries/uiw-react-textarea-code-editor/all";
 import "./react-with-dotnet/libraries/react-simple-code-editor/all";
 import "./react-with-dotnet/libraries/react-player/all";
-import "./react-with-dotnet/libraries/@monaco-editor-react/all";
+import "./react-with-dotnet/libraries/MonacoEditorReact/all";
 
 
 export { ReactWithDotNet };
