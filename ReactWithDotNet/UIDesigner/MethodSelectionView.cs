963e3a6952fc1cb323ce9ba963579c5b72b8c94d
beyaz <beyaz1404@gmail.com>
2023-10-02 22:20:58 +0300

    .

--- ReactWithDotNet/UIDesigner/MethodSelectionView.cs
+++ ReactWithDotNet/UIDesigner/MethodSelectionView.cs
@@ -1,36 +1,33 @@
-﻿using ReactWithDotNet.ThirdPartyLibraries.PrimeReact;
-using static ReactWithDotNet.UIDesigner.Extensions;
+﻿using static ReactWithDotNet.UIDesigner.Extensions;
+
 namespace ReactWithDotNet.UIDesigner;
 
-class MetadataNode 
+class MetadataNode
 {
+    public List<MetadataNode> children { get; } = new();
     public bool IsClass { get; set; }
     public bool IsMethod { get; set; }
     public bool IsNamespace { get; set; }
-    
-    public string NamespaceReference { get; set; }
-    public MethodReference MethodReference { get; set; }
-    public TypeReference TypeReference { get; set; }
 
-    public List<MetadataNode> children { get; } = new();
-    
     public string label { get; set; }
+    public MethodReference MethodReference { get; set; }
+
+    public string NamespaceReference { get; set; }
+    public TypeReference TypeReference { get; set; }
 }
-
-
 
 class MethodSelectionView : ReactComponent
 {
     public string AssemblyFilePath { get; set; }
 
+    public string ClassFilter { get; set; }
+
+    public string MethodFilter { get; set; }
+
     public string SelectedMethodTreeNodeKey { get; set; }
 
     [ReactCustomEvent]
     public Action<string> SelectionChanged { get; set; }
-
-    public string ClassFilter { get; set; }
-    
-    public string MethodFilter { get; set; }
 
     public static MetadataNode FindTreeNode(string AssemblyFilePath, string treeNodeKey, string classFilter, string methodFilter)
     {
@@ -44,11 +41,55 @@ class MethodSelectionView : ReactComponent
             return null;
         }
 
-        var nodes = MetadataHelper.GetMetadataNodes(AssemblyFilePath, classFilter,methodFilter).ToArray();
-        
-        return FindTreeNode(nodes, x=>HasMatch(x,treeNodeKey));
+        var nodes = MetadataHelper.GetMetadataNodes(AssemblyFilePath, classFilter, methodFilter).ToArray();
+
+        return FindTreeNode(nodes, x => HasMatch(x, treeNodeKey));
     }
-    
+
+    protected override Element render()
+    {
+        var nodes = GetNodes().ToList();
+
+        return new div(Height(400), MarginLeftRight(3), OverflowYScroll, CursorPointer, Padding(5), Border(Solid(1, "rgb(217, 217, 217)")), BorderRadius(3))
+        {
+            AsTreeView(nodes)
+        };
+    }
+
+    static MetadataNode FindTreeNode(IEnumerable<MetadataNode> nodes, Func<MetadataNode, bool> hasMatch)
+    {
+        if (nodes == null)
+        {
+            return null;
+        }
+
+        foreach (var childNode in nodes)
+        {
+            var found = FindTreeNode(childNode, hasMatch);
+            if (found is not null)
+            {
+                return found;
+            }
+        }
+
+        return null;
+    }
+
+    static MetadataNode FindTreeNode(MetadataNode node, Func<MetadataNode, bool> hasMatch)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        if (hasMatch(node))
+        {
+            return node;
+        }
+
+        return FindTreeNode(node.children, hasMatch);
+    }
+
     static bool HasMatch(MetadataNode node, string treeNodeKey)
     {
         if (node.IsClass)
@@ -63,51 +104,59 @@ class MethodSelectionView : ReactComponent
 
         return false;
     }
-    
 
-    static MetadataNode FindTreeNode(IEnumerable<MetadataNode> nodes, Func<MetadataNode, bool> hasMatch)
+    Element AsTreeItem(MetadataNode node)
     {
-        if (nodes == null)
+        if (node.IsMethod)
         {
-            return null;
-        }
-        
-        foreach (var childNode in nodes)
-        {
-            var found = FindTreeNode(childNode, hasMatch);
-            if (found is not null)
+            return new FlexRow(AlignItemsCenter)
             {
-                return found;
-            }
-        }
-        
-        return null;
-    }
-    static MetadataNode FindTreeNode(MetadataNode node, Func<MetadataNode,bool> hasMatch)
-    {
-        if (node == null)
-        {
-            return null;
+                new img { Src(GetSvgUrl("Method")), wh(11), mt(5), ml(20) },
+
+                new div { Text(node.label), MarginLeft(5), FontSize13 },
+
+                Id(node.MethodReference.MetadataToken),
+
+                arrangeBackground
+            };
         }
 
-        if (hasMatch(node))
+        if (node.IsClass)
         {
-            return node;
+            return new FlexRow(AlignItemsCenter)
+            {
+                new img { Src(GetSvgUrl("Class")), wh(14), ml(10) },
+
+                new div { Text(node.label), MarginLeft(5), FontSize13 },
+
+                Id(node.TypeReference.FullName),
+                arrangeBackground
+            };
         }
 
-        return FindTreeNode(node.children, hasMatch);
-    }
-    
-
-    protected override Element render()
-    {
-        var nodes = GetNodes().ToList();
-
-
-        return new div(Height(400), MarginLeftRight(3),OverflowYScroll,CursorPointer, Border(Solid(1,"rgb(217, 217, 217)")), BorderRadius(3))
+        if (node.IsNamespace)
         {
-            AsTreeView(nodes)
-        };
+            return new FlexRow(AlignItemsCenter)
+            {
+                new img { Src(GetSvgUrl("Namespace")), wh(14) },
+
+                new div { Text(node.label), MarginLeft(5), FontSize13 }
+            };
+        }
+
+        return new div();
+
+        void arrangeBackground(HtmlElement el)
+        {
+            el += new[]
+            {
+                Hover(BackgroundImage(linear_gradient(90, "#d5d5c1", "whitesmoke")), BorderRadius(3)),
+
+                When(HasMatch(node, SelectedMethodTreeNodeKey), BackgroundImage(linear_gradient(90, "#d1d1c8", "whitesmoke")), BorderRadius(3))
+            };
+
+            el.onClick = OnTreeItemClicked;
+        }
     }
 
     Element AsTreeView(IReadOnlyList<MetadataNode> nodes)
@@ -121,99 +170,32 @@ class MethodSelectionView : ReactComponent
         {
             if (node.children?.Count > 0)
             {
-                var parent = nodeTemplate(node);
+                var parent = AsTreeItem(node);
                 var chldrn = AsTreeView(node.children);
 
                 return new Fragment
                 {
-                    parent+OnClick(OnTreeItemClicked),
+                    parent + OnClick(OnTreeItemClicked),
                     chldrn
                 };
-
             }
 
-            return nodeTemplate(node);
+            return AsTreeItem(node);
         }
-    }
-
-    void OnTreeItemClicked(MouseEvent e)
-    {
-        DispatchEvent(() => SelectionChanged, e.FirstNotEmptyId);
-    }
-
-    Element nodeTemplate(MetadataNode node)
-    {
-
-        void arrangeBackground(HtmlElement el)
-        {
-            
-            el += new []
-            {
-                Hover(BackgroundImage(linear_gradient(90, "#d5d5c1", "whitesmoke")), BorderRadius(3)),
-                
-                When(HasMatch(node, SelectedMethodTreeNodeKey), new[]
-                {
-                    BackgroundImage(linear_gradient(90, "#d1d1c8", "whitesmoke")),
-                    BorderRadius(3)
-                })
-            };
-
-            el.onClick = OnTreeItemClicked;
-
-        }
-        
-        if (node.IsMethod)
-        {
-            return new FlexRow(AlignItemsCenter)
-            {
-                new img { Src(GetSvgUrl("Method")), wh(11), mt(5),ml(20) },
-
-                new div { Text(node.label), MarginLeft(5), FontSize13 },
-                
-                Id(node.MethodReference.MetadataToken),
-                
-                arrangeBackground
-            };
-        }
-
-        if (node.IsClass)
-        {
-            return new FlexRow(AlignItemsCenter)
-            {
-                new img { Src(GetSvgUrl("Class")), wh(14), ml(10) },
-
-                new div { Text(node.label), MarginLeft(5), FontSize13 },
-                
-                Id(node.TypeReference.FullName),
-                arrangeBackground
-            };
-        }
-
-        if (node.IsNamespace)
-        {
-            return new FlexRow(AlignItemsCenter)
-            {
-                new img { Src(GetSvgUrl("Namespace")), wh(14) },
-
-                new div { Text(node.label), MarginLeft(5), FontSize13 },
-            };
-        }
-
-        return new div();
     }
 
     IEnumerable<MetadataNode> GetNodes()
     {
         if (!string.IsNullOrWhiteSpace(AssemblyFilePath) && File.Exists(AssemblyFilePath))
         {
-            return MetadataHelper.GetMetadataNodes(AssemblyFilePath, ClassFilter,MethodFilter);
+            return MetadataHelper.GetMetadataNodes(AssemblyFilePath, ClassFilter, MethodFilter);
         }
 
         return new List<MetadataNode>();
     }
 
-    void OnSelectionChanged(SingleSelectionTreeSelectionParams e)
+    void OnTreeItemClicked(MouseEvent e)
     {
-        DispatchEvent(() => SelectionChanged, e.value);
+        DispatchEvent(() => SelectionChanged, e.FirstNotEmptyId);
     }
 }
