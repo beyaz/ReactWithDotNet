94c569c917d54e2410bc8cb7280b46ea1a33212b
beyaz <beyaz1404@gmail.com>
2023-10-02 22:42:03 +0300

    .

--- ReactWithDotNet/UIDesigner/ReactWithDotNetDesigner.cs
+++ ReactWithDotNet/UIDesigner/ReactWithDotNetDesigner.cs
@@ -632,4 +632,44 @@ public class ReactWithDotNetDesigner : ReactComponent<ReactWithDotNetDesignerMod
 
         OnComponentPreviewRefreshed();
     }
+    
+    
+    // Taken from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_loader
+    class LoadingIcon : ReactPureComponent
+    {
+        protected override Element render()
+        {
+            return new div
+            {
+                new style
+                {
+                    text = @"
+
+.loader {
+  border: 1px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 1px solid #A08139;
+
+  -webkit-animation: spin 1s linear infinite; /* Safari */
+  animation: spin 1s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+"
+                },
+
+                new div { className = "loader", style = { WidthHeightMaximized } }
+            };
+        }
+    }
 }
