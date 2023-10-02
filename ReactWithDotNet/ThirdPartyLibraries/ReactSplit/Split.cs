9b88acb2adacd8ffccef595263dd55685150cf74
beyaz <beyaz1404@gmail.com>
2023-10-03 01:16:54 +0300

    .

---
+++ ReactWithDotNet/ThirdPartyLibraries/ReactSplit/Split.cs
@@ -0,0 +1,43 @@
+﻿namespace ReactWithDotNet.ThirdPartyLibraries.ReactSplit;
+
+
+public sealed class Split : ThirdPartyReactComponent
+{
+
+    [ReactProp]
+    public double[] sizes { get; set; }
+
+    [ReactProp]
+    public double? minSize { get; set; }
+
+    [ReactProp]
+    public bool? expandToMin { get; set; }
+
+
+    [ReactProp]
+    public double? gutterSize { get; set; }
+
+    /// <summary>
+    /// 'center' | 'start' | 'end'
+    /// </summary>
+    [ReactProp]
+    public string gutterAlign { get; set; }
+    
+    
+    [ReactProp]
+    public double? snapOffset { get; set; }
+    
+    
+    [ReactProp]
+    public double? dragInterval { get; set; }
+    
+    /// <summary>
+    /// 'horizontal' | 'vertical'
+    /// </summary>
+    [ReactProp]
+    public string direction { get; set; }
+    
+    
+    [ReactProp]
+    public string cursor { get; set; }
+}
