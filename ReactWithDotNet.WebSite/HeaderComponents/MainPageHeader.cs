12613d915c2feec29efaf772cd19d4ac61170c67
beyaz <beyaz1404@gmail.com>
2023-10-31 22:33:08 +0300

    .

--- ReactWithDotNet.WebSite/HeaderComponents/MainPageHeader.cs
+++ ReactWithDotNet.WebSite/HeaderComponents/MainPageHeader.cs
@@ -6,6 +6,8 @@ class MainPageHeader : PureComponent
     {
         return new header(DisplayFlex, JustifyContentCenter, BoxShadow($"inset 0px -1px 1px {Theme.grey_100}"))
         {
+            PositionSticky, Top(0), BackgroundColor(rgba(255, 255, 255, 0.8)), BackdropFilterBlur(6),
+            
             new div(ContainerStyle)
             {
                 new HeaderMenuBar()
