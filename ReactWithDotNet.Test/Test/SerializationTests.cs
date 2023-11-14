415606a49cf357b4019259984298e8c650ed553f
beyaz <beyaz1404@gmail.com>
2023-11-14 23:05:19 +0300

    .

---
+++ ReactWithDotNet.Test/Test/SerializationTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReactWithDotNet.Test;
+
+[TestClass]
+public class SerializationTests
+{
+    [TestMethod]
+    public void __1__1()
+    {
+        
+    }
+}
