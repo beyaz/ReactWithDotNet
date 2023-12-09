304c96f60959ff25dcd41e54e4455c75cc768e66
beyaz <beyaz1404@gmail.com>
2023-12-09 14:01:55 +0300

    .

--- ReactWithDotNet/Style.cs
+++ ReactWithDotNet/Style.cs
@@ -1,9 +1,219 @@
-﻿namespace ReactWithDotNet;
+﻿using System.Text;
+
+namespace ReactWithDotNet;
 
 [Serializable]
 public sealed partial class Style
 {
-   
-    
+
+    sealed class StyleAttributeNameInfo
+    {
+        public readonly string NameInCamelCase;
+        public readonly string NameInKebabCase;
+
+        public StyleAttributeNameInfo(string nameInCamelCase, string nameInKebabCase)
+        {
+            NameInCamelCase      = nameInCamelCase;
+            NameInKebabCase = nameInKebabCase;
+        }
+    }
+
+    sealed class StyleAttributeValue
+    {
+        public readonly StyleAttributeNameInfo NameInfo;
+        public string Value;
+
+        public StyleAttributeValue(StyleAttributeNameInfo nameInfo)
+        {
+            NameInfo   = nameInfo;
+        }
+
+        public StyleAttributeValue Previous;
+        public StyleAttributeValue Next;
+    }
+
+    StyleAttributeValue headNode;
+
+    static class Names
+    {
+        public static readonly StyleAttributeNameInfo accentColor = new StyleAttributeNameInfo("accentColor","accent-color");
+    }
+
+    string Get(StyleAttributeNameInfo nameInfo)
+    {
+        var currentNode = headNode;
+        
+        while (currentNode != null)
+        {
+            // update
+            if (ReferenceEquals(nameInfo, currentNode.NameInfo))
+            {
+                return currentNode.Value;
+            }
+
+            if (currentNode.Next == null)
+            {
+                break;
+            }
+            
+            currentNode = currentNode.Next;
+        }
+        
+        return null;
+    }
+
+    void Set(StyleAttributeNameInfo nameInfo, string value)
+    {
+        var currentNode = headNode;
+        
+        // try remove
+        if (value == null)
+        {
+            while (currentNode != null)
+            {
+                // remove node
+                if (ReferenceEquals(nameInfo, currentNode.NameInfo))
+                {
+                    currentNode.Previous = currentNode.Next;
+                    return;
+                }
+
+                // not found
+                if (currentNode.Next == null)
+                {
+                    break;
+                }
+            
+                currentNode = currentNode.Next;
+            }
+            
+            // not found
+            return;
+        }
+        
+        if (currentNode == null)
+        {
+            headNode = new StyleAttributeValue(nameInfo) { Value = value };
+            return;
+        }
+
+        
+        while (currentNode != null)
+        {
+            // update
+            if (ReferenceEquals(nameInfo, currentNode.NameInfo))
+            {
+                currentNode.Value = value;
+                return;
+            }
+
+            if (currentNode.Next == null)
+            {
+                break;
+            }
+            
+            currentNode = currentNode.Next;
+        }
+
+        currentNode.Next = new StyleAttributeValue(nameInfo)
+        {
+            Value = value,
+            Previous = currentNode
+        };
+    }
+
+    string ToCss2(bool isImportant)
+    {
+        var sb = new StringBuilder();
+
+        var separator = isImportant ? " !important;" : ";";
+        
+        var currentNode = headNode;
+        
+        while (currentNode != null)
+        {
+            sb.Append(currentNode.NameInfo.NameInKebabCase);
+            sb.Append(":");
+            sb.Append(currentNode.Value);
+            sb.Append(separator);
+
+            if (currentNode.Next == null)
+            {
+                break;
+            }
+            
+            currentNode = currentNode.Next;
+        }
+
+        if (sb.Length == 0)
+        {
+            return null;
+        }
+
+        return sb.ToString();
+    }
+
+    static void transfer2(Style source, Style target)
+    {
+        var nodeInSource = source.headNode;
+        var nodeInTarget = target.headNode;
+        
+        if (nodeInTarget is null)
+        {
+            // fast clone to target
+
+            target.headNode = FastCloneAll(nodeInSource);
+            
+            return;
+        }
+        
+        
+        while (nodeInSource != null)
+        {
+            target.Set(nodeInSource.NameInfo, nodeInSource.Value);
+                
+            nodeInSource = nodeInSource.Next;
+        }
+    }
+
+    static StyleAttributeValue FastCloneAll(StyleAttributeValue headNode)
+    {
+        if (headNode is null)
+        {
+            return null;
+        }
+
+        var clonedHeadNode =  new StyleAttributeValue(headNode.NameInfo)
+        {
+            Value = headNode.Value
+        };
+
+        var nodeInTarget = clonedHeadNode;
+        var nodeInSource = headNode;
+        
+        while (nodeInSource is not null)
+        {
+            nodeInTarget = new StyleAttributeValue(nodeInSource.NameInfo)
+            {
+                Value    = nodeInSource.Value,
+                Previous = nodeInTarget
+            };
+            
+            nodeInSource = nodeInSource.Next;
+        }
+
+        return clonedHeadNode;
+    }
+
+    internal void VisitNotNullValues2(Action<string, string> action)
+    {
+        
+    }
+
+    public string accentColor2
+    {
+        set => Set(Names.accentColor, value);
+        get => Get(Names.accentColor);
+    }
 
 }
