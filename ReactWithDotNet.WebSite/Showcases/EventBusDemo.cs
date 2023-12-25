1dccc3081c6a78069c1f9d689c187bcba080fe2f
beyaz <beyaz1404@gmail.com>
2023-12-25 15:32:49 +0300

    .

--- ReactWithDotNet.WebSite/Showcases/EventBusDemo.cs
+++ ReactWithDotNet.WebSite/Showcases/EventBusDemo.cs
@@ -1,7 +1,108 @@
-﻿namespace ReactWithDotNet.WebSite.Showcases;
+﻿
 
+using ReactWithDotNet.ThirdPartyLibraries.PrimeReact;
 
-public class EventBusDemo : PureComponent
+namespace ReactWithDotNet.WebSite.Showcases;
+
+public class EventBusDemo : Component
+{
+    protected override Element render()
+    {
+        return new FlexRow(WidthHeightMaximized, CursorDefault, Gap(10))
+        {
+            new ComponentA(), new ComponentB()
+        };
+    }
+
+    class ComponentA : Component
+    {
+        protected override Element render()
+        {
+            return new div(Size(400,300), Border(Solid(1,"red")))
+            {
+                new div{ nameof(ComponentA)},
+                new ComponentA_0()
+            };
+        }
+    }
+    
+    class ComponentA_0 : Component
+    {
+        public string Message { get; set; } = nameof(ComponentA_0);
+        
+        protected override Element render()
+        {
+            return new div(Size(300,200), Border(Solid(1,"red")))
+            {
+                new div{ Message },
+                new ComponentA_0_0()
+            };
+        }
+        
+        protected override Task constructor()
+        {
+            Client.ListenEvent("MessageForA_0", OnMessageForA_0);
+            return base.constructor();
+        }
+
+        Task OnMessageForA_0(string message)
+        {
+            Message = message;
+            Client.DispatchEvent("MessageForB","5");
+            return Task.CompletedTask;
+        }
+    }
+    
+    class ComponentA_0_0 : Component
+    {
+        protected override Element render()
+        {
+            return new div(Size(200,100), Border(Solid(1,"red")))
+            {
+                new div{ nameof(ComponentA_0_0)},
+                OnClick(OnClickHandler)
+            };
+        }
+
+        [ReactStopPropagation]
+        Task OnClickHandler(MouseEvent e)
+        {
+            Client.DispatchEvent("MessageForA_0","1");
+            
+            return Task.CompletedTask;
+        }
+    }
+    
+    class ComponentB : Component
+    {
+        public string Message { get; set; } = nameof(ComponentB);
+        
+        protected override Task constructor()
+        {
+            Client.ListenEvent("MessageForB",OnMessageForB);
+            return base.constructor();
+        }
+
+        Task OnMessageForB(string message)
+        {
+            Message = message;
+            
+            Client.DispatchEvent("MessageForA_0","2");
+                
+            return Task.CompletedTask;
+        }
+
+        protected override Element render()
+        {
+            return new div(Size(400,300), Border(Solid(1,"red")))
+            {
+                new div{ Message }
+            };
+        }
+    }
+}
+
+public class EventBusDemo2 : PureComponent
 {
     protected override Element render()
     {
