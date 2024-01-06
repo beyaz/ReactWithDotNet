43f1dd2f3f6b2b3db6952308e59f264959371ebc
beyaz <beyaz1404@gmail.com>
2024-01-06 19:25:38 +0300

    .

--- ReactWithDotNet/UIDesigner/ReactWithDotNetDesigner.cs
+++ ReactWithDotNet/UIDesigner/ReactWithDotNetDesigner.cs
@@ -467,7 +467,8 @@ public class ReactWithDotNetDesigner : Component<ReactWithDotNetDesignerModel>
             return exception.ToString();
         }
         
-        return new FlexColumn(BorderLeft("1px dotted #d9d9d9"), Width(300), PositionFixed, Right(0), Top(0), Height100vh)
+        return new FlexColumn(BorderLeft("1px dotted #d9d9d9"), Width(300), PositionFixed, Right(0), Top(0), Height100vh,
+                              FontFamily("consolas, sans-serif"), FontSize11)
         {
             CreateElementTree(rootNode) + FlexGrow(1),
             CreateStyleEditor() + FlexGrow(3)
@@ -478,10 +479,12 @@ public class ReactWithDotNetDesigner : Component<ReactWithDotNetDesignerModel>
     {
         return new FlexColumn(BorderTop("1px dotted #d9d9d9"))
         {
-            new FlexColumn(Gap(5))
+            new FlexColumn(Gap(5), JustifyContentFlexStart)
             {
                 new input{type = "text", value = "abc"},
-                new input{type = "text", value = "abc2"}
+                new input{type = "text", value = "abc2"},
+                
+                new StyleSearchInput()
             }
         };
     }
@@ -494,7 +497,7 @@ public class ReactWithDotNetDesigner : Component<ReactWithDotNetDesignerModel>
         
         var tree = new FlexColumn
         {
-            FontFamily("system-ui"), FontSize11, CursorDefault, FontWeight500, FontStyleItalic
+             CursorDefault, FontWeight500, FontStyleItalic
         };
 
         addNode(rootNode,"0",2);
@@ -904,5 +907,69 @@ public class ReactWithDotNetDesigner : Component<ReactWithDotNetDesignerModel>
                 new div { className = "loader-designer-react-with-dot-net", style = { WidthHeightMaximized } }
             };
         }
+    }
+
+
+    class StyleSearchInput : Component
+    {
+        Element IconSearch => new svg(svg.Size(18), ViewBox(0, 0, 18, 18))
+        {
+            new path { d = "M14.8539503,14.1467096 C15.0478453,14.3412138 15.0475893,14.6560006 14.8533783,14.8501892 C14.6592498,15.0442953 14.3445263,15.0442862 14.1504091,14.8501689 L12.020126,12.7261364 C11.066294,13.5214883 9.8390282,14 8.5,14 C5.46243388,14 3,11.5375661 3,8.5 C3,5.46243388 5.46243388,3 8.5,3 C11.5375661,3 14,5.46243388 14,8.5 C14,9.83874333 13.5216919,11.0657718 12.726644,12.0195172 L14.8539503,14.1467096 Z M8.5,13 C10.9852814,13 13,10.9852814 13,8.5 C13,6.01471863 10.9852814,4 8.5,4 C6.01471863,4 4,6.01471863 4,8.5 C4,10.9852814 6.01471863,13 8.5,13 Z" }
+        };
+
+        Element IconClose => new svg(svg.Size(18), ViewBox(0, 0, 18, 18))
+        {
+            new path { d = "M8.44 9.5L6 7.06A.75.75 0 1 1 7.06 6L9.5 8.44 11.94 6A.75.75 0 0 1 13 7.06L10.56 9.5 13 11.94A.75.75 0 0 1 11.94 13L9.5 10.56 7.06 13A.75.75 0 0 1 6 11.94L8.44 9.5z" }
+        };
+        protected override Element render()
+        {
+            return new FlexColumn
+            {
+                new style
+                {
+                    """
+                    input:focus { outline:none; }
+                    """
+                },
+                new FlexRow(BackgroundWhite, Border(Solid(1, "#dbdde5")), Padding(3), BorderRadius(3))
+                {
+
+                    IconSearch,
+                    new input
+                    {
+                        type                     = "text",
+                        valueBind                = () => Value,
+                        valueBindDebounceTimeout = 700,
+                        valueBindDebounceHandler = OnTypingFinished,
+                        onKeyDown = OnKeyDown,
+                        style                    = { BorderNone, FlexGrow(1) }
+                    },
+                    IconClose,
+
+
+                },
+                new FlexColumn
+                {
+                    typeof(Mixin).GetProperties(BindingFlags. Static | BindingFlags.Public)
+                        .Where(p=>p.Name.Contains(Value+"",StringComparison.OrdinalIgnoreCase))
+                        .Select(p=>new div{p.Name})
+                        .Take(5)
+                }
+            };
+        }
+
+        [ReactKeyboardEventCallOnly("ESC")]
+        Task OnKeyDown(KeyboardEvent e)
+        {
+            return Task.CompletedTask;
+        }
+
+        Task OnTypingFinished()
+        {
+            
+            return Task.CompletedTask;
+        }
+
+        public string Value { get; set; }
     }
 }
