80c41a672e5a99ba68f655dbd711755294ba0e08
beyaz <beyaz1404@gmail.com>
2024-01-06 13:50:22 +0300

    .

---
+++ ReactWithDotNet.WebSite/$.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ReactWithDotNet.__designer__; // ReSharper disable All
+
+static class Designer
+{
+    public static List<StyleModifier> GetStyle(Type type)
+    {
+        if (type == typeof(ReactWithDotNet.WebSite.Components.GetStartedButton))
+        {
+            return
+            [
+                TextAlignCenter
+            ];
+        }
+        
+        return null;
+    }
+}
+
