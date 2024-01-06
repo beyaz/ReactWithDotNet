757b7ca87f5ea6562f1bc006f20b435130a00439
beyaz <beyaz1404@gmail.com>
2024-01-06 22:12:33 +0300

    .

--- ReactWithDotNet/DesignerElementInfo.cs
+++ ReactWithDotNet/DesignerElementInfo.cs
@@ -5,60 +5,42 @@ namespace ReactWithDotNet;
 public sealed class DesignerComponentInfo
 {
     public Type TargetType { get; set; }
-    
+
     public IReadOnlyList<DesignerElementInfo> VisualTree { get; set; }
 }
 
 public sealed class DesignerElementInfo
 {
-    public IReadOnlyList<int> VisualTreePath { get; set; }
-    
     public IReadOnlyList<StyleModifier> Modifiers { get; set; }
+    public IReadOnlyList<int> VisualTreePath { get; set; }
 }
 
 static class DesignerHelper
 {
-    static IReadOnlyList<DesignerComponentInfo> ReadFromAssembly(Assembly assembly)
-    {
-        var designerType = assembly.GetType("ReactWithDotNet.__designer__.Designer");
-        if (designerType == null)
-        {
-            return null;
-        }
+    static readonly LinkedList<CacheItem> Cache = new();
 
-        var componentInformationListPropertyInfo = designerType.GetProperty("ComponentInformationList", BindingFlags.Static | BindingFlags.Public);
-
-        if (componentInformationListPropertyInfo == null)
-        {
-            throw new MissingMemberException("componentInformationListPropertyInfo");
-        }
-
-        return (IReadOnlyList<DesignerComponentInfo>)componentInformationListPropertyInfo.GetValue(null);
-    }
-    
     public static void Override(Element component, Element rootNode)
     {
-        Type componentType = component.GetType();
+        var componentType = component.GetType();
 
         var records = ReadFromAssembly(componentType.Assembly);
         if (records == null)
         {
             return;
         }
-        
+
         var record = records.FirstOrDefault(x => x.TargetType == componentType);
         if (record == null)
         {
             return;
         }
-        
-        
+
         foreach (var item in record.VisualTree)
         {
             var node = rootNode;
             var i = 1;
             var len = item.VisualTreePath.Count;
-            
+
             while (i < len)
             {
                 var offset = item.VisualTreePath[i];
@@ -67,9 +49,9 @@ static class DesignerHelper
                 {
                     break;
                 }
-                
+
                 node = node._children[offset];
-                
+
                 i++;
             }
 
@@ -82,8 +64,42 @@ static class DesignerHelper
             {
                 ModifyHelper.ProcessModifier(node, styleModifier);
             }
-            
         }
     }
-    
+
+    static IReadOnlyList<DesignerComponentInfo> ReadFromAssembly(Assembly assembly)
+    {
+        var cacheKey = assembly.FullName;
+
+        var item = Cache.FirstOrDefault(x => x.assemblyFullName == cacheKey);
+
+        if (item == null)
+        {
+            item = new(cacheKey, ReadFromAssemblyNoCache(assembly));
+
+            Cache.AddLast(item);
+        }
+
+        return item.value;
+
+        static IReadOnlyList<DesignerComponentInfo> ReadFromAssemblyNoCache(Assembly assembly)
+        {
+            var designerType = assembly.GetType("ReactWithDotNet.__designer__.Designer");
+            if (designerType == null)
+            {
+                return null;
+            }
+
+            var componentInformationListPropertyInfo = designerType.GetProperty("ComponentInformationList", BindingFlags.Static | BindingFlags.Public);
+
+            if (componentInformationListPropertyInfo == null)
+            {
+                throw new MissingMemberException("componentInformationListPropertyInfo");
+            }
+
+            return (IReadOnlyList<DesignerComponentInfo>)componentInformationListPropertyInfo.GetValue(null);
+        }
+    }
+
+    record CacheItem(string assemblyFullName, IReadOnlyList<DesignerComponentInfo> value);
 }
