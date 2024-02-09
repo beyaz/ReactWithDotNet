74b497c33afbacee9f8a51a1a7d6ac61c85f1a22
beyaz <beyaz1404@gmail.com>
2024-02-10 00:59:57 +0300

    .

--- ReactWithDotNet.WebSite/Components/HamburgerButton.cs
+++ ReactWithDotNet.WebSite/Components/HamburgerButton.cs
@@ -8,6 +8,8 @@ class HamburgerButton : Component
 
     protected override Element render()
     {
+        var transition = Transition("all 0.2s ease 0s");
+        
         return new button(DisplayFlexRowCentered, OnClick(Click))
         {
             Border(Solid(1,Theme.grey_300)),
@@ -17,24 +19,24 @@ class HamburgerButton : Component
             When(IsOpen,Background(Theme.grey_50)),
 
             Padding(8),
-            new svg(svg.Width(16),svg.Height(16), svg.ViewBox(0, 0, 16, 16), svg.Fill("none"))
+            new svg(svg.Width(16),svg.Height(16), svg.ViewBox(0, 0, 16, 16), svg.Fill(none))
             {
-                new rect(Transition("all 0.2s ease 0s"), When(IsOpen, Transform("translate(6px, -2px) rotateZ(45deg)")))
+                new rect(transition, When(IsOpen, Transform("translate(6px, -2px) rotateZ(45deg)")))
                 {
-                    x      = "1",
-                    y      = "5",
-                    width  = "14",
-                    height = "1.5",
-                    rx     = "1",
+                    x      = 1,
+                    y      = 5,
+                    width  = 14,
+                    height = 1.5,
+                    rx     = 1,
                     fill   = Theme.primary_main
                 },
-                new rect(Transition("all 0.2s ease 0s"), When(IsOpen, Transform("translate(-5px, 6px) rotateZ(-45deg)")))
+                new rect(transition, When(IsOpen, Transform("translate(-5px, 6px) rotateZ(-45deg)")))
                 {
-                    x      = "1",
-                    y      = "9",
-                    width  = "14",
-                    height = "1.5",
-                    rx     = "1",
+                    x      = 1,
+                    y      = 9,
+                    width  = 14,
+                    height = 1.5,
+                    rx     = 1,
                     fill   = Theme.primary_main
                 }
             }
