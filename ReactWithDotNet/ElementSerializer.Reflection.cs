5cf771dc27c3b5f4b66c957927d6f0916faddc1f
beyaz <beyaz1404@gmail.com>
2024-02-16 23:33:37 +0300

    .

---
+++ ReactWithDotNet/ElementSerializer.Reflection.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace ReactWithDotNet;
+
+static partial class ElementSerializer
+{
+    
+}
