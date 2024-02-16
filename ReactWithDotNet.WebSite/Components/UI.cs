0e765dfc415a1dcb0c296cbf261717b0bca40e14
beyaz <beyaz1404@gmail.com>
2024-02-17 01:27:03 +0300

    .

--- ReactWithDotNet.WebSite/Components/UI.cs
+++ ReactWithDotNet.WebSite/Components/UI.cs
@@ -4,15 +4,14 @@ static class UI
 {
     public static Element ProgressBar(int total, int current)
     {
-        return new div(WidthFull,BackgroundColor("#dddddd"), Border(Solid(3,"#eee")), BorderRadius(15), Height(14))
+        return new div(WidthFull,Height(14), BackgroundColor("#dddddd"), Border(Solid(3, "#eee")), BorderRadius(15))
         {
             new div
             {
                 HeightFull,
-                Width(current,total),
-                BorderRadius(16),
-                
-                BackgroundImage(linear_gradientTo("right","#8490ff","#a3eeff"))
+                Width(current, total),
+                BorderRadius(15),
+                BackgroundImage(linear_gradientTo("right", "#8490ff", "#a3eeff"))
             }
         };
     }
