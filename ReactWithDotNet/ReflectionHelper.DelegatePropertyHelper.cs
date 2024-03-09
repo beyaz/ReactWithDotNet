d21bfec1e6dc22204915a79b726783ca76160fe3
beyaz <beyaz1404@gmail.com>
2024-03-09 21:04:12 +0300

    .

--- ReactWithDotNet/ReflectionHelper.DelegatePropertyHelper.cs
+++ ReactWithDotNet/ReflectionHelper.DelegatePropertyHelper.cs
@@ -8,6 +8,18 @@ static class DelegatePropertyHelper
 {
     public static Delegate ReCalculatePropertyValue(ReactComponentBase reactComponent, PropertyInfo propertyInfo)
     {
+        var methodInfoCreateDelagate = typeof(DelegatePropertyHelper).GetMethod(nameof(CreateDelegate), BindingFlags.Static| BindingFlags.NonPublic);
+        if (methodInfoCreateDelagate is null)
+        {
+            throw new ArgumentNullException(nameof(methodInfoCreateDelagate));
+        }
+        
+        var methodInfoCurry = typeof(DelegatePropertyHelper).GetMethod(nameof(Curry),BindingFlags.Static| BindingFlags.NonPublic);
+        if (methodInfoCurry is null)
+        {
+            throw new ArgumentNullException(nameof(methodInfoCurry));
+        }
+        
         var argumentTypes = typeof(string);
         
         var genericArguments = propertyInfo.PropertyType.GetGenericArguments();
@@ -15,25 +27,28 @@ static class DelegatePropertyHelper
         {
             argumentTypes = genericArguments[0];
         }
+
         
-        var createDelegateMethodInfo = typeof(DelegatePropertyHelper).GetMethod(nameof(CreateDelegate))!.MakeGenericMethod(argumentTypes);
+        
+        var createDelegateMethodInfo = methodInfoCreateDelagate.MakeGenericMethod(argumentTypes);
 
         var delegateFunc = createDelegateMethodInfo.Invoke(null, [reactComponent, propertyInfo]);
+
         
-        var curryMethodInfo = typeof(DelegatePropertyHelper).GetMethod(nameof(Curry))!.MakeGenericMethod(argumentTypes);
+        var curryMethodInfo = methodInfoCurry.MakeGenericMethod(argumentTypes);
 
         return (Delegate) curryMethodInfo.Invoke(null, [reactComponent, delegateFunc]);
     }
     
     [Pure]
-    static Func<T,Task> Curry<T>(ReactComponentBase reactComponent, Func<object,T, Task> a)
+    static Func<A,Task> Curry<A>(ReactComponentBase reactComponent, Func<object,A, Task> a)
     {
         return  x => a(reactComponent, x);
     }
     
     static Func<object,T, Task> CreateDelegate<T>(ReactComponentBase reactComponent, PropertyInfo propertyInfo)
     {
-        var methodInfo = typeof(DelegatePropertyHelper).GetMethod(nameof(DispatchDotNetCustomEvent));
+        var methodInfo = typeof(DelegatePropertyHelper).GetMethod(nameof(DispatchDotNetCustomEvent),BindingFlags.Static| BindingFlags.NonPublic);
         if (methodInfo is null)
         {
             throw new NullReferenceException(nameof(methodInfo));
