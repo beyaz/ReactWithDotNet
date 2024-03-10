aa4bf1bc7ceefad22751993aaeafe8f647cfbe11
beyaz <beyaz1404@gmail.com>
2024-03-10 04:39:21 +0300

    .

--- ReactWithDotNet.Test/Test/DelegatePropertyTest.cs
+++ ReactWithDotNet.Test/Test/DelegatePropertyTest.cs
@@ -9,10 +9,13 @@ public class DelegatePropertyTest
 {
     class MyComponent : Component
     {
-
+        public Func<Task> SampleDelegateProperty0 { get; set; }
+        
         public Func<string,Task> SampleDelegateProperty1 { get; set; }
         
         public Func<string,string , Task> SampleDelegateProperty2 { get; set; }
+        
+        public Func<string,int,string , Task> SampleDelegateProperty3 { get; set; }
         
     }
     
@@ -20,35 +23,63 @@ public class DelegatePropertyTest
     public void CreationTest()
     {
 
-        
+        //{
+        //    MyComponent myComponent = new MyComponent
+        //    {
+        //        ComponentUniqueIdentifier = 5
+        //    };
+            
+        //    var propertyValue = DelegatePropertyHelper.ReCalculatePropertyValue(myComponent, typeof(MyComponent).GetProperty(nameof(myComponent.SampleDelegateProperty0)));
 
+        //    var delegateFunc = (Func<Task>)propertyValue;
+
+        //    delegateFunc();
+
+        //    myComponent.Client.TaskList.Count.Should().Be(1);
+        //}
+
+        //{
+        //    MyComponent myComponent = new MyComponent
+        //    {
+        //        ComponentUniqueIdentifier = 5
+        //    };
+            
+        //    var propertyValue = DelegatePropertyHelper.ReCalculatePropertyValue(myComponent, typeof(MyComponent).GetProperty(nameof(myComponent.SampleDelegateProperty1)));
+
+        //    var delegateFunc = (Func<string, Task>)propertyValue;
+
+        //    delegateFunc("abC");
+
+        //    myComponent.Client.TaskList.Count.Should().Be(1);
+        //}
+
+        
+        //{
+        //    MyComponent myComponent = new MyComponent
+        //    {
+        //        ComponentUniqueIdentifier = 5
+        //    };
+            
+        //    var propertyValue = DelegatePropertyHelper.ReCalculatePropertyValue(myComponent, typeof(MyComponent).GetProperty(nameof(myComponent.SampleDelegateProperty2)));
+
+        //    var delegateFunc = (Func<string, string, Task>)propertyValue;
+
+        //    delegateFunc("abC", "ab");
+
+        //    myComponent.Client.TaskList.Count.Should().Be(1);
+        //}
+        
         {
             MyComponent myComponent = new MyComponent
             {
                 ComponentUniqueIdentifier = 5
             };
             
-            var propertyValue = DelegatePropertyHelper.ReCalculatePropertyValue(myComponent, typeof(MyComponent).GetProperty(nameof(myComponent.SampleDelegateProperty1)));
+            var propertyValue = DelegatePropertyHelper.ReCalculatePropertyValue(myComponent, typeof(MyComponent).GetProperty(nameof(myComponent.SampleDelegateProperty3)));
 
-            var delegateFunc = (Func<string, Task>)propertyValue;
+            var delegateFunc = (Func<string,int, string, Task>)propertyValue;
 
-            delegateFunc("abC");
-
-            myComponent.Client.TaskList.Count.Should().Be(1);
-        }
-
-        
-        {
-            MyComponent myComponent = new MyComponent
-            {
-                ComponentUniqueIdentifier = 5
-            };
-            
-            var propertyValue = DelegatePropertyHelper.ReCalculatePropertyValue(myComponent, typeof(MyComponent).GetProperty(nameof(myComponent.SampleDelegateProperty2)));
-
-            var delegateFunc = (Func<string, string, Task>)propertyValue;
-
-            delegateFunc("abC", "ab");
+            delegateFunc("abC", 6, "ab");
 
             myComponent.Client.TaskList.Count.Should().Be(1);
         }
--- ReactWithDotNet/ReflectionHelper.DelegatePropertyHelper.cs
+++ ReactWithDotNet/ReflectionHelper.DelegatePropertyHelper.cs
@@ -8,27 +8,25 @@ static class DelegatePropertyHelper
 {
     public static Delegate ReCalculatePropertyValue(ReactComponentBase reactComponent, PropertyInfo propertyInfo)
     {
-        
         var genericArguments = propertyInfo.PropertyType.GetGenericArguments();
-        if (genericArguments.Length == 0 || 
-            genericArguments.Length > 4 || 
+        if (genericArguments.Length == 0 ||
+            genericArguments.Length > 4 ||
             genericArguments[^1] != typeof(Task))
         {
             throw DeveloperException($"Custom Delegate should return Task {propertyInfo.Name}");
         }
-        
+
         var argumentTypes = genericArguments.SkipLast(1).ToArray();
-        
-        
-        var methodInfoCreateDelagate = typeof(DelegatePropertyHelper).GetMethod(nameof(CreateDelegate), BindingFlags.Static| BindingFlags.NonPublic);
+
+        var methodInfoCreateDelagate = typeof(DelegatePropertyHelper).GetMethod(nameof(CreateDelegate), BindingFlags.Static | BindingFlags.NonPublic);
         if (methodInfoCreateDelagate is null)
         {
             throw new ArgumentNullException(nameof(methodInfoCreateDelagate));
         }
 
         var nameofCurryMethod = $"Curry{argumentTypes.Length}";
-        
-        var methodInfoCurry = typeof(DelegatePropertyHelper).GetMethod(nameofCurryMethod,BindingFlags.Static| BindingFlags.NonPublic);
+
+        var methodInfoCurry = typeof(DelegatePropertyHelper).GetMethod(nameofCurryMethod, BindingFlags.Static | BindingFlags.NonPublic);
         if (methodInfoCurry is null)
         {
             throw new ArgumentNullException(nameof(methodInfoCurry));
@@ -36,47 +34,48 @@ static class DelegatePropertyHelper
 
         var delegateFunc = methodInfoCreateDelagate.Invoke(null, [reactComponent, propertyInfo]);
 
-        
-        var curryMethodInfo = methodInfoCurry.MakeGenericMethod(argumentTypes);
+        if (argumentTypes.Length > 0)
+        {
+            methodInfoCurry = methodInfoCurry.MakeGenericMethod(argumentTypes);
+        }
 
-        return (Delegate) curryMethodInfo.Invoke(null, [reactComponent, delegateFunc]);
+        return (Delegate)methodInfoCurry.Invoke(null, [reactComponent, delegateFunc]);
     }
-    
+
     [Pure]
-    static Func<Task> Curry0(ReactComponentBase reactComponent, Func<object, Task> func)
+    internal static Func<Task> Curry0(ReactComponentBase reactComponent, Func<object, Task> func)
     {
-        return  () => func(reactComponent);
+        return () => func(reactComponent);
     }
-    
+
     [Pure]
-    static Func<A,Task> Curry1<A>(ReactComponentBase reactComponent, Func<object,A, Task> func)
+    internal static Func<A, Task> Curry1<A>(ReactComponentBase reactComponent, Func<object, A, Task> func)
     {
-        return  x => func(reactComponent, x);
+        return x => func(reactComponent, x);
     }
-    
+
     [Pure]
-    static Func<A,B,Task> Curry2<A,B>(ReactComponentBase reactComponent, Func<object,A,B, Task> func)
+    internal static Func<A, B, Task> Curry2<A, B>(ReactComponentBase reactComponent, Func<object, A, B, Task> func)
     {
-        return  (x,y) => func(reactComponent, x,y);
+        return (x, y) => func(reactComponent, x, y);
     }
-    
+
     [Pure]
-    static Func<A,B,C,Task> Curry3<A,B,C>(ReactComponentBase reactComponent, Func<object,A,B,C, Task> func)
+    internal static Func<A, B, C, Task> Curry3<A, B, C>(ReactComponentBase reactComponent, Func<object, A, B, C, Task> func)
     {
-        return  (x,y,z) => func(reactComponent, x,y,z);
+        return (x, y, z) => func(reactComponent, x, y, z);
     }
-    
-    
+
     static Delegate CreateDelegate(ReactComponentBase reactComponent, PropertyInfo propertyInfo)
     {
-        var methodInfo = typeof(DelegatePropertyHelper).GetMethod(nameof(DispatchDotNetCustomEvent),BindingFlags.Static| BindingFlags.NonPublic);
+        var methodInfo = typeof(DelegatePropertyHelper).GetMethod(nameof(DispatchDotNetCustomEvent), BindingFlags.Static | BindingFlags.NonPublic);
         if (methodInfo is null)
         {
             throw new NullReferenceException(nameof(methodInfo));
         }
 
         var genericArguments = propertyInfo.PropertyType.GetGenericArguments();
-        
+
         DynamicMethod dynamicMethod;
         {
             var componentType = reactComponent.GetType();
@@ -90,64 +89,47 @@ static class DelegatePropertyHelper
                 typeof(object)
             };
 
-            
             parameterTypes.AddRange(genericArguments.SkipLast(1));
 
             dynamicMethod = new(name, returnType, parameterTypes.ToArray());
         }
-        
-        var ilGenerator = dynamicMethod.GetILGenerator();
 
-        ilGenerator.Emit(OpCodes.Ldarg_0);
-        ilGenerator.Emit(OpCodes.Ldstr, propertyInfo.Name);
-        
-        ilGenerator.Emit(OpCodes.Ldc_I4,genericArguments.Length-1);
-        ilGenerator.Emit(OpCodes.Newarr, typeof(object));
-       
-        
-        for (var i = 0; i < genericArguments.Length-1; i++)
+        var il = dynamicMethod.GetILGenerator();
+
+        il.Emit(OpCodes.Ldarg_0);
+        il.Emit(OpCodes.Ldstr, propertyInfo.Name);
+
+        il.Emit(OpCodes.Ldc_I4, genericArguments.Length - 1);
+        il.Emit(OpCodes.Newarr, typeof(object));
+
+        for (var i = 0; i < genericArguments.Length - 1; i++)
         {
-            ilGenerator.Emit(OpCodes.Dup);
-            ilGenerator.Emit(OpCodes.Ldc_I4,i);
-            ilGenerator.Emit(OpCodes.Ldarg,i+1);
-            ilGenerator.Emit(OpCodes.Stelem_I);
+            il.Emit(OpCodes.Dup);
+            il.Emit(OpCodes.Ldc_I4, i);
+            il.Emit(OpCodes.Ldarg, i + 1);
+            if (genericArguments[i].IsValueType)
+            {
+                il.Emit(OpCodes.Box, genericArguments[i]);
+            }
+
+            il.Emit(OpCodes.Stelem_I);
         }
-        
-        ilGenerator.Emit(OpCodes.Call, methodInfo);
-        ilGenerator.Emit(OpCodes.Ret);
 
+        il.Emit(OpCodes.Call, methodInfo);
+        il.Emit(OpCodes.Ret);
 
-        Type targetDelegateType = null;
-        if (genericArguments.Length == 2)
+        var targetDelegateType = genericArguments.Length switch
         {
-            targetDelegateType = typeof(Func<,,>).MakeGenericType([typeof(object), genericArguments[0], typeof(Task)]);
-        }
-        else if (genericArguments.Length == 3)
-        {
-            targetDelegateType = typeof(Func<,,,>).MakeGenericType([typeof(object), genericArguments[0],genericArguments[1] ,typeof(Task)]);
-        }
-        
-        return  dynamicMethod.CreateDelegate(targetDelegateType);
+            1 => typeof(Func<,>).MakeGenericType([typeof(object), typeof(Task)]),
+            2 => typeof(Func<,,>).MakeGenericType([typeof(object), genericArguments[0], typeof(Task)]),
+            3 => typeof(Func<,,,>).MakeGenericType([typeof(object), genericArguments[0], genericArguments[1], typeof(Task)]),
+            4 => typeof(Func<,,,,>).MakeGenericType([typeof(object), genericArguments[0], genericArguments[1], genericArguments[2], typeof(Task)]),
+            _ => throw DeveloperException($"Custom Delegate should return Task {propertyInfo.Name}")
+        };
+
+        return dynamicMethod.CreateDelegate(targetDelegateType);
     }
 
-    static void AAAA(ReactComponentBase reactComponent, string h)
-    {
-        DispatchDotNetCustomEvent(reactComponent, "abc",[h]);
-    }
-    
-    static void AAAA2(ReactComponentBase reactComponent, string h, string j)
-    {
-        DispatchDotNetCustomEvent(reactComponent, "abc",[h,j]);
-    }
-    
-    static object[] AAAA2y(int i)
-    {
-        object[] a = new object[666];
-
-        return a;
-    }
-    
-    
     static Task DispatchDotNetCustomEvent(ReactComponentBase reactComponent, string propertyName, object[] eventArguments)
     {
         var senderInfo = GetEventSenderInfo(reactComponent, propertyName);
