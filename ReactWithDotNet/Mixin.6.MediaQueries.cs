bf97014560e87528491eb7859b06e74c7cf39718
beyaz <beyaz1404@gmail.com>
2024-04-14 12:16:29 +0300

    .

--- ReactWithDotNet/Mixin.6.MediaQueries.cs
+++ ReactWithDotNet/Mixin.6.MediaQueries.cs
@@ -2,18 +2,56 @@
 
 partial class Mixin
 {
-    public static StyleModifier MediaQuery(string query, Style styleForOverride)
-        => new(style => style.MediaQueries.Add(new (query, styleForOverride)));
-
-    public static StyleModifier MediaQuery(string query, params StyleModifier[] styleModifiers)
-        => MediaQuery(query, new Style(styleModifiers));
+    /// <summary>
+    ///     min-width: 1024px
+    /// </summary>
+    public static StyleModifier LG(params StyleModifier[] styleModifiers)
+    {
+        return MediaQuery("(min-width: 1024px)", new Style(styleModifiers));
+    }
 
     /// <summary>
-    ///     min-width: <paramref name="widthAsPixel"/> + 'px'
+    ///     min-width: 768px
     /// </summary>
-    public static StyleModifier WhenMediaSizeGreaterThan(int widthAsPixel,params StyleModifier[] styleModifiers)
-        => MediaQuery($"(min-width: {widthAsPixel}px)", styleModifiers);
-    
+    public static StyleModifier MD(params StyleModifier[] styleModifiers)
+    {
+        return MediaQuery("(min-width: 768px)", new Style(styleModifiers));
+    }
+
+    public static StyleModifier MediaQuery(string query, Style styleForOverride)
+    {
+        return new(style => style.MediaQueries.Add(new(query, styleForOverride)));
+    }
+
+    public static StyleModifier MediaQuery(string query, params StyleModifier[] styleModifiers)
+    {
+        return MediaQuery(query, new Style(styleModifiers));
+    }
+
+    /// <summary>
+    ///     min-width: 640px
+    /// </summary>
+    public static StyleModifier SM(params StyleModifier[] styleModifiers)
+    {
+        return MediaQuery("(min-width: 640px)", new Style(styleModifiers));
+    }
+
+    /// <summary>
+    ///     (min-width: <paramref name="minWidthAsPixel" /> + 'px') and (max-width: <paramref name="maxWidthAsPixel" /> + 'px')
+    /// </summary>
+    public static StyleModifier WhenMediaSizeBetween(int minWidthAsPixel, int maxWidthAsPixel, params StyleModifier[] styleModifiers)
+    {
+        return MediaQuery($"(min-width: {minWidthAsPixel}px) and (max-width: {maxWidthAsPixel}px)", styleModifiers);
+    }
+
+    /// <summary>
+    ///     min-width: <paramref name="widthAsPixel" /> + 'px'
+    /// </summary>
+    public static StyleModifier WhenMediaSizeGreaterThan(int widthAsPixel, params StyleModifier[] styleModifiers)
+    {
+        return MediaQuery($"(min-width: {widthAsPixel}px)", styleModifiers);
+    }
+
     /// <summary>
     ///     Sample Usage:
     ///     <code>
@@ -24,15 +62,15 @@ partial class Mixin
     {
         return WhenMediaSizeGreaterThan(ConvertToNumber(breakpoint), styleModifiers);
     }
-    
+
     /// <summary>
-    ///     max-width: <paramref name="widthAsPixel"/> + 'px'
+    ///     max-width: <paramref name="widthAsPixel" /> + 'px'
     /// </summary>
     public static StyleModifier WhenMediaSizeLessThan(int widthAsPixel, params StyleModifier[] styleModifiers)
-        => MediaQuery($"(max-width: {widthAsPixel}px)", styleModifiers);
+    {
+        return MediaQuery($"(max-width: {widthAsPixel}px)", styleModifiers);
+    }
 
-    
-    
     /// <summary>
     ///     Sample Usage:
     ///     <code>
@@ -42,6 +80,22 @@ partial class Mixin
     public static StyleModifier WhenMediaSizeLessThan(Func<StyleModifier[], StyleModifier> breakpoint, params StyleModifier[] styleModifiers)
     {
         return WhenMediaSizeLessThan(ConvertToNumber(breakpoint), styleModifiers);
+    }
+
+    /// <summary>
+    ///     min-width: 1280px
+    /// </summary>
+    public static StyleModifier XL(params StyleModifier[] styleModifiers)
+    {
+        return MediaQuery("(min-width: 1280px)", new Style(styleModifiers));
+    }
+
+    /// <summary>
+    ///     min-width: 1536px
+    /// </summary>
+    public static StyleModifier XXL(params StyleModifier[] styleModifiers)
+    {
+        return MediaQuery("(min-width: 1536px)", new Style(styleModifiers));
     }
 
     static int ConvertToNumber(Func<StyleModifier[], StyleModifier> breakpoint)
@@ -73,42 +127,4 @@ partial class Mixin
 
         throw DeveloperException($"{breakpoint} parameter can be SM or MD or LG or XML or XXL");
     }
-    
-    /// <summary>
-    ///     (min-width: <paramref name="minWidthAsPixel"/> + 'px') and (max-width: <paramref name="maxWidthAsPixel"/> + 'px')
-    /// </summary>
-    public static StyleModifier WhenMediaSizeBetween(int minWidthAsPixel,int maxWidthAsPixel, params StyleModifier[] styleModifiers)
-        => MediaQuery($"(min-width: {minWidthAsPixel}px) and (max-width: {maxWidthAsPixel}px)", styleModifiers);
-    
-    
-    /// <summary>
-    ///     min-width: 640px
-    /// </summary>
-    public static StyleModifier SM(params StyleModifier[] styleModifiers)
-        => MediaQuery("(min-width: 640px)", new Style(styleModifiers));
-    
-    /// <summary>
-    ///     min-width: 768px
-    /// </summary>
-    public static StyleModifier MD(params StyleModifier[] styleModifiers)
-        => MediaQuery("(min-width: 768px)", new Style(styleModifiers));
-    
-    /// <summary>
-    ///     min-width: 1024px
-    /// </summary>
-    public static StyleModifier LG(params StyleModifier[] styleModifiers)
-        => MediaQuery("(min-width: 1024px)", new Style(styleModifiers));
-    
-    /// <summary>
-    ///     min-width: 1280px
-    /// </summary>
-    public static StyleModifier XL(params StyleModifier[] styleModifiers)
-        => MediaQuery("(min-width: 1280px)", new Style(styleModifiers));
-    
-    /// <summary>
-    ///     min-width: 1536px
-    /// </summary>
-    public static StyleModifier XXL(params StyleModifier[] styleModifiers)
-        => MediaQuery("(min-width: 1536px)", new Style(styleModifiers));
-    
 }
