c57dc7b085ec6c73341169059703e90b0cea3b3c
beyaz <beyaz1404@gmail.com>
2024-04-27 14:50:47 +0300

    .

--- ReactWithDotNet/HtmlTextGenerator.cs
+++ ReactWithDotNet/HtmlTextGenerator.cs
@@ -163,7 +163,7 @@ static class HtmlTextGenerator
         static IEnumerable<char> convertChar(char c, int index)
         {
             if (char.IsUpper(c) && index != 0) yield return '-';
-            yield return char.ToLower(c, new CultureInfo("EN-us"));
+            yield return char.ToLower(c, CultureInfo_en_US);
         }
 
         return string.Concat(dotnetPropertyName.SelectMany(convertChar));
