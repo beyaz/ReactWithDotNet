600ce4cbb1e486a8c043e67c37f00f2cb0545664
beyaz <beyaz1404@gmail.com>
2024-05-03 02:25:03 +0300

    .

---
+++ ReactWithDotNet/ThirdPartyLibraries/split_js/Editor.cs
@@ -0,0 +1,22 @@
+﻿namespace ReactWithDotNet.ThirdPartyLibraries.split_js;
+
+public sealed class Split : ThirdPartyReactComponent
+{
+    [ReactProp]
+    public int? minSize { get; set; }
+    
+    [ReactProp]
+    public int? maxSize { get; set; }
+    
+    [ReactProp]
+    public int? gutterSize { get; set; }
+
+    [ReactProp]
+    public string gutterAlign { get; set; }
+
+    [ReactProp]
+    public int? snapOffset { get; set; }
+    
+    [ReactProp]
+    public int? dragInterval { get; set; }
+}
