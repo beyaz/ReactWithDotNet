f4815b92d880f5a39f0f2da4d80c7f1db08d5500
beyaz <beyaz1404@gmail.com>
2024-06-03 23:05:28 +0300

    .

--- ReactWithDotNet.WebSite/Components/ArrowUpDownIcon.cs
+++ ReactWithDotNet.WebSite/Components/ArrowUpDownIcon.cs
@@ -1,24 +1,26 @@
 ﻿namespace ReactWithDotNet.WebSite.Components;
 
-class ArrowUpDownIcon : PureComponent
+sealed class ArrowUpDownIcon : PureComponent
 {
-    public bool IsArrowUp { get; set; }
+    public bool IsArrowUp { get; init; }
+
+    public int Size { get; init; } = 24;
 
     protected override Element render()
     {
-        var arrowDown = new svg(svg.ViewBox(0, 0, 24, 24), svg.Width(24),svg.Height(24), Transition("all", 400))
+        var arrowDown = new svg(svg.ViewBox(0, 0, 24, 24), svg.Size(Size), Transition("all", 400))
         {
             new path { d = "M8.12 9.29 12 13.17l3.88-3.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-4.59 4.59c-.39.39-1.02.39-1.41 0L6.7 10.7a.9959.9959 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0z" }
         };
 
         if (IsArrowUp)
         {
-            return arrowDown+WithStyle([
+            return arrowDown + WithStyle([
                 Transform("rotate(-180deg)")
             ]);
         }
-        
-        return arrowDown+WithStyle([
+
+        return arrowDown + WithStyle([
             Transform("rotate(0deg)")
         ]);
     }
