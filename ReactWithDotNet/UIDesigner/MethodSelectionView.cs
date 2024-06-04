24c0c3ba595382ecf39520eb52a0bb23831d7c25
beyaz <beyaz1404@gmail.com>
2024-06-05 00:21:30 +0300

    .

--- ReactWithDotNet/UIDesigner/MethodSelectionView.cs
+++ ReactWithDotNet/UIDesigner/MethodSelectionView.cs
@@ -93,7 +93,7 @@ sealed class MethodSelectionView : Component<MethodSelectionView.State>
             canShowCollapseIcon ?
             new legend(DisplayFlexRowCentered, OnClick(_ => Task.FromResult(state = state with{IsCollapsed = !state.IsCollapsed})))
             {
-                new ArrowUpDownIcon{ IsArrowUp = state.IsCollapsed, Size = 16}
+                new ArrowRightDownIcon{ IsArrowRight = state.IsCollapsed, Size = 16}
             }:null,
             
             content
@@ -255,9 +255,9 @@ sealed class MethodSelectionView : Component<MethodSelectionView.State>
         return Task.CompletedTask;
     }
     
-    sealed class ArrowUpDownIcon : PureComponent
+    sealed class ArrowRightDownIcon : PureComponent
     {
-        public bool IsArrowUp { get; init; }
+        public bool IsArrowRight { get; init; }
 
         public int Size { get; init; } = 24;
 
@@ -268,10 +268,10 @@ sealed class MethodSelectionView : Component<MethodSelectionView.State>
                 new path { d = "M8.12 9.29 12 13.17l3.88-3.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-4.59 4.59c-.39.39-1.02.39-1.41 0L6.7 10.7a.9959.9959 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0z" }
             };
 
-            if (IsArrowUp)
+            if (IsArrowRight)
             {
                 return arrowDown + WithStyle([
-                    Transform("rotate(-180deg)")
+                    Transform("rotate(-90deg)")
                 ]);
             }
 
