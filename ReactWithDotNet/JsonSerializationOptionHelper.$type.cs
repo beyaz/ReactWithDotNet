4dbee4ace6479b3cd18258d32f3dd119653c4d5a
beyaz <beyaz1404@gmail.com>
2024-06-16 11:38:09 +0300

    .

--- ReactWithDotNet/JsonSerializationOptionHelper.$type.cs
+++ ReactWithDotNet/JsonSerializationOptionHelper.$type.cs
@@ -1,38 +1,17 @@
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace ReactWithDotNet;
-
-
 
 partial class JsonSerializationOptionHelper
 {
     sealed class JsonConverterFactoryForCommounUsage : JsonConverterFactory
     {
         internal static readonly JsonConverterFactoryForCommounUsage Instance = new();
-
-        record TypeSerializationInfo
-        {
-            public bool CanConvert { get; init; }
-            
-            public IReadOnlyList<PropertyInfoCalculated> PropertiesAsList { get; init; }
-            
-            public IReadOnlyDictionary<string, PropertyInfoCalculated> PropertiesAsDictionary { get; init; }
-            
-            public string TypeKey { get; init; }
-
-            public sealed record PropertyInfoCalculated
-            {
-                public PropertyInfo PropertyInfo{ get; init; }
-                public Func<object, object> GetFunction{ get; init; }
-                public Action<object, object> SetFunction{ get; init; }
-                public string JsonPropertyName { get; init; }
-                public object DefaultValue { get; init; }
-            }
-        }
 
         static readonly ConcurrentDictionary<Type, TypeSerializationInfo> typeMap = [];
 
@@ -42,7 +21,7 @@ partial class JsonSerializationOptionHelper
             {
                 return info.CanConvert;
             }
-            
+
             var canConvert = canConvertImpl(typeToConvert);
             if (canConvert is false)
             {
@@ -56,13 +35,13 @@ partial class JsonSerializationOptionHelper
                 .Where(p => p.GetCustomAttribute<JsonIgnoreAttribute>() is null)
                 .Select(toCalculatedProperty)
                 .ToList();
-            
+
             typeMap.TryAdd(typeToConvert, new()
             {
-                CanConvert = true,
-                TypeKey=$"{typeToConvert.FullName}, {typeToConvert.Assembly.GetName().Name}",
-                PropertiesAsList = properties,
-                PropertiesAsDictionary = properties.ToDictionary(x=>x.JsonPropertyName)
+                CanConvert             = true,
+                TypeKey                = $"{typeToConvert.FullName}, {typeToConvert.Assembly.GetName().Name}",
+                PropertiesAsList       = properties,
+                PropertiesAsDictionary = properties.ToDictionary(x => x.JsonPropertyName)
             });
             return true;
 
@@ -72,7 +51,7 @@ partial class JsonSerializationOptionHelper
                 {
                     return false;
                 }
-                
+
                 // ignore core types
                 if (typeToConvert.Assembly.GetName().Name == nameof(ReactWithDotNet))
                 {
@@ -84,22 +63,22 @@ partial class JsonSerializationOptionHelper
                 {
                     return false;
                 }
-                
+
                 return typeToConvert.IsClass;
-                
+
                 static bool isEnumerable(Type type)
                 {
                     return typeof(IEnumerable).IsAssignableFrom(type);
                 }
             }
-            
+
             static TypeSerializationInfo.PropertyInfoCalculated toCalculatedProperty(PropertyInfo propertyInfo)
             {
                 var jsonPropertyName = propertyInfo.GetCustomAttribute<JsonPropertyNameAttribute>()?.Name ?? propertyInfo.Name;
-                
-                return new ()
+
+                return new()
                 {
-                    PropertyInfo = propertyInfo,
+                    PropertyInfo     = propertyInfo,
                     GetFunction      = ReflectionHelper.CreateGetFunction(propertyInfo),
                     SetFunction      = ReflectionHelper.CreateSetFunction(propertyInfo),
                     DefaultValue     = propertyInfo.PropertyType.IsValueType ? Activator.CreateInstance(propertyInfo.PropertyType) : null,
@@ -112,49 +91,22 @@ partial class JsonSerializationOptionHelper
         {
             return (JsonConverter)Activator.CreateInstance(typeof(PolymorphicJsonConverter<>).MakeGenericType(typeToConvert));
         }
-        
+
         sealed class PolymorphicJsonConverter<T> : JsonConverter<T> where T : class
         {
-            public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
-            {
-                writer.WriteStartObject();
-
-                var type = value.GetType();
-
-                var typeSerializationInfo = typeMap[type];
-
-                writer.WriteString("$type", typeSerializationInfo.TypeKey);
-                
-                foreach (var item in typeSerializationInfo.PropertiesAsList)
-                {
-                    var propertyValue = item.GetFunction(value);
-
-                    if (propertyValue == item.DefaultValue)
-                    {
-                        continue;
-                    }
-                    
-                    writer.WritePropertyName(item.JsonPropertyName);
-                    
-                    JsonSerializer.Serialize(writer, propertyValue, options);
-                }
-
-                writer.WriteEndObject();
-            }
-            
             public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             {
                 var typeSerializationInfo = typeMap[typeToConvert];
-                
+
                 object instance = null;
-                    
+
                 while (reader.Read())
                 {
                     if (reader.TokenType == JsonTokenType.EndObject)
                     {
                         return (T)instance;
                     }
-                    
+
                     var propertyName = reader.GetString();
 
                     if (propertyName == "$type")
@@ -166,26 +118,81 @@ partial class JsonSerializationOptionHelper
                         {
                             throw new JsonException("Missing type discriminator property.");
                         }
+
                         var type = Type.GetType(typeKey);
                         if (type is null)
                         {
                             throw new JsonException($"Missing type: {typeKey}");
                         }
+
                         instance = Activator.CreateInstance(type);
-                            
+
                         continue;
                     }
-                        
+
                     var propertyInfoCalculated = typeSerializationInfo.PropertiesAsDictionary[propertyName];
 
                     var propertyInfo = propertyInfoCalculated.PropertyInfo;
-                        
+
                     var propertyValue = JsonSerializer.Deserialize(ref reader, propertyInfo.PropertyType, options);
-                        
+
                     propertyInfoCalculated.SetFunction(instance, propertyValue);
                 }
 
                 return (T)instance;
+            }
+
+            public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+            {
+                writer.WriteStartObject();
+
+                var type = value.GetType();
+
+                var typeSerializationInfo = typeMap[type];
+
+                writer.WriteString("$type", typeSerializationInfo.TypeKey);
+
+                var propertyList = CollectionsMarshal.AsSpan(typeSerializationInfo.PropertiesAsList);
+
+                var count = propertyList.Length;
+
+                for (var i = 0; i < count; i++)
+                {
+                    var item = propertyList[i];
+
+                    var propertyValue = item.GetFunction(value);
+
+                    if (propertyValue == item.DefaultValue)
+                    {
+                        continue;
+                    }
+
+                    writer.WritePropertyName(item.JsonPropertyName);
+
+                    JsonSerializer.Serialize(writer, propertyValue, options);
+                }
+
+                writer.WriteEndObject();
+            }
+        }
+
+        record TypeSerializationInfo
+        {
+            public bool CanConvert { get; init; }
+
+            public List<PropertyInfoCalculated> PropertiesAsList { get; init; }
+
+            public IReadOnlyDictionary<string, PropertyInfoCalculated> PropertiesAsDictionary { get; init; }
+
+            public string TypeKey { get; init; }
+
+            public sealed record PropertyInfoCalculated
+            {
+                public PropertyInfo PropertyInfo { get; init; }
+                public Func<object, object> GetFunction { get; init; }
+                public Action<object, object> SetFunction { get; init; }
+                public string JsonPropertyName { get; init; }
+                public object DefaultValue { get; init; }
             }
         }
     }
