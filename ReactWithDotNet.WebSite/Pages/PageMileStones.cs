7b34fc64a31ca01b8b93e8e01f6371e7d10200ee
beyaz <beyaz1404@gmail.com>
2024-08-07 00:08:50 +0300

    .

--- ReactWithDotNet.WebSite/Pages/PageMileStones.cs
+++ ReactWithDotNet.WebSite/Pages/PageMileStones.cs
@@ -1,15 +1,37 @@
 ﻿namespace ReactWithDotNet.WebSite.Pages;
 
-sealed class PageMileStones : PureComponent
+sealed class PageMilestones : PureComponent
 {
     protected override Element render()
     {
-        return new FlexColumn
+        return new FlexColumn(Gap(32))
         {
             new div
             {
-                new h4{ "Core Concept" },
-                new Progressbar{ Value = 95} + Size(300,50)
+                new h4{ "Milestone 1: Core Concept" },
+                new Progressbar{ Value = 95},
+                new p
+                {
+                    "Core react conceps like Functional components, Class components, PureComponents and communication react js client and .net core server "
+                }
+            },
+            new div
+            {
+                new h4{ "Milestone 2: Creating wrapper classes of popular react libraries like MUI, AntDesign" },
+                new Progressbar{ Value = 5},
+                new p
+                {
+                    "When this milestone completed, you can use MUI, or other famous components in your project. This insfrastructure is ready to use. We are plan to generate wrapper classes automatically."
+                },
+            },
+            new div
+            {
+                new h4{ "Milestone 3: Use Client option" },
+                new Progressbar{ Value = 3},
+                new p
+                {
+                    "After this part finished, you can write js code in c# language. I'm planning mini .net runtime in js language. My plan is not to build a new c# to js compiler. My plan is creating a new MSIL code runner in js language. Not getting all .net core assemblies. Only required part of MSIL codes will be downloaded into client side."
+                }
             }
         };
 
@@ -21,11 +43,11 @@ sealed class PageMileStones : PureComponent
         
         protected override Element render()
         {
-            return new div(SizeFull, BorderRadius(8), Border(1, solid, Gray300))
+            return new div(SizeFull, BorderRadius(4), Border(1, solid, Gray300), Size(300,20))
             {
-                new div(Width(Value, 100), Background(Gray400), HeightFull)
+                new FlexRowCentered(Width(Value, 100), Background(linear_gradient(90,Gray100, Gray300)), HeightFull, FontSize11)
                 {
-
+                    "%"+Value
                 }
             };
         }
