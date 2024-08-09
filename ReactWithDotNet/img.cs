10d09cebeb923f827b70ba9793dcb6039382d0a1
beyaz <beyaz1404@gmail.com>
2024-08-09 19:49:59 +0300

    .

--- ReactWithDotNet/img.cs
+++ ReactWithDotNet/img.cs
@@ -13,4 +13,15 @@ partial class Mixin
     ///     img.src = <paramref name="src" />
     /// </summary>
     public static HtmlElementModifier Src(string src) => CreateHtmlElementModifier<img>(element => element.src = src);
+
+
+    /// <summary>
+    ///     img.loading = 'lazy'
+    /// </summary>
+    public static HtmlElementModifier LoadingLazy => img.Loading("lazy");
+    
+    /// <summary>
+    ///     img.loading = 'eager'
+    /// </summary>
+    public static HtmlElementModifier LoadingEager => img.Loading("eager");
 }
