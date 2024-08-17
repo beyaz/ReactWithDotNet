650dcfef9f15f9cc37b6c91970641e545b4b7ec3
beyaz <beyaz1404@gmail.com>
2024-08-17 22:17:21 +0300

    .

---
+++ ReactWithDotNet.WebSite/HeaderComponents/MainPageFooter.cs
@@ -0,0 +1,16 @@
+﻿namespace ReactWithDotNet.WebSite.HeaderComponents;
+
+sealed class MainPageFooter : PureComponent
+{
+    protected override Element render()
+    {
+        return new footer(BorderTop(1, solid, Gray100), Height(50), WidthFull, Background("white"), PositionFixed, Bottom(0))
+        {
+            DisplayFlexRowCentered,
+            new HighlightedText
+            {
+                Text = "React [\u2665] .Net"
+            }
+        };
+    }
+}
--- ReactWithDotNet.WebSite/HeaderComponents/MainPageHeader.cs
+++ ReactWithDotNet.WebSite/HeaderComponents/MainPageHeader.cs
@@ -15,18 +15,3 @@ sealed class MainPageHeader : PureComponent
         };
     }
 }
-
-sealed class MainPageFooter : PureComponent
-{
-    protected override Element render()
-    {
-        return new footer(BorderTop(1, solid, Gray100), Height(50), WidthFull, Background("white"), PositionFixed, Bottom(0))
-        {
-            DisplayFlexRowCentered,
-            new HighlightedText
-            {
-                Text = "React [\u2665] .Net"
-            }
-        };
-    }
-}
