c3b51666a2175c3b63890b1ccc707a48dc681fc8
beyaz <beyaz1404@gmail.com>
2024-08-25 15:47:26 +0300

    .

--- ReactWithDotNet.WebSite/Pages/PageMileStones.cs
+++ ReactWithDotNet.WebSite/Pages/PageMileStones.cs
@@ -4,47 +4,49 @@ sealed class PageMilestones : PureComponent
 {
     protected override Element render()
     {
-        return new FlexColumn(Gap(32))
+        return new FlexColumnCentered(WidthFull)
         {
-            new MilestoneContainer
+            new FlexColumn(Gap(32),MaxWidth(820))
             {
-                new h5{ "Milestone 1: Core Concept" },
-                new Progressbar{ Value = 95},
-                new p
+                new MilestoneContainer
                 {
-                    "Core react conceps like Functional components, Class components, PureComponents and communication react js client and .net core server."
-                }
-            },
-            new MilestoneContainer
-            {
-                new h5{ "Milestone 2: Creating wrapper classes of popular react libraries like MUI, AntDesign." },
-                new Progressbar{ Value = 5},
-                new p
-                {
-                    "When this milestone completed, you can use MUI, or other famous components in your project. This insfrastructure is ready to use. We are plan to generate wrapper classes automatically."
+                    new h5{ "Milestone 1: Core Concept" },
+                    new Progressbar{ Value = 95},
+                    new p
+                    {
+                        "Core react conceps like Functional components, Class components, PureComponents and communication react js client and .net core server."
+                    }
                 },
-            },
-            new MilestoneContainer
-            {
-                new h5{ "Milestone 3: Use Client option" },
-                new Progressbar{ Value = 3},
-                new p
+                new MilestoneContainer
                 {
-                    "After this part finished, you can write js code in c# language. I'm planning mini .net runtime in js language. My plan is not to build a new c# to js compiler. My plan is creating a new MSIL code runner in js language. Not getting all .net core assemblies. Only required part of MSIL codes will be downloaded into client side."
-                }
-            },
+                    new h5{ "Milestone 2: Creating wrapper classes of popular react libraries like MUI, AntDesign." },
+                    new Progressbar{ Value = 5},
+                    new p
+                    {
+                        "When this milestone completed, you can use MUI, or other famous components in your project. This insfrastructure is ready to use. We are plan to generate wrapper classes automatically."
+                    },
+                },
+                new MilestoneContainer
+                {
+                    new h5{ "Milestone 3: Use Client option" },
+                    new Progressbar{ Value = 3},
+                    new p
+                    {
+                        "After this part finished, you can write js code in c# language. I'm planning mini .net runtime in js language. My plan is not to build a new c# to js compiler. My plan is creating a new MSIL code runner in js language. Not getting all .net core assemblies. Only required part of MSIL codes will be downloaded into client side."
+                    }
+                },
             
-            new MilestoneContainer
-            {
-                new h5{ "Milestone 4: React Native Integration" },
-                new Progressbar{ Value = 0},
-                new p
+                new MilestoneContainer
                 {
-                    "React native user interfaces will be build by server driven ui."
+                    new h5{ "Milestone 4: React Native Integration" },
+                    new Progressbar{ Value = 0},
+                    new p
+                    {
+                        "React native user interfaces will be build by server driven ui."
+                    }
                 }
             }
         };
-
     }
 
     class MilestoneContainer : PureComponent
