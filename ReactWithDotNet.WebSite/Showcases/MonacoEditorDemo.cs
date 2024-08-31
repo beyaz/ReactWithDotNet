e28fca84a0852eae33f0146b9acb7ad6600836c3
beyaz <beyaz1404@gmail.com>
2024-09-01 01:15:33 +0300

    .

--- ReactWithDotNet.WebSite/Showcases/MonacoEditorDemo.cs
+++ ReactWithDotNet.WebSite/Showcases/MonacoEditorDemo.cs
@@ -1,21 +1,12 @@
-﻿using System.Threading.Tasks;
-using ReactWithDotNet.ThirdPartyLibraries.MonacoEditorReact;
-
+﻿using ReactWithDotNet.ThirdPartyLibraries.MonacoEditorReact;
 
 namespace ReactWithDotNet.WebSite.Showcases;
 
-class MonacoEditorDemoState
-{
-    public string Content { get; set; }
-    
-    public int LetterCount { get; set; }
-}
-class MonacoEditorDemo : Component<MonacoEditorDemoState>
+class MonacoEditorDemo : Component<MonacoEditorDemo.State>
 {
     protected override Task constructor()
     {
-
-        state = new MonacoEditorDemoState
+        state = new()
         {
             Content = """
                       {
@@ -25,9 +16,9 @@ class MonacoEditorDemo : Component<MonacoEditorDemoState>
                       }
                       """
         };
-        
+
         OnKeypressFinished();
-        
+
         return Task.CompletedTask;
     }
 
@@ -35,21 +26,21 @@ class MonacoEditorDemo : Component<MonacoEditorDemoState>
     {
         return new FlexColumn
         {
-            new link{href = "https://fonts.cdnfonts.com/css/ibm-plex-mono-3", rel = "stylesheet"},
+            new link { href = "https://fonts.cdnfonts.com/css/ibm-plex-mono-3", rel = "stylesheet" },
             new Editor
             {
                 width           = "640px",
                 height          = "360px",
                 defaultLanguage = "json",
-          
-                valueBind                = ()=>state.Content,
+
+                valueBind                = () => state.Content,
                 valueBindDebounceTimeout = 500,
                 valueBindDebounceHandler = OnKeypressFinished,
 
                 options =
                 {
-                    renderLineHighlight ="none",
-                    fontFamily          ="'IBM Plex Mono Medium', 'Courier New', monospace",
+                    renderLineHighlight = "none",
+                    fontFamily          = "'IBM Plex Mono Medium', 'Courier New', monospace",
                     minimap             = new { enabled            = false },
                     unicodeHighlight    = new { showExcludeOptions = false }
                 }
@@ -64,7 +55,14 @@ class MonacoEditorDemo : Component<MonacoEditorDemoState>
     Task OnKeypressFinished()
     {
         state.LetterCount = state.Content.Length;
-        
+
         return Task.CompletedTask;
+    }
+
+    internal sealed record State
+    {
+        public string Content { get; set; }
+
+        public int LetterCount { get; set; }
     }
 }
