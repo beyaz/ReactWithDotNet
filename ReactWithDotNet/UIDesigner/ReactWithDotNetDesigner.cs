99a86feb9c387f9dc558d05bfd652f2166f67be4
beyaz <beyaz1404@gmail.com>
2024-09-07 20:44:08 +0300

    .

--- ReactWithDotNet/UIDesigner/ReactWithDotNetDesigner.cs
+++ ReactWithDotNet/UIDesigner/ReactWithDotNetDesigner.cs
@@ -53,20 +53,6 @@ sealed class HotReloadListener : Component<HotReloadListener.State>
 
 public sealed class ReactWithDotNetDesigner : Component<ReactWithDotNetDesignerModel>
 {
-    sealed class IconLeft : PureComponent
-    {
-        public string Color { get; init; } = "#c5d7e8";
-        
-        protected override Element render()
-        {
-            return new svg(ViewBox(0, 0, 50, 50), svg.Size(16))
-            {
-                new path { fill = Color,  d = "M25 1C11.767 1 1 11.767 1 25s10.767 24 24 24 24-10.767 24-24S38.233 1 25 1zm0 46C12.869 47 3 37.131 3 25S12.869 3 25 3s22 9.869 22 22-9.869 22-22 22z" },
-                new path { fill = Color,  d = "M29.293 10.293 14.586 25l14.707 14.707 1.414-1.414L17.414 25l13.293-13.293z" }
-            };
-        }
-    }
-    
     public static string UrlPath => "/$";
 
     public int UpdatingProgress { get; set; }
@@ -287,12 +273,11 @@ public sealed class ReactWithDotNetDesigner : Component<ReactWithDotNetDesignerM
         {
             new div(PositionAbsolute, Top(0), state.PropertyPanelIsClosed ? MarginLeft(7) : Right(-8), Size(16), When(state.PropertyPanelIsClosed, PositionSticky))
             {
-                UpdatingProgress is > 0 and <= 100 ? new LoadingIcon() + Size(16) :
-                    
-                new IconLeft { Color = "#afafaf"} + (state.PropertyPanelIsClosed ? Rotate("180deg") : null),
-                
+                UpdatingProgress is > 0 and <= 100 ? new IconLoading() + Size(16) :
+                    new IconLeft { Color = "#afafaf" } + (state.PropertyPanelIsClosed ? Rotate("180deg") : null),
+
                 OnClick(state.PropertyPanelIsClosed ? OpenPropertyPanel : ClosePropertyPanel),
-                
+
                 CursorDefault
             },
 
@@ -652,8 +637,22 @@ public sealed class ReactWithDotNetDesigner : Component<ReactWithDotNetDesignerM
         return Task.CompletedTask;
     }
 
+    sealed class IconLeft : PureComponent
+    {
+        public string Color { get; init; } = "#c5d7e8";
+
+        protected override Element render()
+        {
+            return new svg(ViewBox(0, 0, 50, 50), svg.Size(16))
+            {
+                new path { fill = Color, d = "M25 1C11.767 1 1 11.767 1 25s10.767 24 24 24 24-10.767 24-24S38.233 1 25 1zm0 46C12.869 47 3 37.131 3 25S12.869 3 25 3s22 9.869 22 22-9.869 22-22 22z" },
+                new path { fill = Color, d = "M29.293 10.293 14.586 25l14.707 14.707 1.414-1.414L17.414 25l13.293-13.293z" }
+            };
+        }
+    }
+
     // Taken from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_loader
-    class LoadingIcon : PureComponent
+    class IconLoading : PureComponent
     {
         protected override Element render()
         {
