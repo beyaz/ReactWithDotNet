c75e6209c88eab6898a54cca6c4480423c9ddc41
beyaz <beyaz1404@gmail.com>
2024-09-14 23:59:23 +0300

    .

---
+++ ReactWithDotNet.WebSite/Components/GradientText.cs
@@ -0,0 +1,18 @@
+﻿namespace ReactWithDotNet.WebSite.Components;
+
+sealed class GradientText : PureComponent
+{
+    protected override Element render()
+    {
+        return new span
+        {
+            children,
+            
+            PaddingLeftRight(3),
+            WebkitTextFillColor(transparent),
+            Background(linear_gradientTo("right", Blue400, Blue600)),
+            BackgroundClipText,
+            WebkitBackgroundClipText
+        };
+    }
+}
