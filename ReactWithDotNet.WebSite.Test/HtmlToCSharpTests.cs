67d87e6e02ea2ac24b280c66e3d75bd9aad4e3e9
beyaz <beyaz1404@gmail.com>
2024-09-21 09:23:22 +0300

    .

--- ReactWithDotNet.WebSite.Test/HtmlToCSharpTests.cs
+++ ReactWithDotNet.WebSite.Test/HtmlToCSharpTests.cs
@@ -102,75 +102,28 @@ public class HtmlToCSharpTests
     [TestMethod]
     public void _1()
     {
+
+
+
         Assert("""
-               <a target='_blank' />
+               <div aria-hidden="true"   data-testid="AcUnitIcon"  style = "display: flex; flexDirection: row; color: blue;">
+
+               </div>
                """,
                """
-               new a { TargetBlank }
+               new FlexRow(Aria("hidden", "true"), Data("testid", "AcUnitIcon"), Color("blue"))
                """);
 
-        //Assert("""
-        //       <a target='_blank' style = "color:rgb(28, 32, 37);border-radius:12px;"/>
-        //       """,
-        //       """
-        //       new a { TargetBlank, Color(rgb(28, 32, 37)), BorderRadius(12) }
-        //       """);
 
-        //Assert("""
-        //       <a target='_blank'  aria-hidden="true"   data-testid="AcUnitIcon"  style = "color:rgb(28, 32, 37);border-radius:12px;"/>
-        //       """,
-        //       """
-        //       new a
-        //       {
-        //           Aria("hidden", "true"),
-        //           Data("testid", "AcUnitIcon"),
-        //           TargetBlank,
-        //           Color(rgb(28, 32, 37)),
-        //           BorderRadius(12)
-        //       }
-        //       """);
 
-        //Assert("""
-        //       <a target='_blank'  aria-hidden="true"   data-testid="AcUnitIcon"  style = "color:rgb(28, 32, 37);border-radius:12px;">
-        //       xyz
-        //       </a>
-        //       """,
-        //       """
-        //       new a
-        //       {
-        //           "xyz",
-        //           Aria("hidden", "true"),
-        //           Data("testid", "AcUnitIcon"),
-        //           TargetBlank,
-        //           Color(rgb(28, 32, 37)),
-        //           BorderRadius(12)
-        //       }
-        //       """);
+        Assert("""
+               <div style="width: 96.7px; padding-top: 1px; padding-bottom: 3px; padding-left: 4px; padding-right: 2px;">
 
-        //Assert("""
-        //       <div aria-hidden="true"   data-testid="AcUnitIcon"  style = "display: flex; flexDirection: row; color: blue;">
-
-        //       </div>
-        //       """,
-        //       """
-        //       new FlexRow { Aria("hidden", "true"), Data("testid", "AcUnitIcon"), Color("blue") }
-        //       """);
-
-        //Assert("""
-        //       <div style='color: blue; font-size: 14px;'>abc</div>
-        //       """,
-        //       """
-        //       new div { "abc", Color("blue"), FontSize14 }
-        //       """);
-
-        //Assert("""
-        //       <div style="width: 96.7px; padding-top: 1px; padding-bottom: 3px; padding-left: 4px; padding-right: 2px;">
-
-        //       </div>
-        //       """,
-        //       """
-        //       new div { Width(96.7), Padding(1, 2, 3, 4) }
-        //       """);
+               </div>
+               """,
+               """
+               new div(Width(96.7), Padding(1, 2, 3, 4))
+               """);
 
         //Assert("""
         //       <div style="width: 246.7px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
