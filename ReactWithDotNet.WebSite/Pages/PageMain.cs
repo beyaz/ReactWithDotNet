e9c0e32d7e6d683d7d3d71eee211f52aae498b4d
beyaz <beyaz1404@gmail.com>
2024-10-05 23:08:15 +0300

    .

--- ReactWithDotNet.WebSite/Pages/PageMain.cs
+++ ReactWithDotNet.WebSite/Pages/PageMain.cs
@@ -30,8 +30,6 @@ class PageMain : PureComponent
                 {
                     LineHeight(50), LG(LineHeight(60)),
 
-                    // new HighlightedText { Text = "Write [react.js]  application in [c#]  language" }
-
                     "Write ", new GradientText { "react.js" }, "  application in ", new GradientText { "c#" }, "  language"
                 },
 
