0c5495a816932afc64dc050868a58bcc17598416
beyaz <beyaz1404@gmail.com>
2024-10-22 22:54:23 +0300

    .

--- ReactWithDotNet/JsClientEngine/index.jsx
+++ ReactWithDotNet/JsClientEngine/index.jsx
@@ -18,7 +18,9 @@ import "./react-with-dotnet/libraries/nextui-org/all";
 import "./react-with-dotnet/libraries/uiw-react-textarea-code-editor/all";
 import "./react-with-dotnet/libraries/react-simple-code-editor/all";
 import "./react-with-dotnet/libraries/react-player/all";
+import "./react-with-dotnet/libraries/MonacoEditorReact/all";
 import "./react-with-dotnet/libraries/react-quill/all";
 import "./react-with-dotnet/libraries/react-split/all";
+import "./react-with-dotnet/libraries/_uploady_/all";
 
 export { ReactWithDotNet };
