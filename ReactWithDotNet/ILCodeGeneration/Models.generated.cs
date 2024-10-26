739720eda36a44177cb7305db1ce2d3b0c75b80f
beyaz <beyaz1404@gmail.com>
2024-10-27 01:44:17 +0300

    .

--- ReactWithDotNet/ILCodeGeneration/Models.generated.cs
+++ ReactWithDotNet/ILCodeGeneration/Models.generated.cs
@@ -2,31 +2,24 @@
 
 public record TypeReferenceModel
 {
-    // @formatter:off
-
     public required string Name { get; init; }
 
     public required string Namespace { get; init; }
     
     public required MetadataScopeModel Scope { get; init; }
-
-    // @formatter:on
 }
 
 public sealed record ParameterDefinitionModel
 {
-     // @formatter:off
-     
     public required int Index { get; init; }
 
     public required string Name { get; init; }
 
     public required TypeReferenceModel ParameterType { get; init; }
-    
-    // @formatter:on
 }
 
-public enum ExceptionHandlerType {
+public enum ExceptionHandlerType 
+{
     Catch = 0,
     Filter = 1,
     Finally = 2,
@@ -35,8 +28,6 @@ public enum ExceptionHandlerType {
 
 public sealed record ExceptionHandler
 {
-    // @formatter:off
-    
     public required int HandlerEnd { get; init; }
     
     public required int HandlerStart { get; init; }
@@ -44,156 +35,101 @@ public sealed record ExceptionHandler
     public required TypeReferenceModel CatchType { get; init; }
 
     public required ExceptionHandlerType  HandlerType { get; init; }
-
-    // @formatter:on
 }
 
 
 public record MemberReferenceModel
 {
-    // @formatter:off
-    
     public required string Name { get; init; }
     
     public required TypeReferenceModel DeclaringType { get; init; }
     
     public required string FullName { get; init; }
-
-    // @formatter:on
 }
 
 public record FieldReferenceModel : MemberReferenceModel
 {
-    // @formatter:off
-    
     public required TypeReferenceModel FieldType { get; init; }
-
-    // @formatter:on
 }
 
 public record FieldDefinitionModel : FieldReferenceModel
 {
-    // @formatter:off
-    
     public required IReadOnlyList<CustomAttributeModel> CustomAttributes { get; init; }
-
-    // @formatter:on
 }
 
 public record PropertyReferenceModel : MemberReferenceModel
 {
-    // @formatter:off
-    
     public required TypeReferenceModel PropertyType { get; init; }
     
     public required IReadOnlyList<ParameterDefinitionModel> Parameters { get; init; }
-
-    // @formatter:on
 }
 
 public record PropertyDefinitionModel : PropertyReferenceModel
 {
-    // @formatter:off
-    
     public required MethodDefinitionModel GetMethod { get; init; }
     
     public required MethodDefinitionModel SetMethod { get; init; }
     
     public required IReadOnlyList<CustomAttributeModel> CustomAttributes { get; init; }
-
-    // @formatter:on
 }
 
 public record EventReferenceModel : MemberReferenceModel
 {
-    // @formatter:off
-    
     public required TypeReferenceModel EventType { get; init; }
-
-    // @formatter:on
 }
-
 
 public sealed record MethodBodyModel
 {
-    // @formatter:off
-    
     public required IReadOnlyList<int> Instructions { get; init; }
 
     public required IReadOnlyDictionary<int, object> Operands { get; init; }
       
     public required IReadOnlyList<ExceptionHandler> ExceptionHandlers { get; init; }
-    
-    // @formatter:on
 }
 
 public record MethodReferenceModel
 {
-    // @formatter:off
-    
     public required string Name { get; init; }
     
     public required TypeReferenceModel ReturnType { get; init; }
     
     public required IReadOnlyList<ParameterDefinitionModel> Parameters { get; init; }
-    
-    // @formatter:on
 }
 
 public sealed record CustomAttributeArgumentModel
 {
-    // @formatter:off
-    
     public required TypeReferenceModel Type { get; init; }
     
     public required object Value { get; init; }
-    
-    // @formatter:on
 }
 
 public sealed record CustomAttributeNamedArgumentModel
 {
-    // @formatter:off
-    
     public required string Name { get; init; }
     
     public required CustomAttributeArgumentModel Argument { get; init; }
-    
-    // @formatter:on
 }
-
 
 public sealed record CustomAttributeModel
 {
-    // @formatter:off
-    
     public required IReadOnlyList<CustomAttributeNamedArgumentModel> Fields { get; init; }
     
     public required IReadOnlyList<CustomAttributeNamedArgumentModel> Properties { get; init; }
     
     public required IReadOnlyList<CustomAttributeArgumentModel> ConstructorArguments { get; init; }
     
-     public required MethodReferenceModel Constructor { get; init; }
-    
-    // @formatter:on
+    public required MethodReferenceModel Constructor { get; init; }
 }
-
 
 public sealed record MethodDefinitionModel : MethodReferenceModel
 {
-    // @formatter:off
-    
     public required MethodBodyModel Body { get; init; }
     
     public required IReadOnlyList<CustomAttributeModel> CustomAttributes { get; init; }
-    
-    // @formatter:on
 }
 
 public sealed record TypeDefinitionModel : TypeReferenceModel
 {
-    // @formatter:off
-    
     public required TypeReferenceModel BaseType { get; init; }
     
     public required IReadOnlyList<CustomAttributeModel> CustomAttributes { get; init; }
@@ -205,37 +141,23 @@ public sealed record TypeDefinitionModel : TypeReferenceModel
     public required IReadOnlyList<PropertyDefinitionModel> Properties { get; init; }
     
     public required IReadOnlyList<TypeDefinitionModel> NestedTypes { get; init; }
-
-    // @formatter:on
 }
 
 public sealed record MetadataScopeModel
 {
-    // @formatter:off
-
     public required string Name { get; init; }
-    
-    // @formatter:on
 }
 
 public sealed record ArrayTypeModel : TypeReferenceModel
 {
-    // @formatter:off
-    
     public required int Rank { get; init; }
     
     public required TypeReferenceModel ElementType { get; init; }
-    
-    // @formatter:on
 }
 
 public sealed record GenericInstanceMethodModel : MethodReferenceModel
 {
-    // @formatter:off
-    
     public required MethodReferenceModel ElementMethod { get; init; }
     
     public required IReadOnlyList<TypeReferenceModel> GenericArguments { get; init; }
-    
-    // @formatter:on
 }
