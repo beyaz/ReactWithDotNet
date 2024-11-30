a2e12b0ec6a3813d0124c48a29c6699b6ebf6446
beyaz <beyaz1404@gmail.com>
2024-11-30 22:12:29 +0300

    .

--- ReactWithDotNet/UIDesigner/ReactWithDotNetJavaScriptFiles.cs
+++ ReactWithDotNet/UIDesigner/ReactWithDotNetJavaScriptFiles.cs
@@ -80,14 +80,14 @@ public sealed class ReactWithDotNetJavaScriptFiles
             resourceName = requestPath;
         }
 
-        if (requestPath == "/clr.js" || requestPath == "/long.js")
+        if (requestPath.EndsWith("/clr.js", StringComparison.OrdinalIgnoreCase) || requestPath.EndsWith("/long.js", StringComparison.OrdinalIgnoreCase))
         {
             if (Environment.UserName == "beyaz")
             {
                 return new()
                 {
                     Type = ContentTypeApplication_Javascript,
-                    Data = File.ReadAllBytes($@"C:\github\ReactWithDotNet\ReactWithDotNet\JsClientEngine\{requestPath.RemoveFromStart("/")}")
+                    Data = File.ReadAllBytes($@"C:\github\ReactWithDotNet\ReactWithDotNet\JsClientEngine\{requestPath.Split('/').Last()}")
                 };
             }
 
