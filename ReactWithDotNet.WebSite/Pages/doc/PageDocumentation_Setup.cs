66a9efb1360b9e01013f7b661abe5ed5959fab2f
beyaz <beyaz1404@gmail.com>
2024-11-30 21:59:47 +0300

    .

--- ReactWithDotNet.WebSite/Pages/doc/PageDocumentation_Setup.cs
+++ ReactWithDotNet.WebSite/Pages/doc/PageDocumentation_Setup.cs
@@ -7,32 +7,29 @@ sealed class PageDocumentation_Setup : PageDocumentation
 {
     protected override Element CreateContent()
     {
-        return new FlexRow(WidthFull)
+        return new article(PaddingTopBottom(4 * rem))
         {
-            new article(PaddingTopBottom(4 * rem))
+            new h1
             {
-                new h1
-                {
-                    "Setup"
-                },
-                Space,
-                new p
-                {
-                    "Skip long and boring documentations!",
-                    br,
-                    "Stop thinking about js bundle size problems, npm and library dependencies.",
+                "Setup"
+            },
+            Space,
+            new p
+            {
+                "Skip long and boring documentations!",
+                br,
+                "Stop thinking about js bundle size problems, npm and library dependencies.",
 
-                    br, br,
-                    "As always we said that, if you are familiar to c# and react library, you are already know ReactWithDotNet library.",
-                    br, br,
-                    "Best way to learn ReactWithDotNet library is see our 'Counter' sample project",
-                    br,
-                    br,
-                    new a
-                    {
-                        "Counter Sample",
-                        Href("https://github.com/beyaz/ReactWithDotNet.Samples")
-                    }
+                br, br,
+                "As always we said that, if you are familiar to c# and react library, you are already know ReactWithDotNet library.",
+                br, br,
+                "Best way to learn ReactWithDotNet library is see our 'Counter' sample project",
+                br,
+                br,
+                new a
+                {
+                    "Counter Sample",
+                    Href("https://github.com/beyaz/ReactWithDotNet.Samples")
                 }
             }
         };
