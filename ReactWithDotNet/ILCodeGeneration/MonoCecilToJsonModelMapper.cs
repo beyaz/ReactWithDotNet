b87c42414d328010693c14feaf22ef18a01a768a
beyaz <beyaz1404@gmail.com>
2024-12-30 12:19:48 +0300

    .

--- ReactWithDotNet/ILCodeGeneration/MonoCecilToJsonModelMapper.cs
+++ ReactWithDotNet/ILCodeGeneration/MonoCecilToJsonModelMapper.cs
@@ -1308,6 +1308,26 @@ static class MonoCecilToJsonModelMapper
 
                return true;
            }
+           
+           public static bool IsSame(MetadataTable metadataTable, MethodDefinitionModel model, MethodReference reference)
+           {
+               if (reference.Name != model.Name)
+               {
+                   return false;
+               }
+
+               if (reference.DeclaringType.IndexAt(metadataTable) != model.DeclaringType)
+               {
+                   return false;
+               }
+
+               if (!reference.Parameters.IsFullMatchWith(model.Parameters, metadataTable))
+               {
+                   return false;
+               }
+
+               return true;
+           }
        }
     }
     
@@ -1347,26 +1367,7 @@ static class MonoCecilToJsonModelMapper
             isSame<MethodReferenceModel, MethodReferenceModel>((modelA, modelB) => Compare.Method.IsSame(metadataTable, modelA, modelB)),
             isSame<MethodDefinitionModel, MethodDefinitionModel>((modelA, modelB) => Compare.Method.IsSame(metadataTable, modelA, modelB)),
             isSame<MethodReferenceModel, MethodReference>((model, reference) => Compare.Method.IsSame(metadataTable, model, reference)),
-
-            isSame<MethodDefinitionModel, MethodReference>((model, reference) =>
-            {
-                if (reference.Name != model.Name)
-                {
-                    return false;
-                }
-
-                if (reference.DeclaringType.IndexAt(metadataTable) != model.DeclaringType)
-                {
-                    return false;
-                }
-
-                if (!reference.Parameters.IsFullMatchWith(model.Parameters, metadataTable))
-                {
-                    return false;
-                }
-
-                return true;
-            }),
+            isSame<MethodDefinitionModel, MethodReference>((model, reference) =>Compare.Method.IsSame(metadataTable, model, reference)),
 
             isSame<GenericInstanceMethodModel, GenericInstanceMethodModel>((modelA, modelB) =>
             {
