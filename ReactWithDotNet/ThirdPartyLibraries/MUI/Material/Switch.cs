5449588b8644628a9acbf814705d3048894a61ae
beyaz <beyaz1404@gmail.com>
2025-02-01 18:05:27 +0300

    .

--- ReactWithDotNet/ThirdPartyLibraries/MUI/Material/Switch.cs
+++ ReactWithDotNet/ThirdPartyLibraries/MUI/Material/Switch.cs
@@ -1,4 +1,4 @@
-// auto generated code (do not edit manually)
+﻿// auto generated code (do not edit manually)
 
 namespace ReactWithDotNet.ThirdPartyLibraries.MUI.Material;
 
@@ -9,12 +9,23 @@ public partial class Switch
     ///     <br/>
     ///     It supports both default and custom theme colors, which can be added as shown in the
     ///     <br/>
-    ///     [palette customization guide](https://mui.com/material-ui/customization/palette/#adding-new-colors).
+    ///     [palette customization guide](https://mui.com/material-ui/customization/palette/#custom-colors).
     ///     <br/>
     ///     @default 'primary'
     /// </summary>
     [ReactProp]
     public string color { get; set; }
+    
+    /// <summary>
+    ///     The color of the component.
+    ///     <br/>
+    ///     It supports both default and custom theme colors, which can be added as shown in the
+    ///     <br/>
+    ///     [palette customization guide](https://mui.com/material-ui/customization/palette/#custom-colors).
+    ///     <br/>
+    ///     @default 'primary'
+    /// </summary>
+    public static Modifier Color(string value) => CreateThirdPartyReactComponentModifier<Switch>(x => x.color = value);
     
     /// <summary>
     ///     The size of the component.
@@ -25,4 +36,13 @@ public partial class Switch
     /// </summary>
     [ReactProp]
     public string size { get; set; }
+    
+    /// <summary>
+    ///     The size of the component.
+    ///     <br/>
+    ///     `small` is equivalent to the dense switch styling.
+    ///     <br/>
+    ///     @default 'medium'
+    /// </summary>
+    public static Modifier Size(string value) => CreateThirdPartyReactComponentModifier<Switch>(x => x.size = value);
 }
