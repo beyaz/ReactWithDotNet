ddf06b830dcf8bd0d42c0e780c9599d463bd57b6
beyaz <beyaz1404@gmail.com>
2025-02-14 00:17:17 +0300

    .

--- ReactWithDotNet/UIDesigner/ReactWithDotNetJavaScriptFiles.cs
+++ ReactWithDotNet/UIDesigner/ReactWithDotNetJavaScriptFiles.cs
@@ -162,7 +162,7 @@ public sealed class ReactWithDotNetJavaScriptFiles
 
 partial class Mixin
 {
-    internal const string Version = "1.0.12";
+    internal const string Version = "1.0.13";
     public static readonly string IndexJsFilePath = $"/ReactWithDotNet/{Version}/index.js";
     public static readonly string IndexCssFilePath = $"/ReactWithDotNet/{Version}/index.css";
 }
