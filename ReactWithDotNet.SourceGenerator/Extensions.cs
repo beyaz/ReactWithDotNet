71a686c7b2e2d971ac0abcc375e172b75c18f3db
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-03-16 10:24:08 +0300

    .

--- ReactWithDotNet.SourceGenerator/Extensions.cs
+++ ReactWithDotNet.SourceGenerator/Extensions.cs
@@ -1,9 +1,25 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using System.Diagnostics;
 
 namespace ReactWithDotNet;
 
 static class Extensions
 {
+    
+    public static string GetFileFromProject(this SyntaxNodeAnalysisContext context, string fileName)
+    {
+        var sourceReferenceResolver = context.Compilation.Options.SourceReferenceResolver;
+        var normalizedPath = sourceReferenceResolver.NormalizePath(fileName, null);
+        var resolvedPath = sourceReferenceResolver.ResolveReference(normalizedPath, null);
+
+        if (resolvedPath != null && File.Exists(resolvedPath))
+        {
+            return File.ReadAllText(resolvedPath);
+        }
+
+        return null;
+    }
+    
     public static void AttachToDebugger()
     {
         if (!Debugger.IsAttached)
