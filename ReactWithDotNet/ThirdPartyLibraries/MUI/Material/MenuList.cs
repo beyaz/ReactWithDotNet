7f9a0e636bf1073b9f08a7c1997f57323adab624
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-08-15 00:13:12 +0300

    .

---
+++ ReactWithDotNet/ThirdPartyLibraries/MUI/Material/MenuList.cs
@@ -0,0 +1,13 @@
+﻿namespace ReactWithDotNet.ThirdPartyLibraries.MUI.Material;
+
+public sealed class MenuList : ElementBase
+{
+    [ReactProp]
+    public bool? autoFocus { get; set; }
+
+    [ReactProp]
+    public bool? disableAutoFocusItem { get; set; }
+   
+    [ReactProp]
+    public string variant { get; set; }
+}
