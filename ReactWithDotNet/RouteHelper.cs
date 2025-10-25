76652bbaa9946c264de8f10b03a31ce7337619ef
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-10-25 11:55:07 +0300

    .

---
+++ ReactWithDotNet/RouteHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace ReactWithDotNet;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public sealed class RouteAttribute : Attribute
+{
+    public string Url { get; }
+
+    public RouteAttribute(string url)
+    {
+        Url = url;
+    }
+}
+
+public sealed class RouteInfo
+{
+    public string Url { get; init; }
+
+    public Type Page { get; init; }
+}
+
+public static class RouteHelper
+{
+    public static IReadOnlyDictionary<string, RouteInfo> GetRoutesFromAssembly(Assembly assembly)
+    {
+        var routes =
+        (
+            from type in assembly.GetTypes()
+            from route in type.GetCustomAttributes<RouteAttribute>()
+            select new { Template = route.Url, type }
+        )
+        .ToDictionary(x => x.Template, x => new RouteInfo
+        {
+            Url = x.Template, 
+            Page = x.type
+        }, StringComparer.OrdinalIgnoreCase);
+        
+        return routes;
+    }
+}
